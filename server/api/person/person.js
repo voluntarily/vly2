9298e150ceebed631e0685b7311d96cbaf14eabd
Andrew Watkins <andrew@groat.nz>
2019-05-03 08:48:57 +1200

    add person model and db script

---
+++ server/api/person/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// simplified version without Auth
+const personSchema = new Schema({
+  name: { type: 'String', index: true, unique: true, required: true }, // long full name
+  moniker: { type: 'String', default: '' }, // how we should address you - eg. Andrew
+  about: { type: 'String', default: '' }, // person description
+  email: { type: 'String', index: true, required: true }, // person@example.com
+  phone: { type: 'String', required: true }, // +64 27 7031007
+  gender: { type: 'String', default: '' }, // whatever they want to write.
+  password: { type: 'String' }, // encoded
+  language: { type: String, default: 'EN', lowercase: true }, // en, mi, fr etc
+  avatar: String, // url to image
+  role: {
+    type: [String],
+    required: true,
+    default: ['volunteer'],
+    enum: ['admin', 'op-provider', 'volunteer', 'content-provider', 'tester']
+  },
+  // used to indicate whether people show up in searches.
+  status: {
+    type: 'String',
+    required: true,
+    default: 'active',
+    enum: ['active', 'inactive', 'hold']
+  },
+  dateAdded: { type: 'Date', default: Date.now, required: true }
+})
+
+module.exports = mongoose.model('Person', personSchema)
