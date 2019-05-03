ae30bc3310e7150c3d5d6e2f8163b0f69792e1b8
Andrew Watkins <andrew@groat.nz>
2019-05-03 18:51:22 +1200

    added test people

--- server/models/kitten.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
-
-const kittenSchema = new Schema({
-  name: { type: String, required: true }
-})
-
-module.exports = mongoose.model('Kitten', kittenSchema)
