1072df245470f456d58a97677e414ed74c7a4ae8
Darcy Cox <darcycox97@gmail.com>
2019-05-08 22:07:33 +1200

    Include validation for person and opportunity references

--- server/api/interest/interest.js
+++ server/api/interest/interest.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const idvalidator = require('mongoose-id-validator');
 
 const interestSchema = new Schema({
-    personId: {type: 'String', required: true},
-    opportunityId: {type: 'String', required: true},
+    person: {type: Schema.Types.ObjectId, ref: 'Person', required: true},
+    opportunity: {type: Schema.Types.ObjectId, ref: 'Opportunity', required: true},
     comment: String
 })
 
+interestSchema.plugin(idvalidator);
 module.exports = mongoose.model('Interest', interestSchema)
