f4f6bfbc70e2755b8a0666484e83675872e87188
Andrew Meads <andrew.meads@auckland.ac.nz>
2019-05-24 10:07:44 +1200

    Refactored RegisterInterestItem to use FormattedMessages

--- components/Interest/RegisterInterestItem.js
+++ components/Interest/RegisterInterestItem.js
@@ -1,5 +1,6 @@
 /*
-  Dumb component. Contains information about a volunteer's interest in an opportunity. Unlike InterestItem, this one is a Form allowing state changes.
+  Dumb component. Contains information about a volunteer's interest in an opportunity.
+  Unlike InterestItem, this one is a Form allowing state changes.
 */
 
 import React, { Component } from 'react'
@@ -31,11 +32,9 @@ class RegisterInterestItem extends Component {
 
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        // console.log('success!')
-        // console.log(values)
         const interest = this.props.interest
         interest.comment = values.comment
-        interest.status = this.getNextStatus()
+        interest.status = getNextStatus(interest)
 
         this.props.onChangeStatus(interest)
       }
@@ -50,93 +49,6 @@ class RegisterInterestItem extends Component {
     this.props.onWithdraw(this.props.interest)
   }
 
-  getNextStatus() {
-    switch (this.props.interest.status) {
-      case null:
-        return 'interested'
-
-      case 'invited':
-        return 'committed'
-    }
-  }
-
-  // Returns some config options for this component, depending on the state of the interest we're viewing.
-  getOptions() {
-
-    const options = {
-      headerAlwaysVisible: true,
-      headingText: '',
-      subHeadingText: '',
-      nextStateButtonEnabled: true,
-      nextStateButtonText: '',
-      withdrawInterestButtonEnabled: true,
-      withdrawInterestButtonText: 'Withdraw Interest',
-      formAlwaysVisible: true,
-      showFormButtonText: '',
-      hideFormButtonText: 'Cancel',
-      commentsEditable: false,
-      commentsPlaceholderText: ''
-    }
-
-    switch (this.props.interest.status) {
-
-      case null:
-        options.headerAlwaysVisible = false
-        options.headingText = 'How do you want to get involved?'
-        options.subHeadingText = 'Type in how you want to get involved, and an organizer will get in touch with you :)'
-        options.nextStateButtonText = 'Get Involved!'
-        options.showFormButtonText = 'I\'m Interested'
-        options.commentsPlaceholderText = 'How do you want to help out? Got any questions?'
-        options.commentsEditable = true
-        options.withdrawInterestButtonEnabled = false
-        options.formAlwaysVisible = false
-        break
-
-      case 'interested':
-        options.headingText = 'Thank you for expressing your interest!'
-        options.subHeadingText = 'The organizer will be in touch shortly :)'
-        options.nextStateButtonEnabled = false
-        break
-
-      case 'invited':
-        options.headingText = 'You\'ve been invited to participate!'
-        options.subHeadingText = 'Please let the organizer know whether you can attend.'
-        options.nextStateButtonText = 'I can make it :)'
-        options.withdrawInterestButtonText = 'I can\'t make it :('
-        break
-
-      case 'committed':
-        options.headingText = 'Thank you so much!'
-        options.subHeadingText = 'You have agreed to participate in this event!'
-        options.nextStateButtonEnabled = false
-        break
-
-      case 'declined':
-        options.headingText = 'Our apologies'
-        options.subHeadingText = 'Thank you so much for registering your interest. However, all available spots for this event have been filled.'
-        options.nextStateButtonEnabled = false
-        options.withdrawInterestButtonEnabled = false
-        break
-
-      case 'completed':
-        options.headingText = 'Thank you so much!'
-        options.subHeadingText = 'We hope you enjoyed your event, and we look forward to working with you in the future!'
-        options.nextStateButtonEnabled = false
-        options.withdrawInterestButtonEnabled = false
-        break
-
-      case 'cancelled':
-        options.headingText = 'Our apologies'
-        options.subHeadingText = 'Thank you so much for registering your interest. However, unfortunately this event has been cancelled by the organizer.'
-        options.nextStateButtonEnabled = false
-        options.withdrawInterestButtonEnabled = false
-        break
-
-    }
-
-    return options
-  }
-
   render() {
 
     const {
@@ -144,7 +56,7 @@ class RegisterInterestItem extends Component {
     } = this.props.form
 
     // Options to configure the controls on this page based on the state of the interest.
-    const options = this.getOptions()
+    const options = getOptions(this.props.interest)
 
     return (
       <div>
@@ -195,9 +107,6 @@ class RegisterInterestItem extends Component {
                     {options.withdrawInterestButtonText}
                   </Button>
                 </Popconfirm>
-                {/*<Button type='danger' shape='round' onClick={this.handleWithdrawButtonClicked.bind(this)}>
-                  {options.withdrawInterestButtonText}
-            </Button>*/}
                 &nbsp;
             </span> : null}
 
@@ -205,7 +114,7 @@ class RegisterInterestItem extends Component {
             {!options.formAlwaysVisible && !this.state.isFormVisible ?
               <span>
                 <Button type='primary' shape='round' onClick={() => this.setState({ isFormVisible: true })}>
-                  <FormattedMessage id='claimOp' defaultMessage="I'm Interested" description='Button to show interest in an opportunity on OpDetails page' />
+                  {options.showFormButtonText}
                 </Button>
                 &nbsp;
             </span> : null}
@@ -214,7 +123,7 @@ class RegisterInterestItem extends Component {
             {!options.formAlwaysVisible && this.state.isFormVisible ?
               <span>
                 <Button type='secondary' shape='round' onClick={() => this.setState({ isFormVisible: false })}>
-                  Cancel
+                  {options.hideFormButtonText}
                 </Button>
                 &nbsp;
             </span> : null}
@@ -227,6 +136,8 @@ class RegisterInterestItem extends Component {
 
 }
 
+
+// Ensures the correct properties are being supplied to this component
 RegisterInterestItem.propTypes = {
   interest: PropTypes.shape({
     person: PropTypes.string.isRequired,
@@ -239,6 +150,8 @@ RegisterInterestItem.propTypes = {
   onWithdraw: PropTypes.func.isRequired
 }
 
+
+// Adds form logic to this component
 export default Form.create({
   name: 'register_interest_form',
   onFieldsChange(props, changedFields) {
@@ -250,7 +163,93 @@ export default Form.create({
       comment: Form.createFormField({ ...props.interest.comment, value: props.interest.comment })
     }
   }
-  // onValuesChange (_, values) {
-  //   console.log('onValuesChange', values)
-  // }
 })(RegisterInterestItem)
+
+
+// Returns the next status, given the current status
+function getNextStatus(interest) {
+  switch (interest.status) {
+    case null:
+      return 'interested'
+
+    case 'invited':
+      return 'committed'
+  }
+}
+
+// Returns some config options for this component, depending on the state of the interest we're viewing.
+function getOptions(interest) {
+
+  const options = {
+    headerAlwaysVisible: true,
+    headingText: '',
+    subHeadingText: '',
+    nextStateButtonEnabled: true,
+    nextStateButtonText: '',
+    withdrawInterestButtonEnabled: true,
+    withdrawInterestButtonText: <FormattedMessage id='withdrawInterestButton' defaultMessage='Withdraw Interest' description='Button for volunteer to withdraw interest in an opportunity' />,
+    formAlwaysVisible: true,
+    showFormButtonText: '',
+    hideFormButtonText: <FormattedMessage id='registerInterestHideForm' defaultMessage='Cancel' description='Button to hide express-interest form' />,
+    commentsEditable: false,
+    commentsPlaceholderText: ''
+  }
+
+  switch (interest.status) {
+
+    case null:
+      options.headerAlwaysVisible = false
+      options.headingText = <FormattedMessage id='getInvolvedHeading' defaultMessage='How do you want to get involved?' description='Heading displayed on form allowing volunteer to express interest in an opportunity' />
+      options.subHeadingText = <FormattedMessage id='getInvolvedSubHeading' defaultMessage='Type in how you want to get involved, and an organizer will get in touch with you :)' description='Sub-heading displayed on form allowing volunteer to express interest in an opportunity' />
+      options.nextStateButtonText = <FormattedMessage id='getInvolvedButton' defaultMessage='Get Involved!' description='Button allowing volunteer to express interest in an opportunity' />
+      options.showFormButtonText = <FormattedMessage id='registerInterestShowForm' defaultMessage="I'm Interested" description='Button to allow volunteer to start expressing interest in an opportunity' />
+      options.commentsPlaceholderText = 'How do you want to help out? Got any questions?' // Can't use FormattedMessage here, is there something else I can use?
+      options.commentsEditable = true
+      options.withdrawInterestButtonEnabled = false
+      options.formAlwaysVisible = false
+      break
+
+    case 'interested':
+      options.headingText = <FormattedMessage id='isInterestedHeading' defaultMessage='Thank you for expressing your interest!' description='Heading on express-interest form when volunteer has already expressed interest' />
+      options.subHeadingText = <FormattedMessage id='isInterestedSubHeading' defaultMessage='The organizer will be in touch shortly :)' description='Sub-heading on express-interest form when volunteer has already expressed interest' />
+      options.nextStateButtonEnabled = false
+      break
+
+    case 'invited':
+      options.headingText = <FormattedMessage id='isInvitedHeading' defaultMessage="You've been invited to participate!" description='Heading displayed on express-interest form when volunteer has been invited to participate' />
+      options.subHeadingText = <FormattedMessage id='isInvitedSubHeading' defaultMessage='Please let the organizer know whether you can attend.' description='Sub-heading displayed on express-interest form when volunteer has been invited to participate' />
+      options.nextStateButtonText = <FormattedMessage id='isInvitedAcceptButton' defaultMessage='I can make it :)' description='Allows volunteer to accept invitation to participate in opportunity' />
+      options.withdrawInterestButtonText = <FormattedMessage id='isInvitedRejectButton' defaultMessage="I can't make it :(" description='Allows volunteer to withdraw from an opportunity once they have been invited' />
+      break
+
+    case 'committed':
+      options.headingText = <FormattedMessage id='isCommittedHeading' defaultMessage='Thank you so much!' description='Heading displayed when volunteer has committed to an op' />
+      options.subHeadingText = <FormattedMessage id='isCommittedSubHeading' defaultMessage='You have agreed to participate in this event!' description='Sub-heading displayed when volunteer has committed to an op' />
+      options.nextStateButtonEnabled = false
+      break
+
+    case 'declined':
+      options.headingText = <FormattedMessage id='isDeclinedHeading' defaultMessage='Our apologies' description='Heading displayed when volunteer has been declined by opportunity organizer' />
+      options.subHeadingText = <FormattedMessage id='isDeclinedSubHeading' defaultMessage='Thank you so much for registering your interest. However, all available spots for this event have been filled.' description='Sub-heading displayed when volunteer has been declined by opportunity organizer' />
+      options.nextStateButtonEnabled = false
+      options.withdrawInterestButtonEnabled = false
+      break
+
+    case 'completed':
+      options.headingText = <FormattedMessage id='isCompletedHeading' defaultMessage='Thank you so much!' description='Heading displayed when volunteer has participated in an op' />
+      options.subHeadingText = <FormattedMessage id='isCompletedSubHeading' defaultMessage='We hope you enjoyed your event, and we look forward to working with you in the future!' description='Sub-heading displayed when volunteer has participated in an op' />
+      options.nextStateButtonEnabled = false
+      options.withdrawInterestButtonEnabled = false
+      break
+
+    case 'cancelled':
+      options.headingText = <FormattedMessage id='isCancelledHeading' defaultMessage='Our apologies' description='Heading displayed to volunteer when opportunity is cancelled by organizer' />
+      options.subHeadingText = <FormattedMessage id='isCancelledSubHeading' defaultMessage='Thank you so much for registering your interest. However, unfortunately this event has been cancelled by the organizer.' description='Sub-heading displayed to volunteer when opportunity is cancelled by organizer' />
+      options.nextStateButtonEnabled = false
+      options.withdrawInterestButtonEnabled = false
+      break
+
+  }
+
+  return options
+}
