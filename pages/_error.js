6e9ab8475726832e66791e8020ce60d1f06f9d6e
Vaughan <me@vaughan.ai>
2019-05-25 17:41:04 +1200

    Added 404 handler
    Added a new error page handler to render a custom 404 page. All so Walt can put an emoji on it one day.

---
+++ pages/_error.js
@@ -0,0 +1,108 @@
+/**
+ * Creating a page named _error.js lets you override HTTP error messages
+ */
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import App, { Container } from 'next/app'
+import Markdown from 'markdown-to-jsx'
+import { withRouter } from 'next/router'
+import { FormattedMessage, FormattedNumber } from 'react-intl'
+import publicPage, { FullPage } from '../hocs/publicPage'
+import { Spacer } from '../components/VTheme/VTheme'
+
+class ErrorPage extends React.Component {
+
+  static propTypes() {
+    return {
+      errorCode: React.PropTypes.number.isRequired,
+      url: React.PropTypes.string.isRequired
+    }
+  }
+
+  static getInitialProps({res, xhr}) {
+    const errorCode = res ? res.statusCode : (xhr ? xhr.status : null)
+    return {errorCode}
+  }
+
+  render() {
+    var response
+    switch (this.props.errorCode) {
+      case 200: // Also display a 404 if someone requests /_error explicitly
+      case 404:
+        response = (
+          <FullPage>
+            <Head>
+            <FormattedMessage
+              id='error.pagenotfound.title'
+              description='Page title for the 404 error'
+              defaultMessage='Page not found'
+              tagName="title"
+            />
+            </Head>
+            <Spacer />
+            <Spacer />
+            <div>
+            <FormattedMessage
+              id='error.pagenotfound.title'
+              description='Page title for the 404 error'
+              defaultMessage='Page not found'
+              tagName="h1"
+            />
+            <FormattedMessage
+              id='error.pagenotfound.description'
+              defaultMessage="The page you are looking for is not here. We have looked everywhere but it doesn't seem to exist"
+              description='Use the local equivalent of Hello World.'
+            />
+          &nbsp;
+          </div>
+          <Container className="pt-5 text-center">
+            <p>
+              An <strong>HTTP { this.props.errorCode }</strong> error occurred while
+              trying to access <strong>{ this.props.router.pathname }</strong>
+            </p>
+          </Container>
+        </FullPage>
+        )
+        break
+      default:
+        response = (
+          <FullPage>
+            <Head>
+            <FormattedMessage
+              id='error.pagenotfound.title'
+              description='Page title for the 404 error'
+              defaultMessage='Page not found'
+              tagName="title"
+            />
+            </Head>
+            <Spacer />
+            <Spacer />
+            <div>
+            <FormattedMessage
+              id='error.servererror.title'
+              description='A server error'
+              defaultMessage='Sorry, there was a problem and we can not complete this task. We have let our team know so they can take a look and fix it. For now try to refresh the page, or go back to the previous page'
+              tagName="h1"
+            />
+
+            <Container className="pt-5 text-center">
+              <h1 className="display-4">HTTP { this.props.errorCode } Error</h1>
+              <p>
+                An <strong>HTTP { this.props.errorCode }</strong> error occurred while
+                trying to access <strong>{ this.props.router.pathname }</strong>
+              </p>
+            </Container>
+          &nbsp;
+          </div>
+          </FullPage>
+
+        )
+    }
+
+    return response
+  }
+
+}
+
+export default withRouter(ErrorPage)
