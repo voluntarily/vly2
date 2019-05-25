0bde6e98a9b9e700291bd3923b56567cb8e27222
Darcy Cox <darcycox97@gmail.com>
2019-05-25 12:41:44 +1200

    Include all opportunities when search endpoint hit

--- server/api/search/search.controller.js
+++ server/api/search/search.controller.js
@@ -1,16 +1,18 @@
 
 // const Interest = require('./interest')
+const Opportunity = require('../opportunity/opportunity')
 
 /**
   api/search?q='keyword' -> lists all opportunities with properties matching the keyword.
  */
 const searchOpportunities = async (req, res) => {
   console.log(req.query)
+  const opportunities = await Opportunity.find({}).exec()
   try {
     // const opps;
     if (req.query.q) {
       // search
-      res.json("success!!!")
+      res.json(opportunities);
     } else {
         res.status(400).send('Bad Request: must include keyword parameters');
     }
