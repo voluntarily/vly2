b1d2e150174bfcb6a00b2a00a48b535a0a2197bf
Darcy Cox <darcycox97@gmail.com>
2019-05-25 12:55:31 +1200

    fix linting issues

--- server/api/tags/tag.js
+++ server/api/tags/tag.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const tagSchema = new Schema({
-  tag: String, // "e.g C++",
+  tag: String // "e.g C++",
 })
 
 module.exports = mongoose.model('Tag', tagSchema)
