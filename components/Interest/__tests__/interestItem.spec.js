f471bbaf589486efb3345f53c45da6fe6425c907
Sian <phillips.sian@gmail.com>
2019-05-25 13:46:11 +1200

    VP-123: Added unit testing for Interest Item

---
+++ components/Interest/__tests__/interestItem.spec.js
@@ -0,0 +1,19 @@
+import InterestItem from '../InterestItem'
+import test from 'ava'
+import { mount } from 'enzyme'
+
+test('constructs properly', t => {
+  const wrapper = mount(<InterestItem interest={{
+    person: 'Test Name',
+    opportunity: 'Test Opportunity',
+    comment: 'Test Comment',
+    status: 'Test Status',
+    _id: '11223344'
+  }} />)
+
+  t.truthy(wrapper.find('.person'), 'Test Name')
+  t.truthy(wrapper.find('.opportunity'), 'Test Opportunity')
+  t.truthy(wrapper.find('.comment'), 'Test Comment')
+  t.truthy(wrapper.find('.status'), 'Test Status')
+  t.truthy(wrapper.find('._id'), '11223344')
+})
