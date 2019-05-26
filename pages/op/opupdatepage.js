b323d2a6800a82cfdcf5e6d402aa32819ba3eb5b
Darcy Cox <darcycox97@gmail.com>
2019-05-26 19:39:43 +1200

    Working on tag validation. Only sending one request for a new tag. need to send for all non-existing tags

--- pages/op/opupdatepage.js
+++ pages/op/opupdatepage.js
@@ -29,12 +29,58 @@ export class OpUpdatePage extends Component {
     }
   }
 
+  async componentDidMount () {
+    try {
+      await this.props.dispatch(reduxApi.actions.tags.get())
+      console.log('got tags')
+      console.log(this.props.tags.data)
+    } catch (err) {
+      console.log('error in getting tags', err)
+    }
+  }
+
   handleCancel = (op) => {
     Router.back()
   }
 
   async handleAdd (op) {
     if (!op) return
+
+    // determine which tags need to be added to the database
+    // based on whether they exist already or not.
+    // const userTags = op.tags // user-defined tags
+    const userTags = ['databases2', 'backend2']
+    const dbTags = this.props.tags.data // tags existing in db
+    const tagIds = [] // user-defined tags converted to their objectids
+    const newTags = [] // user-defined tag values that aren't in the db
+
+    userTags.forEach(tag => {
+      const match = dbTags.find((dbTag) => tag === dbTag.tag)
+      if (match) {
+        tagIds.push(match._id)
+      } else {
+        newTags.push({ tag: tag })
+      }
+    })
+
+    console.log('tags needing to be added:')
+    console.log(newTags)
+
+    // send post requests for each non-existing tag to create them
+    const promises = []
+    newTags.forEach(newTag => {
+      promises.push(this.props.dispatch(reduxApi.actions.tags.post({}, { body: JSON.stringify(newTag) })))
+    })
+
+    console.log(promises)
+    const results = await Promise.all(promises)
+    console.log(results)
+    results.forEach(result => {
+      tagIds.push(result._id)
+    })
+
+    op.tags = tagIds
+
     // Actual data request
     let res = {}
     if (op._id) {
