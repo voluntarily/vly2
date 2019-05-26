446367f295bea0ea98adb9e78bac0acc14947b82
Minh-Phu <Lminhphu0304@gmail.com>
2019-05-26 14:20:36 +1200

    Implement upload image to local folder
    In production it should use cloud

--- server/api/postImage/postImage.controller.js
+++ server/api/postImage/postImage.controller.js
@@ -1,50 +1,28 @@
-/**
- * Get system health and ask questions
- */
-// const FileReader = require('filereader').FileReader
-const IncomingForm = require('formidable').IncomingForm
-// TODO get the version number or build number here
-const greet = 'Hello from Voluntari.ly V0.0.2'
-// const fs = require('fs')
-// const FR = new FileReader()
+
+const fs = require('fs')
 // check a bunch of things here like whether the db is connected and responding.
 // any depended upon api services
 const postImage = (req, res) => {
-  var form = new IncomingForm()
-  form.on('file', (field, file) => {
+  const ImageBin = req.body.image
+  const ImageBuffer = Buffer.from(ImageBin, 'binary')
 
-    // let FR = new FileReader()
-    // let ImageBin = FR.readAsBinaryString(file)
-    // let ImageBuffer = Buffer.from(ImageBin, 'binary')
-    // fs.writeFile('upload/image.jpeg', ImageBuffer, (err) => {
-    //   if (err) console.log(err)
-    //   else {
-    //     console.log('success')
-    //   }
-    // })
-    console.log(JSON.stringify(file))
-  })
-  form.on('end', () => {
-    const response = {
-      message: greet,
-      health: 'OK',
-      params: req.params,
-      query: req.query
+  const result = {
+    status: 0,
+    message: '',
+    imageUR: ''
+  }
+  fs.writeFile('./static/img/' + req.body.file, ImageBuffer, (err) => {
+    if (err) {
+      result.status = 418
+      result.message = 'I am a tea pot'
+    } else {
+      console.log('success')
+      result.status = 200
+      result.message = 'Success'
+      result.imageURL = './static/img/' + req.body.file
     }
-
-    res.json(response)
+    res.status(result.status).json(result)
   })
-  form.parse(req)
-
-  // let ImageBuffer = Buffer.from(ImageBin, 'binary')
-  // fs.writeFile('upload/image.jpeg', ImageBuffer, (err) => {
-  //   if (err) console.log(err)
-  //   else {
-  //     console.log('success')
-  //   }
-  // })
-
-  // return res.status(200).json(result)
 }
 
 module.exports = postImage
