433d5493362307289b0896dfea2d010baec98a0b
Alisa Herrera-Hayman <alisamhh@gmail.com>
2019-05-26 15:04:28 +1200

    Fixed broken expectedBadHealth test

--- components/testredux/__tests__/ReduxAsyncTest.spec.js
+++ components/testredux/__tests__/ReduxAsyncTest.spec.js
@@ -64,6 +64,11 @@ test.serial('bad api/health', async t => {
   // undefined store returns initial state
   fetchMock.get(`${API_URL}/health`, 404)
 
+  const expectedBadHealth = {
+    message: 'Hello from Voluntari.ly V0.0.2',
+    health: 'Not OK'
+  }
+
   await fetchHealth()(t.context.realStore.dispatch)
   const newstate = await t.context.realStore.getState().health
   t.deepEqual(newstate, expectedBadHealth)
