882f1a41d774c611f9ff96f31ec4725d64c9d47f
watkinsav <watkinsav@CORP.AMAZONWORKSPACES.COM>
2019-05-29 18:37:12 +1200

    fix econn problem

--- server/util/test-memory-mongo.js
+++ server/util/test-memory-mongo.js
@@ -1,34 +1,43 @@
 // mockMongo
 // File for abstracting generic before, beforeEach, afterEach and after code
 import mongoose from 'mongoose'
-// mongoose.Promise = Promise
+mongoose.Promise = Promise
 const { MongoMemoryServer } = require('mongodb-memory-server')
 
 class MemoryMongo {
   constructor () {
     this.connection = null
     this.mms = new MongoMemoryServer({
-      // debug: false, // by default false
-      // binary: {
-      //   version: '4.0.5'
-      // }
+      debug: false, // by default false
+      binary: {
+        debug: false
+        // version: '4.0.5'
+      }
     })
   }
 
   async start () {
-    const mongoUri = await this.mms.getConnectionString()
-    // console.log('Test mongodb connecting to:', mongoUri)
+    this.mongoUri = await this.mms.getConnectionString()
+    // console.log('Test mongodb connecting to:', this.mongoUri)
     const mongooseOpts = {
       // options for mongoose 4.11.3 and above
       useNewUrlParser: true,
       useCreateIndex: true
     }
-    this.connection = await mongoose.connect(mongoUri, mongooseOpts)
+    this.connection = await mongoose.connect(this.mongoUri, mongooseOpts)
+
   }
 
-  stop () {
-    mongoose.disconnect()
-    return this.mms.stop()
+  async stop () {
+    try {
+      // await mongoose.disconnect()
+      this.connection = null
+      // console.log('test mongodb disconnected', this.mongoUri)
+      await this.mms.stop()
+      this.mms = null
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   cleanup () {
