c30c5c35f41369cb33cd5fa6bf642960c51e9069
AidanTheDeveloper <aidanharris1001@gmail.com>
2019-06-09 14:57:09 +1200

    [VP-32] Swapped out antD uploader for uppy

--- components/UploadComponent/ImageUploadComponent.js
+++ components/UploadComponent/ImageUploadComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
-import { message, Upload, Icon, Button } from 'antd'
+import { message } from 'antd'
 import callApi from '../../lib/apiCaller'
 
-const validImageFile = ['image/png', 'image/jpeg', 'image/jpg', 'image/svg']
+const Uppy = require('@uppy/core')
+const { DragDrop } = require('@uppy/react')
+
+// const validImageFile = ['image/png', 'image/jpeg', 'image/jpg', 'image/svg']
 
 class ImageUpload extends Component {
   TWO_MEGABYTES = 2000000
@@ -13,58 +16,89 @@ class ImageUpload extends Component {
 
   constructor (props) {
     super(props)
-    this.onChangeImageUpload = this.onChangeImageUpload.bind(this)
-    this.uploadCustomRequest = this.uploadCustomRequest.bind(this)
+    this.onFileAdded = this.onFileAdded.bind(this)
+    this.uppy = Uppy({
+      id: 'uppy',
+      autoProceed: false,
+      allowMultipleUploads: false,
+      debug: true,
+      formData: true,
+      restrictions: {
+        maxFileSize: 2000000,
+        maxNumberOfFiles: 1,
+        minNumberOfFiles: 0,
+        allowedFileTypes: null
+      },
+      meta: {}
+    })
+
+    this.uppy.on('file-added', this.onFileAdded)
+    // TODO Override other uppy events (file-added, file-removed, upload, upload-progress, upload-success, complete, error, upload-errror, upload-retry, info-visible, info-hidden, cancel-all, restriction-failed)
   }
 
-  onChangeImageUpload (info) {
-    const fileType = info.file.type
-    const isImage = validImageFile.includes(fileType)
-
-    if (info.file.size > this.TWO_MEGABYTES) {
-      message.error('You can only upload image files less than 2 Mb')
-      this.setState({ fileList: [] })
-    } else if (!isImage) {
-      message.error('You can upload only image files')
-      this.setState({ fileList: [] })
-    } else {
-      let fileList = [...info.fileList]
-      fileList = fileList.slice(-1)
-      this.setState({ fileList })
-    }
-  }
-
-  uploadCustomRequest ({ onSuccess, onError, file }) {
+  onFileAdded (file) {
     let FR = new window.FileReader()
     FR.onloadend = e => {
-      callApi('images', 'post', { image: e.currentTarget.result, file: 'img.png' }).then(response => {
+      callApi('images', 'post', { image: e.currentTarget.result, file: file.name }).then(response => {
         console.log('Success! ' + response)
-        onSuccess('ok')
       },
       error => {
-        // TODO Clear file list upon error
         message.error('An error occured: ' + error.status + ' ' + error.statusText)
-        onError('fail')
       })
     }
-    FR.readAsBinaryString(file)
+    FR.readAsBinaryString(file.data)
   }
+  // this.onChangeImageUpload = this.onChangeImageUpload.bind(this)
+  // this.uploadCustomRequest = this.uploadCustomRequest.bind(this)
+
+  // onChangeImageUpload (info) {
+  //   const fileType = info.file.type
+  //   const isImage = validImageFile.includes(fileType)
+
+  //   if (info.file.size > this.TWO_MEGABYTES) {
+  //     message.error('You can only upload image files less than 2 Mb')
+  //     this.setState({ fileList: [] })
+  //   } else if (!isImage) {
+  //     message.error('You can upload only image files')
+  //     this.setState({ fileList: [] })
+  //   } else {
+  //     let fileList = [...info.fileList]
+  //     fileList = fileList.slice(-1)
+  //     this.setState({ fileList })
+  //   }
+  // }
+
+  // uploadCustomRequest ({ onSuccess, onError, file }) {
+  //   let FR = new window.FileReader()
+  //   FR.onloadend = e => {
+  //     callApi('images', 'post', { image: e.currentTarget.result, file: 'img.png' }).then(response => {
+  //       console.log('Success! ' + response)
+  //       onSuccess('ok')
+  //     },
+  //     error => {
+  //       // TODO Clear file list upon error
+  //       message.error('An error occured: ' + error.status + ' ' + error.statusText)
+  //       onError('fail')
+  //     })
+  //   }
+  //   FR.readAsBinaryString(file)
+  // }
 
   render () {
-    const up = (process.env.NODE_ENV !== 'test') &&
-      <Upload
-        fileList={this.state.fileList}
-        name='file'
-        onChange={this.onChangeImageUpload}
-        showUploadList
-        customRequest={this.uploadCustomRequest}
-        multiple={false}>
-        <Button
-          className='ant-upload-drag-button'
-          accessibilityLabel='Image upload button'>
-          <Icon type='upload' />Click to Upload!
-        </Button>
-      </Upload>
+    const up = (process.env.NODE_ENV !== 'test') && <DragDrop uppy={this.uppy} />
+    // <Upload
+    //   fileList={this.state.fileList}
+    //   name='file'
+    //   onChange={this.onChangeImageUpload}
+    //   showUploadList
+    //   customRequest={this.uploadCustomRequest}
+    //   multiple={false}>
+    //   <Button
+    //     className='ant-upload-drag-button'
+    //     accessibilityLabel='Image upload button'>
+    //     <Icon type='upload' />Click to Upload!
+    //   </Button>
+    // </Upload>
     return up
   }
 }
