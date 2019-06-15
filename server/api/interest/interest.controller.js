aba71f66aa7b3592fce8e31a3e7593014440b0ab
Minh-Phu <Lminhphu0304@gmail.com>
2019-06-15 12:10:12 +1200

    Add new email template for different status
    The email template still need some edit before it can be sent to actual user

--- server/api/interest/interest.controller.js
+++ server/api/interest/interest.controller.js
@@ -1,6 +1,6 @@
 const Interest = require('./interest')
 const Person = require('../person/person')
-// const Opportunity = require('../opportunity/opportunity')
+const Opportunity = require('../opportunity/opportunity')
 const { emailPerson } = require('../person/email/emailperson')
 
 /**
@@ -36,6 +36,13 @@ const updateInterest = async (req, res) => {
   try {
     await Interest.update({ _id: req.body._id }, { $set: { status: req.body.status } }).exec()
 
+    const { opportunity, status, person } = req.body // person in here is the volunteer-- quite not good naming here
+    Opportunity.findById(opportunity, (err, opportunityFound) => {
+      if (err) console.log(err)
+      else {
+        processStatusToSendEmail(status, opportunityFound, person)
+      }
+    })
     res.json(req.body)
   } catch (err) {
     res.status(404).send(err)
@@ -57,9 +64,13 @@ const createInterest = async (req, res) => {
     if (process.env.NODE_ENV !== 'test') {
       const { comment } = req.body
       requestor.volunteerComment = comment
-      Person.findById(interesetPersonID, (err, person) => {
+      Person.findById(interesetPersonID, (err, volunteer) => {
         if (err) console.log(err)
-        else sendEmailNotification(requestor, person, title) // This method will send to both requestor and person
+        else {
+          requestor.emailTemplate = 'RequestorNotificationEmail'
+          volunteer.emailTemplate = 'acknowledgeInterest'
+          sendEmailInformBoth(requestor, volunteer, title) // This method will send to both requestor and person
+        }
       })
     }
 
@@ -68,18 +79,64 @@ const createInterest = async (req, res) => {
   })
 }
 
-const sendEmailNotification = (requestor, volunteer, volunteerEvent) => {
+/**
+ * Quite different from the processStatusToSendEmail this function will send email to both requestor and volunteer inform a new interest created
+ * @param {*} requestor
+ * @param {*} volunteer
+ * @param {*} volunteerEvent The title of it
+ */
+const sendEmailInformBoth = (requestor, volunteer, volunteerEvent) => {
   requestor.volunteerEvent = volunteerEvent
   requestor.volunteerName = volunteer.nickname
   volunteer.volunteerEvent = volunteerEvent
-
   const emailProps = {
     send: true
   }
 
-  emailPerson(volunteer, 'acknowledgeInterest', emailProps)
-  emailPerson(requestor, 'RequestorNotificationEmail', emailProps)
+  emailPerson(volunteer, volunteer.emailTemplate, emailProps)
+  emailPerson(requestor, requestor.emailTemplate, emailProps)
 }
+
+const processStatusToSendEmail = (interestStatus, opportunity, volunteer) => {
+  const { _id, nickname } = volunteer
+  const { requestor, title } = opportunity
+  if (interestStatus === 'invited' || interestStatus === 'declined') {
+    // send email to volunteer
+    sendEmailBaseOn(interestStatus, _id, title)
+  } else if (interestStatus === 'committed') {
+    // send email to requestor
+    sendEmailBaseOn(interestStatus, requestor, title, nickname)
+  }
+}
+
+/**
+ * This will be easier to add more status without having too much if. All we need is add another folder in email template folder and the status will reference to that folder
+ * @param {*} status status will be used to indicate which email template to use
+ * @param {*} personID so we can find the email of that person
+ * @param {*} opportunityTitle just making the email content clearer
+ * @param {*} volunteerNickname optional this only be used to inform requestor when volunteer is commited
+ */
+const sendEmailBaseOn = (status, personID, opportunityTitle, volunteerNickname = '') => {
+  console.log('The status is ', status)
+  Person.findById(personID, (err, person) => {
+    if (err) console.log(err)
+    else {
+      const emailProps = {
+        send: true
+      }
+      person.email = 'emma.lockman76@ethereal.email'
+      person.volunteerEvent = opportunityTitle
+      person.volunteerNickname = volunteerNickname
+      emailPerson(person, status, emailProps)
+    }
+  })
+}
+
+// Possible states of the opportunity
+// 1 ->Intertested
+// 2 ->Invited
+// 3 ->Declined
+// 4 ->Commited
 
 // async function maybeInnovativelyDestructivelySendEmailPossibly (volunteerId, organizerId, prevStatus, currentStatus, modifier) {
 //   if (modifier == 'volunteer') {
