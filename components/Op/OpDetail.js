5279724f4851a94436ebd3bb959a2822ee310d1e
Darcy Cox <darcycox97@gmail.com>
2019-06-19 13:30:41 +1200

    Include whole tag object in opp response

--- components/Op/OpDetail.js
+++ components/Op/OpDetail.js
@@ -49,7 +49,7 @@ export function OpDetail ({ op }) {
           <ItemListing>⏱&nbsp;{op.duration}</ItemListing>
           <ItemListing>🏫&nbsp;{op.location}</ItemListing>
           <ItemListing>📝&nbsp;{op.status}</ItemListing>
-          <ItemListing>🔖&nbsp;{op.tags}</ItemListing>
+          <ItemListing>🔖&nbsp;//TODO</ItemListing>
 
           <Spacer />
 
