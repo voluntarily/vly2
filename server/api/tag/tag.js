723aa49b7ff8b128b05dfcbf9b66588b1b11218e
Darcy Cox <darcycox97@gmail.com>
2019-06-19 14:14:23 +1200

    Utilise mongoose validation for tag schema

--- server/api/tag/tag.js
+++ server/api/tag/tag.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 // Enforce unique constraint on entries in tag table, case-insensitive
 const tagSchema = new Schema({
-  tag: String // "e.g C++"
+  tag: { type: String, lowercase: true, unique: true, required: true } // "e.g C++"
 })
 
 module.exports = mongoose.model('Tag', tagSchema)
