67b747523a2d5f9200b4463412b1b04d2b91654d
Michael Chinn <michael.j.chinn@gmail.com>
2019-06-24 20:06:08 +1200

    Add ability based on authenticated user to request

--- server/api/person/person.js
+++ server/api/person/person.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const { Role } = require('../../services/auth/role')
 
 // simplified version without Auth
 const personSchema = new Schema({
@@ -15,8 +16,8 @@ const personSchema = new Schema({
   role: {
     type: [String],
     required: true,
-    default: ['volunteer'],
-    enum: ['admin', 'orgAdmin', 'opportunityProvider', 'volunteer', 'activityProvider', 'resourceProvider', 'tester']
+    default: [Role.VOLUNTEER_PROVIDER],
+    enum: [Role.ADMIN, Role.ORG_ADMIN, Role.OPPORTUNITY_PROVIDER, Role.VOLUNTEER_PROVIDER, Role.ACTIVITY_PROVIDER, Role.RESOURCE_PROVIDER, Role.TESTER]
   },
   // used to indicate whether people show up in searches.
   status: {
---
+++ server/services/auth/role.js
@@ -0,0 +1,15 @@
+const Role = {
+  ANON: 'anonymous',
+  ALL: 'all',
+  VOLUNTEER_PROVIDER: 'volunteer',
+  OPPORTUNITY_PROVIDER: 'opportunityProvider',
+  ACTIVITY_PROVIDER: 'activityProvider',
+  RESOURCE_PROVIDER: 'resourceProvider',
+  TESTER: 'tester',
+  ADMIN: 'admin',
+  ORG_ADMIN: 'orgAdmin'
+}
+
+module.exports = {
+  Role
+}
