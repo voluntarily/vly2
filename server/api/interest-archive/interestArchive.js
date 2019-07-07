ffc935e71c2503a5c3fcabc70ea9a08149ea8ebf
Aidan Harris <aidanharris1001@gmail.com>
2019-07-07 16:19:38 +1200

    [VP-287] Added interestArchive model

---
+++ server/api/interest-archive/interestArchive.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Interest = require('../interest/interest')
+
+const interestArchiveSchema = new Schema({
+  ...Interest.schema.obj,
+  opportunity: { type: Schema.Types.ObjectId, ref: 'OpportunityArchive', required: true },
+})
+
+module.exports = mongoose.model('InterestArchive', interestArchiveSchema)
