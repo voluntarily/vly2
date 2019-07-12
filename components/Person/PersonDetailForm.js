45966a371cbe1886e3ad099fa20f9b8534cc3531
Walter Lim <waltissomewhere@gmail.com>
2019-07-12 17:31:07 +1200

    profile work

--- components/Person/PersonDetailForm.js
+++ components/Person/PersonDetailForm.js
@@ -4,6 +4,16 @@ import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl'
 import RichTextEditor from '../Editor/RichTextEditor'
 import ImageUpload from '../UploadComponent/ImageUploadComponent'
+import { TextHeadingBold, TextP } from '../VTheme/VTheme'
+import {
+  DescriptionContainer,
+  FormGrid,
+  InputContainer,
+  MediumInputContainer,
+  ShortInputContainer,
+  TitleContainer
+} from '../VTheme/FormStyles'
+import PageTitle from '../LandingPageComponents/PageTitle'
 const { TextArea } = Input
 
 // TODO - only the owner and admins should be able to edit the person record.
@@ -25,11 +35,11 @@ class PersonDetailForm extends Component {
     this.props.form.setFieldsValue({ about: value })
   }
 
-  setImgUrl = (value) => {
+  setImgUrl = value => {
     this.props.form.setFieldsValue({ avatar: value })
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
@@ -50,17 +60,74 @@ class PersonDetailForm extends Component {
 
   render () {
     // get translated labels
-    const personName = (<FormattedMessage id='personName' defaultMessage='Full Name' description='person full name label in PersonDetails Form' />)
-    const personnickname = (<FormattedMessage id='personnickname' defaultMessage='Nickname' description='person nickname label in personDetails Form' />)
-    const personEmail = (<FormattedMessage id='personEmail' defaultMessage='Email' description='person email label in personDetails Form' />)
-    const personPhone = (<FormattedMessage id='personPhone' defaultMessage='Phone' description='person phone label in personDetails Form' />)
-    const personAbout = (<FormattedMessage id='personAbout' defaultMessage='About you' description='person about label in personDetails Form' />)
-    const personAvatar = (<FormattedMessage id='personAvatar' defaultMessage='Image Link' description='person Image URL label in personDetails Form' />)
-    const personGender = (<FormattedMessage id='personGender' defaultMessage='Gender' description='person gender label in personDetails Form' />)
-    const personRole = (<FormattedMessage id='personRole' defaultMessage='Role' description='person Role label in personDetails page' />)
-    const personStatus = (<FormattedMessage id='personStatus' defaultMessage='Availability' description='active or retired status' />)
+    const personName = (
+      <FormattedMessage
+        id='personName'
+        defaultMessage='Full Name'
+        description='person full name label in PersonDetails Form'
+      />
+    )
+    const personnickname = (
+      <FormattedMessage
+        id='personnickname'
+        defaultMessage='Nickname'
+        description='person nickname label in personDetails Form'
+      />
+    )
+    const personEmail = (
+      <FormattedMessage
+        id='personEmail'
+        defaultMessage='Email'
+        description='person email label in personDetails Form'
+      />
+    )
+    const personPhone = (
+      <FormattedMessage
+        id='personPhone'
+        defaultMessage='Phone'
+        description='person phone label in personDetails Form'
+      />
+    )
+    const personAbout = (
+      <FormattedMessage
+        id='personAbout'
+        defaultMessage='About you'
+        description='person about label in personDetails Form'
+      />
+    )
+    const personAvatar = (
+      <FormattedMessage
+        id='personAvatar'
+        defaultMessage='Image Link'
+        description='person Image URL label in personDetails Form'
+      />
+    )
+    const personGender = (
+      <FormattedMessage
+        id='personGender'
+        defaultMessage='Gender'
+        description='person gender label in personDetails Form'
+      />
+    )
+    const personRole = (
+      <FormattedMessage
+        id='personRole'
+        defaultMessage='Role'
+        description='person Role label in personDetails page'
+      />
+    )
+    const personStatus = (
+      <FormattedMessage
+        id='personStatus'
+        defaultMessage='Availability'
+        description='active or retired status'
+      />
+    )
     const {
-      getFieldDecorator, getFieldsError, getFieldError, isFieldTouched
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched
     } = this.props.form
 
     const roleOptions = [
@@ -89,34 +156,26 @@ class PersonDetailForm extends Component {
 
     return (
       <div className='PersonDetailForm'>
-        <Form
-          {...formItemLayout}
-          onSubmit={this.handleSubmit}
-          hideRequiredMark
-          colon={false}
-        >
-          <Row>
-            <Col
-              xs={{ span: 24 }}
-              md={{ span: 8 }}
-            >
-              <h2>1. Basic Contact</h2>
-              <p>How do we get in touch?
-              </p>
-            </Col>
-            <Col
-              xs={{ span: 24 }}
-              md={{ span: 16 }}
-            >
+        <PageTitle>
+          <h2>Edit Your Profile</h2>
+        </PageTitle>
+
+        <Form onSubmit={this.handleSubmit} hideRequiredMark colon={false}>
+          <FormGrid>
+            <DescriptionContainer>
+              <TitleContainer>
+                <TextHeadingBold>About You</TextHeadingBold>
+              </TitleContainer>
+              <TextP>How do we get in touch?</TextP>
+            </DescriptionContainer>
+            <InputContainer>
               <Form.Item
                 label={personName}
                 validateStatus={nameError ? 'error' : ''}
                 help={nameError || ''}
               >
                 {getFieldDecorator('name', {
-                  rules: [
-                    { required: true, message: 'Name is required' }
-                  ]
+                  rules: [{ required: true, message: 'Name is required' }]
                 })(
                   <Input placeholder='e.g. Salvador Felipe Jacinto Dalí y Domenech.' />
                 )}
@@ -124,86 +183,69 @@ class PersonDetailForm extends Component {
               <Form.Item label={personnickname}>
                 {getFieldDecorator('nickname', {
                   rules: []
-                })(
-                  <Input placeholder='e.g Dali' />
-                )}
+                })(<Input placeholder='e.g Dali' />)}
               </Form.Item>
               <Form.Item label={personEmail}>
                 {getFieldDecorator('email', {
                   rules: []
-                })(
-                  <Input placeholder='salvador@dali.com' />
-                )}
+                })(<Input placeholder='salvador@dali.com' />)}
               </Form.Item>
               <Form.Item label={personPhone}>
                 {getFieldDecorator('phone', {
                   rules: []
-                })(
-                  <Input placeholder='000 000 0000' />
-                )}
+                })(<Input placeholder='000 000 0000' />)}
               </Form.Item>
               <Form.Item label={personAbout}>
                 {getFieldDecorator('about', {
-                  rules: [
-
-                  ]
+                  rules: []
                 })(
-                  isTest
-                    ? <TextArea rows={20} placeholder='You can use markdown here.' />
-                    : <RichTextEditor onChange={this.setAbout} />
-
+                  isTest ? (
+                    <TextArea
+                      rows={20}
+                      placeholder='You can use markdown here.'
+                    />
+                  ) : (
+                    <RichTextEditor onChange={this.setAbout} />
+                  )
                 )}
               </Form.Item>
               <Form.Item label={personGender}>
                 {getFieldDecorator('gender', {
                   rules: []
-                })(
-                  <Input placeholder='write what you want here. ' />
-                )}
+                })(<Input placeholder='write what you want here. ' />)}
               </Form.Item>
-            </Col>
-          </Row>
-          <Divider />
+            </InputContainer>
+          </FormGrid>
 
-          <Row>
-            <Col
-              xs={{ span: 24 }}
-              md={{ span: 8 }}
-            >
-              <h2>Avatar (optional)</h2>
-              <p>Help people to recognise you or reflect your character.
-              </p>
-            </Col>
-            <Col
-              xs={{ span: 24 }}
-              md={{ span: 16 }}
-            >
+          <Divider />
+          <FormGrid>
+            <DescriptionContainer>
+              <TitleContainer>
+                <TextHeadingBold>Avatar (optional)</TextHeadingBold>
+              </TitleContainer>
+              <TextP>
+                Help people to recognise you or reflect your character.
+              </TextP>
+            </DescriptionContainer>
+            <InputContainer>
               <Form.Item label={personAvatar}>
                 {getFieldDecorator('avatar', {
                   rules: [
-                    {/* { type: 'url', message: 'a URL is required' } */}
+                    {
+                      /* { type: 'url', message: 'a URL is required' } */
+                    }
                   ]
-                })(
-                  <Input />
-                )}
+                })(<Input />)}
                 <ImageUpload setImgUrl={this.setImgUrl} />
               </Form.Item>
               <Form.Item label={personRole}>
                 {getFieldDecorator('role', {
-                  rules: [
-                    { required: true, message: 'role is required' }
-                  ]
-                })(
-                  <Checkbox.Group
-                    options={roleOptions}
-                  />
-                )}
+                  rules: [{ required: true, message: 'role is required' }]
+                })(<Checkbox.Group options={roleOptions} />)}
               </Form.Item>
               <Form.Item label={personStatus}>
                 {getFieldDecorator('status', {
-                  rules: [
-                    { required: true, message: 'status is required' }
-                  ]
+                  rules: [{ required: true, message: 'status is required' }]
                 })(
                   <Radio.Group buttonStyle='solid'>
                     <Radio.Button value='inactive'>Not Available</Radio.Button>
@@ -213,39 +255,42 @@ class PersonDetailForm extends Component {
                   </Radio.Group>
                 )}
               </Form.Item>
-            </Col>
-          </Row>
-          <Row>
-            <Col
-              style={{ textAlign: 'right' }}
-              xs={{ span: 24, offset: 0 }}
-              md={{ span: 8, offset: 12 }}
-            >
-              <Button
-                type='secondary'
-                htmlType='button'
-                onClick={this.props.onCancel}
+            </InputContainer>
+          </FormGrid>
+          <FormGrid>
+            
+            <Row>
+              <Col
+                style={{ textAlign: 'right' }}
+                xs={{ span: 24, offset: 0 }}
+                md={{ span: 8, offset: 12 }}
               >
-                <FormattedMessage
-                  id='cancel'
-                  defaultMessage='Cancel'
-                  description='Label for cancel button on person details form'
-                />
-              </Button>
-              <Button
-                type='primary'
-                htmlType='submit'
-                disabled={hasErrors(getFieldsError())}
-                style={{ marginLeft: 8 }}
-              >
-                <FormattedMessage
-                  id='savePerson'
-                  defaultMessage='Save'
-                  description='Label for submit button on person details form'
-                />
-              </Button>
-            </Col>
-          </Row>
+                <Button
+                  type='secondary'
+                  htmlType='button'
+                  onClick={this.props.onCancel}
+                >
+                  <FormattedMessage
+                    id='cancel'
+                    defaultMessage='Cancel'
+                    description='Label for cancel button on person details form'
+                  />
+                </Button>
+                <Button
+                  type='primary'
+                  htmlType='submit'
+                  disabled={hasErrors(getFieldsError())}
+                  style={{ marginLeft: 8 }}
+                >
+                  <FormattedMessage
+                    id='savePerson'
+                    defaultMessage='Save'
+                    description='Label for submit button on person details form'
+                  />
+                </Button>
+              </Col>
+            </Row>
+          </FormGrid>
         </Form>
       </div>
     )
@@ -262,7 +307,15 @@ PersonDetailForm.propTypes = {
     phone: PropTypes.string,
     gender: PropTypes.string,
     avatar: PropTypes.any,
-    role: PropTypes.arrayOf(PropTypes.oneOf(['admin', 'opportunityProvider', 'volunteer', 'activityProvider', 'tester'])),
+    role: PropTypes.arrayOf(
+      PropTypes.oneOf([
+        'admin',
+        'opportunityProvider',
+        'volunteer',
+        'activityProvider',
+        'tester'
+      ])
+    ),
     status: PropTypes.oneOf(['active', 'inactive', 'hold'])
   }),
   form: PropTypes.object,
@@ -282,15 +335,42 @@ export default Form.create({
   },
   mapPropsToFields (props) {
     return {
-      name: Form.createFormField({ ...props.person.name, value: props.person.name }),
-      nickname: Form.createFormField({ ...props.person.nickname, value: props.person.nickname }),
-      about: Form.createFormField({ ...props.person.about, value: props.person.about }),
-      email: Form.createFormField({ ...props.person.email, value: props.person.email }),
-      phone: Form.createFormField({ ...props.person.phone, value: props.person.phone }),
-      gender: Form.createFormField({ ...props.person.gender, value: props.person.gender }),
-      avatar: Form.createFormField({ ...props.person.avatar, value: props.person.avatar }),
-      role: Form.createFormField({ ...props.person.role, value: props.person.role }),
-      status: Form.createFormField({ ...props.person.status, value: props.person.status })
+      name: Form.createFormField({
+        ...props.person.name,
+        value: props.person.name
+      }),
+      nickname: Form.createFormField({
+        ...props.person.nickname,
+        value: props.person.nickname
+      }),
+      about: Form.createFormField({
+        ...props.person.about,
+        value: props.person.about
+      }),
+      email: Form.createFormField({
+        ...props.person.email,
+        value: props.person.email
+      }),
+      phone: Form.createFormField({
+        ...props.person.phone,
+        value: props.person.phone
+      }),
+      gender: Form.createFormField({
+        ...props.person.gender,
+        value: props.person.gender
+      }),
+      avatar: Form.createFormField({
+        ...props.person.avatar,
+        value: props.person.avatar
+      }),
+      role: Form.createFormField({
+        ...props.person.role,
+        value: props.person.role
+      }),
+      status: Form.createFormField({
+        ...props.person.status,
+        value: props.person.status
+      })
     }
   },
   onValuesChange (_, values) {
