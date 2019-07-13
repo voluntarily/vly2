e26c4d4c91a55eb9bdc6d1aed46fd8d1d2fe70f2
Andrew Watkins <andrew@groat.nz>
2019-07-13 15:26:13 +1200

    demonstrate small change

--- components/examples/Hello.js
+++ components/examples/Hello.js
@@ -1,6 +1,6 @@
 // React Components can be very very terse
 export const Greet = () => <p className='greeting' >Hello, *Your name here*!</p>
 
-export const Hello = ({ name }) => <p className='greeting' >Hello, {name}</p>
+export const Hello = ({ name, surname }) => <p className='greeting' >Hello, {name} {surname}</p>
 
 export default Greet
--- components/examples/__tests__/Hello.spec.js
+++ components/examples/__tests__/Hello.spec.js
@@ -5,12 +5,12 @@ import { mount } from 'enzyme'
 test('Hello renders properly', t => {
   const wrapper = mount(<Hello />)
   t.truthy(wrapper.find('p').first().hasClass('greeting'))
-  t.is(wrapper.find('p').first().text(), 'Hello, ')
+  t.is(wrapper.find('p').first().text(), 'Hello,  ')
 })
 
 test('Hello Andrew renders properly', t => {
-  const wrapper = mount(<Hello name='Andrew' />)
-  t.is(wrapper.find('p').first().text(), 'Hello, Andrew')
+  const wrapper = mount(<Hello name='Andrew' surname='Watkins' />)
+  t.is(wrapper.find('p').first().text(), 'Hello, Andrew Watkins')
 })
 
 test('Greet renders properly', t => {
