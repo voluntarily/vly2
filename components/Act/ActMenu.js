68c7a9d8521820f13208b064f364e16d25c7332d
Alisa Herrera-Hayman <alisamhh@gmail.com>
2019-07-14 11:59:07 +1200

    Dynamically populating tag subheadings based on hardcoded tag object.

--- components/Act/ActMenu.js
+++ components/Act/ActMenu.js
@@ -1,59 +1,55 @@
 /* Display a grid of opanisation cards from an [op]
  */
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { Menu } from 'antd';
 
-const ActivityNavHeading = styled.div`
+const TagMenu = styled(Menu)`
   height: 32px;
   width: 240px;
   font-family: Inter;
   font-size: 24px;
   letter-spacing: -0.3px;
   line-height: 48px;
-`
-const ActivityNavSubheading = styled.div`
-  color: #653CAD;
-  font-size: 16px;
-  font-weight: bold;
-  letter-spacing: -0.2px;
-  line-height: 48px;
+
+  .subheading {
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: -0.2px;
+  }
+
+  .color {
+    color: #653CAD;
+  }
 `
 
-const ActMenu = ({ acts, ...props }) => (
-  <ActivityNavHeading>
-    <Menu
-    style={{ width: 256 }}
-    mode="inline"
-    >
-      <Menu.ItemGroup key="g1" title="Discover">
-        <ActivityNavSubheading>
-          <Menu.Item key="1">Top rated</Menu.Item>
-          <Menu.Item key="2">Top trending</Menu.Item>
-        </ActivityNavSubheading>
-      </Menu.ItemGroup>
-      <Menu.ItemGroup key="g2" title="Categories">
-        <ActivityNavSubheading>
-          <Menu.Item key="3">Science</Menu.Item>
-          <Menu.Item key="4">Programming</Menu.Item>
-          <Menu.Item key="5">Geology</Menu.Item>
-        </ActivityNavSubheading>
-      </Menu.ItemGroup>
-    </Menu>
-  </ActivityNavHeading>
-)
 
-ActMenu.practTypes = {
-  acts: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      subtitle: PropTypes.string,
-      imgUrl: PropTypes.any,
-      duration: PropTypes.string
-    })
-  ) // optional
+
+const ActMenu = ({ acts, ...props }) => {
+  const tagCounts = {
+    Science: 91312,
+    Programming: 1312,
+    Geology: 1312,
+    Ballet: 1239
+  }
+  const tags = Object.keys(tagCounts)
+  return (
+    <TagMenu>
+      <Menu
+      style={{ width: 256 }}
+      mode="inline"
+      >
+        <Menu.ItemGroup key="g1">Discover</Menu.ItemGroup>
+        <Menu.ItemGroup key="10" className='subheading color'>Top rated (132)</Menu.ItemGroup>
+        <Menu.ItemGroup key="20" className='subheading color'>Top trending (132)</Menu.ItemGroup>
+      
+        <Menu.ItemGroup key="g2">Categories</Menu.ItemGroup>
+        {tags.map((tag, i) => <Menu.ItemGroup key={i} className='subheading color'>{tag} {tagCounts[tag]}</Menu.ItemGroup>)}
+        <Menu.ItemGroup key="6" className='color'>More filters</Menu.ItemGroup>
+      </Menu>
+    </TagMenu>
+  )
 }
 
 export default ActMenu
