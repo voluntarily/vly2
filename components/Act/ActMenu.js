31bf0000c517254acb0789b026deb1732f40bea8
Alisa Herrera-Hayman <alisamhh@gmail.com>
2019-07-13 16:47:25 +1200

    Added font styling to nav

--- components/Act/ActMenu.js
+++ components/Act/ActMenu.js
@@ -2,24 +2,48 @@
  */
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
 import { Menu } from 'antd';
 
+const ActivityNavHeading = styled.div`
+  height: 32px;
+  width: 240px;
+  font-family: Inter;
+  font-size: 24px;
+  letter-spacing: -0.3px;
+  line-height: 48px;
+`
+const ActivityNavSubheading = styled.div`
+  color: #653CAD;
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: -0.2px;
+  line-height: 48px;
+`
+
 const ActMenu = ({ acts, ...props }) => (
-    <div>
+  <div>
+    <ActivityNavHeading>
       <Menu
       style={{ width: 256 }}
       mode="inline"
       >
         <Menu.ItemGroup key="g1" title="Discover">
-          <Menu.Item key="1">Top rated</Menu.Item>
-          <Menu.Item key="2">Top trending</Menu.Item>
+          <ActivityNavSubheading>
+            <Menu.Item key="1">Top rated</Menu.Item>
+            <Menu.Item key="2">Top trending</Menu.Item>
+          </ActivityNavSubheading>
         </Menu.ItemGroup>
         <Menu.ItemGroup key="g2" title="Categories">
-          <Menu.Item key="3">Science</Menu.Item>
-          <Menu.Item key="4">Programming</Menu.Item>
-          <Menu.Item key="5">Geology</Menu.Item>
+          <ActivityNavSubheading>
+            <Menu.Item key="3">Science</Menu.Item>
+            <Menu.Item key="4">Programming</Menu.Item>
+            <Menu.Item key="5">Geology</Menu.Item>
+          </ActivityNavSubheading>
         </Menu.ItemGroup>
-    </Menu>
+      </Menu>
+    </ActivityNavHeading>
   </div>
 )
 
