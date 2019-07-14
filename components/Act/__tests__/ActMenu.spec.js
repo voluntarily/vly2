d198e6c9381170250ba02dac070d3516347ca0ba
Alisa Herrera-Hayman <alisamhh@gmail.com>
2019-07-14 21:09:50 +1200

    Test in progress

--- components/Act/__tests__/ActMenu.spec.js
+++ components/Act/__tests__/ActMenu.spec.js
@@ -4,21 +4,9 @@ import { renderWithIntl } from '../../../lib/react-intl-test-helper'
 
 import ActMenu from '../ActMenu'
 
-// Initialise activity menu
-const actMenu = {
-  'Discover': {
-    'Top rated': 132,
-    'Top trending': 150
-  },
-  'Categories': {
-    'Science': 91312,
-    'Programming': 1312,
-    'Geology': 1312,
-    'Ballet': 1239
-  }
-}
+// TO DO: Fix test as it is currently a false positive. 
 
 test('render the activity menu headers', t => {
-  const wrapper = renderWithIntl(<Menu.ItemGroup>{Object.keys(actMenu[0])}</Menu.ItemGroup>)
+  const wrapper = renderWithIntl(<ActMenu />)
   t.truthy(wrapper.find('Discover'))
 })
