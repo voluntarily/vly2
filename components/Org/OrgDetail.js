1bdc2a5957bf5f8645a36cadca81385137fd1a45
alisayunusova <alisa.yunusova@westpac.co.nz>
2019-07-26 16:25:11 +1200

    VP-399 Use organisation website instead of a placeholder in org sidebar

--- components/Org/OrgDetail.js
+++ components/Org/OrgDetail.js
@@ -93,7 +93,7 @@ const OrgDetail = ({ org, ...props }) => (
         />
         <OrgContainer>
           <TextPBold>Get in touch</TextPBold>
-          <ContactContainer><Icon type='global' />&nbsp;&nbsp;placeholderwebsite.co.nz</ContactContainer>
+          {org.website && <ContactContainer><Icon type='global' />&nbsp;&nbsp;{org.website}</ContactContainer>}
           <ContactContainer><Icon type='mail' />&nbsp;&nbsp;placeholder@email.co.nz</ContactContainer>
           <ContactContainer><Icon type='facebook' />&nbsp;&nbsp;placeholder</ContactContainer>
           <ContactContainer><Icon type='twitter' />&nbsp;@placeholderTwitter</ContactContainer>
@@ -133,6 +133,7 @@ OrgDetail.propTypes = {
       PropTypes.oneOf(['admin', 'op', 'vp', 'ap', 'other'])
     ).isRequired,
     imgUrl: PropTypes.string,
+    website: PropTypes.string,
     _id: PropTypes.string.isRequired
   }).isRequired
 }
