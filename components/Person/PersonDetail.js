5fde133970a2ad3ee0555cfc1db641b284436eb4
Walter Lim <waltissomewhere@gmail.com>
2019-07-31 14:33:32 +1200

    lint and test fix

--- components/Person/PersonDetail.js
+++ components/Person/PersonDetail.js
@@ -1,4 +1,4 @@
-import { Icon, Divider } from 'antd'
+import { Divider, Icon } from 'antd'
 import Markdown from 'markdown-to-jsx'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
@@ -10,13 +10,19 @@ import {
   TextPBold,
   TextH1,
   TextH3,
-  TextSubtitle
+  SpacerSmall,
+  Spacer
 } from '../VTheme/VTheme'
 
 const ProfileGrid = styled.div`
   display: grid;
   grid-template-columns: 16rem 1fr;
   gap: 5rem;
+
+  @media screen and (min-width: 768px) and (max-width: 1280px) {
+    grid-template-columns: 16rem 1fr;
+    gap: 5rem;
+  }
 
   @media screen and (max-width: 767px) {
     grid-template-columns: calc(100vw - 2rem);
@@ -26,8 +32,6 @@ const ProfileGrid = styled.div`
     /* iPhone X */
     grid-template-columns: calc(100vw - 2rem);
   }
-
-
 `
 
 const ProfileImage = styled.img`
@@ -37,6 +41,19 @@ const ProfileImage = styled.img`
 const DetailItem = styled.div`
   margin-top: 0.5rem;
   margin-bottom: 0.5rem;
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+`
+const DetailItemMobile = styled.div`
+  display: none;
+
+  @media screen and (max-width: 767px) {
+    display: initial;
+    margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  position: relative;
+  }
 `
 
 const ListItem = styled.div`
@@ -46,59 +63,69 @@ const ListItem = styled.div`
 `
 
 const PersonDetail = ({ person }, ...props) => (
-  <div>
-    <Head title={person.nickname} />
-    <ProfileGrid>
-      <GridContainer>
-        <ProfileImage src={person.avatar} alt={person.nickname} />
-        <DetailItem>
-              <Icon type='history' /> 312 ops completed
-          </DetailItem>
-          <DetailItem>
-              <Icon type='safety' /> School Safe
-          </DetailItem>
-      </GridContainer>
-      <GridContainer>
-        <ListItem>
-          <TextH1>
-            {person.name}&nbsp;&nbsp;
-            {person.status ? <Icon type='safety-certificate' theme='filled' style={{ color: '#1da1f2' }} /> : <Icon type='close-circle' />}
-          </TextH1>
-          <TextPBold>{person.org}</TextPBold>
-          <Divider />
-          <TextSubtitle>
-            <Markdown children={person.about || ''} />
-          </TextSubtitle>
+  <ProfileGrid>
+    <GridContainer>
+      <Head title={person.nickname} />
+      <ProfileImage src={person.avatar} alt={person.nickname} />
+      <DetailItem>
+        <Icon type='history' /> 312 ops completed
+      </DetailItem>
+      <DetailItem>
+        <Icon type='safety' /> School Safe
+      </DetailItem>
+    </GridContainer>
+    <GridContainer>
+      <ListItem>
+        <TextH1>
+          {person.name}
+          {person.status ? (
+            <Icon
+              type='safety-certificate'
+              theme='filled'
+              style={{ color: '#1da1f2' }}
+            />
+          ) : (
+            <Icon type='close-circle' />
+          )}
+        </TextH1>
+        <TextPBold>{person.org}</TextPBold>
+        <Divider />
+        <Markdown children={person.about || ''} />
 
-          <DetailItem>
-            <TextSubtitle>
-            <a href={`mailto:${person.email}`}>
-              <Icon type='mail' /> {person.email}
-            </a>
-            </TextSubtitle>
-          </DetailItem>
-          <DetailItem>
-            <TextSubtitle>
-            <a href={`tel:${person.phone}`}>
-              <Icon type='phone' /> {person.phone}
-            </a>
-            </TextSubtitle>
-          </DetailItem>
-        </ListItem>
-        <ListItem>
-          <TextH3>Latest Activities</TextH3>
-          <Divider />
-        </ListItem>
-        <ListItem>
-          <TextH3>Latest Achievements</TextH3>
-          <Divider />
-        </ListItem>
-        <div>
-          <Icon type='coffee' /> <PersonRoles roles={person.role} />
-        </div>
-      </GridContainer>
-    </ProfileGrid>
-  </div>
+        <TextPBold>
+          <a href={`mailto:${person.email}`}>
+            <Icon type='mail' /> {person.email}
+          </a>
+        </TextPBold>
+        <SpacerSmall />
+
+        <TextPBold>
+          <a href={`tel:${person.phone}`}>
+            <Icon type='phone' /> {person.phone}
+          </a>
+        </TextPBold>
+
+      </ListItem>
+      <DetailItemMobile>
+        <p>        <Icon type='history' /> 312 ops completed</p>
+      </DetailItemMobile>
+      <DetailItemMobile>
+        <p> <Icon type='safety' /> School Safe</p>
+      </DetailItemMobile>
+      <Spacer />
+      <ListItem>
+        <TextH3>Latest Activities</TextH3>
+        <Divider />
+      </ListItem>
+      <ListItem>
+        <TextH3>Latest Achievements</TextH3>
+        <Divider />
+      </ListItem>
+      <div>
+        <Icon type='coffee' /> <PersonRoles roles={person.role} />
+      </div>
+    </GridContainer>
+  </ProfileGrid>
 )
 
 PersonDetail.propTypes = {
