524298cebaa960a1c9ab54426b961471098ecc79
Cyrus <cyrus.raitava@gmail.com>
2019-08-04 13:39:06 +1200

    Introduce location component into Person pages, and forms

--- pages/person/persondetailpage.js
+++ pages/person/persondetailpage.js
@@ -17,6 +17,7 @@ const blankPerson = {
   name: '',
   nickname: '',
   about: '',
+  location: '',
   email: '',
   phone: '',
   gender: '',
@@ -32,6 +33,7 @@ export class PersonDetailPage extends Component {
   static async getInitialProps ({ store, query, req }) {
     // Get one Org
     const isNew = query && query.new && query.new === 'new'
+    await store.dispatch(reduxApi.actions.locations.get())
     if (isNew) {
       return {
         isNew: true,
@@ -161,6 +163,7 @@ PersonDetailPage.propTypes = {
     name: PropTypes.string,
     nickname: PropTypes.string,
     about: PropTypes.string,
+    location: PropTypes.string,
     email: PropTypes.string,
     phone: PropTypes.string,
     gender: PropTypes.string,
