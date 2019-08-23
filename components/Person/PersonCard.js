012f1ceb9de2a755cd5787c5644d282c93159b65
Eteroa Lafaele <eteroalafaele080@gmail.com>
2019-08-23 17:19:19 +1200

    Imported to the Link

--- components/Person/PersonCard.js
+++ components/Person/PersonCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import Link from 'next/link'
+import Link from 'next/link'
 
 const PersonCard = ({ person, ...props }) => (
   <div>
