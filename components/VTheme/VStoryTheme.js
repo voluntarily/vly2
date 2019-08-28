de5665773197748cb88ae3d68ef9d0e9547aa7ff
Walter Lim <waltissomewhere@gmail.com>
2019-08-28 14:30:31 +1200

    add intro and styling

---
+++ components/VTheme/VStoryTheme.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const StoryIntroContainer = styled.div`
+  margin: 4rem 4rem 0 4rem;
+  width: auto;
+`
+
+
+
+export const ColorGrid = styled.div`
+margin: 2rem 0 4rem 0;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+gap: 1rem;
+`
+
+export const ColorContainer = styled.div`
+
+`
+export const ColorSwatch = styled.div`
+height: 14.25rem;
+`
