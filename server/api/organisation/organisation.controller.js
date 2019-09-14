ed192f2301574140e0daacd893010609666b2554
Andrew Watkins <andrew@groat.nz>
2019-09-15 08:36:19 +1200

    added 404 test and removed search for now

--- server/api/organisation/organisation.controller.js
+++ server/api/organisation/organisation.controller.js
@@ -25,51 +25,52 @@ const getOrganisations = async (req, res) => {
     return res.status(400).send(e)
   }
 
+  // try {
+  // TODO: [VP-508] Add searching for orgs by category, name and tags
+  //   if (req.query.search) {
+  //     const search = req.query.search.trim()
+  //     const regexSearch = escapeRegex(search)
+
+  //     // split around one or more whitespace characters
+  //     const keywordArray = search.split(/\s+/)
+
+  //     // case insensitive regex which will find tags matching any of the array values
+  //     const tagSearchExpression = new RegExp(keywordArray.map(w => escapeRegex(w)).join('|'), 'i')
+
+  //     // find tag ids to include in the opportunity search
+  //     const matchingTagIds = await Tag.find({ 'tag': tagSearchExpression }, '_id').exec()
+  //     const searchExpression = new RegExp(regexSearch, 'i')
+  //     const searchParams = {
+  //       $or: [
+  //         { 'name': searchExpression },
+  //         { 'info': searchExpression },
+  //       ]
+  //     }
+
+  //     // mongoose isn't happy if we provide an empty array as an expression
+  //     if (matchingTagIds.length > 0) {
+  //       const tagIdExpression = {
+  //         $or: matchingTagIds.map(id => ({ 'tags': id }))
+  //       }
+  //       searchParams.$or.push(tagIdExpression)
+  //     }
+  //     query = {
+  //       $and: [
+  //         searchParams,
+  //         query
+  //       ]
+  //     }
+  //   }
+
   try {
-    if (req.query.search) {
-      const search = req.query.search.trim()
-      const regexSearch = escapeRegex(search)
-
-      // split around one or more whitespace characters
-      const keywordArray = search.split(/\s+/)
-
-      // case insensitive regex which will find tags matching any of the array values
-      const tagSearchExpression = new RegExp(keywordArray.map(w => escapeRegex(w)).join('|'), 'i')
-
-      // find tag ids to include in the opportunity search
-      const matchingTagIds = await Tag.find({ 'tag': tagSearchExpression }, '_id').exec()
-      const searchExpression = new RegExp(regexSearch, 'i')
-      const searchParams = {
-        $or: [
-          { 'name': searchExpression },
-          { 'slug': searchExpression }
-        ]
-      }
-
-      // mongoose isn't happy if we provide an empty array as an expression
-      if (matchingTagIds.length > 0) {
-        const tagIdExpression = {
-          $or: matchingTagIds.map(id => ({ 'tags': id }))
-        }
-        searchParams.$or.push(tagIdExpression)
-      }
-      query = {
-        $and: [
-          searchParams,
-          query
-        ]
-      }
-    }
-
-    try {
-      const got = await Organisation.find(query, select).sort(sort).exec()
-      res.json(got)
-    } catch (e) {
-      res.status(404).send(e)
-    }
+    const got = await Organisation.find(query, select).sort(sort).exec()
+    res.json(got)
   } catch (e) {
-    return res.status(500).send(e)
+    res.status(404).send(e)
   }
+  // } catch (e) {
+  //   return res.status(500).send(e)
+  // }
 }
 
 module.exports = {
