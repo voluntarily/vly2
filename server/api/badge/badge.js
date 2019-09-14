d58577ce1d318b592f2b2140777414bd50584241
Minh-Phu <Lminhphu0304@gmail.com>
2019-09-15 10:57:48 +1200

    Change Badge Mongo Schema

---
+++ server/api/badge/badge.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const idvalidator = require('mongoose-id-validator')
+const Badge = new Schema({
+  entityType: {
+    type: String
+  },
+  entityId: {
+    type: String,
+    required: true
+  },
+  person: {
+    type: Schema.Types.ObjectId, // Person who will receive this badge
+    ref: 'Person',
+    required: true
+  },
+  image: {
+    type: String,
+    defualt: ''
+  },
+  badgeclass: {
+    type: String,
+    required: true
+  },
+  issuer: {
+    type: String,
+    required: true
+  },
+  badgeclassOpenBadgeId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date
+  },
+  issuedOn: {
+    type: Date
+  },
+  issuerOpenBadgeId: {
+    type: String,
+    required: true
+  }
+})
+
+Badge.plugin(idvalidator)
+module.exports = mongoose.model('Badge', Badge)
