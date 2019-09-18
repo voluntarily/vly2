499cefe948f0a9377a5e8a95ba56f89ada3da484
Minh-Phu <Lminhphu0304@gmail.com>
2019-09-18 20:55:11 +1200

    Add unitest for issuing badge

--- server/api/badge/__tests__/badge.spec.js
+++ server/api/badge/__tests__/badge.spec.js
@@ -1,9 +1,35 @@
 import test from 'ava'
-require('isomorphic-fetch')
-import fetchMock from 'fetch-mock'
+import { fetchMock } from 'fetch-mock'
 import MockExpressRequest from 'mock-express-request'
 import MockExpressResponse from 'mock-express-response'
 import { issueNewBadge } from '../badge.controller'
+
+const expectedBadgeResult = {    
+  "entityType": "Assertion",
+  "entityId": "WDVAtNTfT_S8JuO-u8rVEw",
+  "openBadgeId": "https://api.badgr.io/public/assertions/WDVAtNTfT_S8JuO-u8rVEw",
+  "createdAt": "2019-09-12T04:33:37.129885Z",
+  "createdBy": "ZR_9B1v2S2OHJ9UoFIs5vw",
+  "badgeclass": "HhIer_mgTtahY6TRT22L5A",
+  "badgeclassOpenBadgeId": "https://api.badgr.io/public/badges/HhIer_mgTtahY6TRT22L5A",
+  "issuer": "aSBVfm84SMiF0c16O9jCOA",
+  "issuerOpenBadgeId": "https://api.badgr.io/public/issuers/aSBVfm84SMiF0c16O9jCOA",
+  "image": "https://media.badgr.io/uploads/badges/assertion-WDVAtNTfT_S8JuO-u8rVEw.png",
+  "recipient": {
+      "identity": "sha256$cc37fd25e8687b0c8adbd743f7e43997a0d5eb279b5eea11b7d5787f7b0f5842",
+      "hashed": true,
+      "type": "email",
+      "plaintextIdentity": "andrew@omgtech.co.nz",
+      "salt": "dc5af5ab2e8e456daaf80b5352741a82"
+  },
+  "issuedOn": "2019-09-12T04:33:36.761207Z",
+  "narrative": null,
+  "evidence": [],
+  "revoked": false,
+  "revocationReason": null,
+  "expires": null,
+  "extensions": {}
+}
 
 test.failing('Test if receive token from Badgr', async t => {
   const mockReq = new MockExpressRequest()
@@ -15,12 +41,22 @@ test.failing('Test if receive token from Badgr', async t => {
   t.assert(expectedStatusCode === actualStatusCode)
 })
 
-test.serial('Test request is not allowed to issue badge', async t => {
-  fetchMock.sandbox()
-    .mock('glob:https://api.badgr.io/**', 200)
-    .post('*', { access_token: 'DUMY' })
-  global.fetch = fetchMock
+test.serial('Test request issue badge return information about badge', async t => {
 
+  let mockFetch = fetchMock.sandbox()
+    .get('*',200)
+    .post('begin:https://api.badgr.io/o/token', { 
+      body: {
+        access_token: 'asodjhfsiuh'
+      }
+    })
+    .post('begin:https://api.badgr.io/v2/badgeclasses', {
+      body: {
+        result: expectedBadgeResult
+      }
+    })
+  global.fetch = mockFetch
+  
   const mockReq = new MockExpressRequest()
   const mockRes = new MockExpressResponse()
   mockReq.session = {
@@ -28,11 +64,16 @@ test.serial('Test request is not allowed to issue badge', async t => {
     me: {},
     user: {}
   }
+  mockReq.body ={ 
+    email: 'Testing@gmail.com',
+    _id: 'Anothertest'
+  }
   mockReq.params = {
     badgeID: 'Maklsjdfad'
   }
   await issueNewBadge(mockReq, mockRes)
-
-  const expectedStatusCode = 403
-  t.assert(expectedStatusCode === mockRes.statusCode, 'Status code should be 403 forbiden')
+  const responseData = mockRes._getJSON()
+  const expectedStatusCode = 200
+  t.assert(expectedStatusCode === mockRes.statusCode, 'Status code should be 200 OK')
+  t.deepEqual (responseData.result,expectedBadgeResult)
 })
