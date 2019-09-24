0b5c1a62b05237272a90331ba9bb60fd4091daa4
Walter Lim <waltissomewhere@gmail.com>
2019-09-24 18:05:18 +1200

    initial commit - fixed orgcard img cropping and added some title text for ops on orgpages

--- pages/org/orgdetailpage.js
+++ pages/org/orgdetailpage.js
@@ -8,7 +8,8 @@ import RegisterMemberSection from '../../components/Member/RegisterMemberSection
 import OpListSection from '../../components/Op/OpListSection'
 import OrgDetail from '../../components/Org/OrgDetail'
 import OrgDetailForm from '../../components/Org/OrgDetailForm'
-import { FullPage, Spacer } from '../../components/VTheme/VTheme'
+import { FullPage, Spacer, H3Bold, H3Black } from '../../components/VTheme/VTheme'
+import TitleSectionSub from '../../components/LandingPageComponents/TitleSectionSub'
 import publicPage from '../../hocs/publicPage'
 import reduxApi, { withOrgs } from '../../lib/redux/reduxApi.js'
 import { MemberStatus } from '../../server/api/member/member.constants'
@@ -24,6 +25,7 @@ const blankOrg = {
   twitter: null,
   category: ['vp']
 }
+
 class OrgDetailPage extends Component {
   state = {
     editing: false,
@@ -42,7 +44,9 @@ class OrgDetailPage extends Component {
       await store.dispatch(reduxApi.actions.organisations.get(query))
       if (store.getState().session.isAuthenticated) {
         const meid = store.getState().session.me._id
-        await store.dispatch(reduxApi.actions.members.get({ orgid: query.id, meid: meid }))
+        await store.dispatch(
+          reduxApi.actions.members.get({ orgid: query.id, meid: meid })
+        )
       }
       return {
         isNew: false,
@@ -59,7 +63,8 @@ class OrgDetailPage extends Component {
 
   handleCancel = () => {
     this.setState({ editing: false })
-    if (this.props.isNew) { // return to previous
+    if (this.props.isNew) {
+      // return to previous
       Router.back()
     }
   }
@@ -67,7 +72,9 @@ class OrgDetailPage extends Component {
   async handleDelete (org) {
     if (!org) return
     // Actual data request
-    await this.props.dispatch(reduxApi.actions.organisations.delete({ id: org._id }))
+    await this.props.dispatch(
+      reduxApi.actions.organisations.delete({ id: org._id })
+    )
     // TODO: error handling - how can this fail?
     message.success('Deleted. ')
     Router.replace(`/orgs`)
@@ -78,9 +85,16 @@ class OrgDetailPage extends Component {
     // Actual data request
     let res = {}
     if (org._id) {
-      res = await this.props.dispatch(reduxApi.actions.organisations.put({ id: org._id }, { body: JSON.stringify(org) }))
+      res = await this.props.dispatch(
+        reduxApi.actions.organisations.put(
+          { id: org._id },
+          { body: JSON.stringify(org) }
+        )
+      )
     } else {
-      res = await this.props.dispatch(reduxApi.actions.organisations.post({}, { body: JSON.stringify(org) }))
+      res = await this.props.dispatch(
+        reduxApi.actions.organisations.post({}, { body: JSON.stringify(org) })
+      )
       org = res[0]
       Router.replace(`/orgs/${org._id}`)
     }
@@ -88,13 +102,17 @@ class OrgDetailPage extends Component {
     message.success('Saved.')
   }
 
-  handleDeleteCancel = () => { message.error('Delete Cancelled') }
+  handleDeleteCancel = () => {
+    message.error('Delete Cancelled')
+  }
 
   render () {
     // TODO: [VP-274] identify if current person is an org Admin for this organisation
-    const isOrgAdmin = this.props.members.data.length && (this.props.members.data[0].status === MemberStatus.ORGADMIN)
-    const isAdmin = (this.props.me && this.props.me.role.includes('admin'))
-    const canEdit = (isOrgAdmin || isAdmin)
+    const isOrgAdmin =
+      this.props.members.data.length &&
+      this.props.members.data[0].status === MemberStatus.ORGADMIN
+    const isAdmin = this.props.me && this.props.me.role.includes('admin')
+    const canEdit = isOrgAdmin || isAdmin
     const canRemove = isAdmin
 
     let content = ''
@@ -111,49 +129,97 @@ class OrgDetailPage extends Component {
     }
 
     if (!org) {
-      content = <div>
-        <h2>Sorry this organisation is not available</h2>
-        <Button shape='round'>
-          <Link href='/orgs'><a>
-            <FormattedMessage id='showOrgs' defaultMessage='Show All' description='Button to show all organisations' />
-          </a></Link>
-        </Button>
-        {/* <Button shape='round'>
+      content = (
+        <div>
+          <h2>Sorry this organisation is not available</h2>
+          <Button shape='round'>
+            <Link href='/orgs'>
+              <a>
+                <FormattedMessage
+                  id='showOrgs'
+                  defaultMessage='Show All'
+                  description='Button to show all organisations'
+                />
+              </a>
+            </Link>
+          </Button>
+          {/* <Button shape='round'>
           <Link href='/org/new'><a>
             <FormattedMessage id='org.altnew' defaultMessage='New Organisation' description='Button to create a new organisation' />
           </a></Link>
         </Button> */}
-      </div>
-    } else {
-      content = this.state.editing
-        ? <div>
-          <OrgDetailForm org={org} onSubmit={this.handleSubmit.bind(this, org)} onCancel={this.handleCancel.bind(this)} />
         </div>
-        : <div>
+      )
+    } else {
+      content = this.state.editing ? (
+        <div>
+          <OrgDetailForm
+            org={org}
+            onSubmit={this.handleSubmit.bind(this, org)}
+            onCancel={this.handleCancel.bind(this)}
+          />
+        </div>
+      ) : (
+        <div>
           <OrgDetail org={org} meid={this.props.me._id} />
+          <Divider />
+          <H3Black>
+            <FormattedMessage
+              id='getInvolved'
+              defaultMessage='Get involved'
+              description='Header for org activities'
+            />
+          </H3Black>
+          <h5>Get involved with {org.name} opportunities in your community</h5>
           <OpListSection org={org._id} />
           <Spacer />
           <Divider />
           <div style={{ float: 'right' }}>
-            <Button shape='round'><Link href='/orgs'><a>
-              <FormattedMessage id='showOrgs' defaultMessage='Show All' description='Button to show all organisations' />
-            </a></Link></Button>
-            {canEdit && <Button type='primary' shape='round' onClick={() => this.setState({ editing: true })} >Edit</Button>}
-            {canRemove && <Popconfirm title='Confirm removal of this organisation.' onConfirm={this.handleDelete.bind(this, org)} onCancel={this.handleDeleteCancel.bind(this)} okText='Yes' cancelText='No'>
-              <Button style={{ float: 'right' }} type='danger' shape='round' >
-                <FormattedMessage id='deleteOrg' defaultMessage='Remove Organisation' description='Button to remove an Organisatino on OrgDetails page' />
+            <Button shape='round'>
+              <Link href='/orgs'>
+                <a>
+                  <FormattedMessage
+                    id='showOrgs'
+                    defaultMessage='Show All'
+                    description='Button to show all organisations'
+                  />
+                </a>
+              </Link>
+            </Button>
+            {canEdit && (
+              <Button
+                type='primary'
+                shape='round'
+                onClick={() => this.setState({ editing: true })}
+              >
+                Edit
               </Button>
-            </Popconfirm>}
-
+            )}
+            {canRemove && (
+              <Popconfirm
+                title='Confirm removal of this organisation.'
+                onConfirm={this.handleDelete.bind(this, org)}
+                onCancel={this.handleDeleteCancel.bind(this)}
+                okText='Yes'
+                cancelText='No'
+              >
+                <Button style={{ float: 'right' }} type='danger' shape='round'>
+                  <FormattedMessage
+                    id='deleteOrg'
+                    defaultMessage='Remove Organisation'
+                    description='Button to remove an Organisatino on OrgDetails page'
+                  />
+                </Button>
+              </Popconfirm>
+            )}
           </div>
-          {this.props.isAuthenticated && <RegisterMemberSection orgid={org._id} meid={this.props.me._id} />}
+          {this.props.isAuthenticated && (
+            <RegisterMemberSection orgid={org._id} meid={this.props.me._id} />
+          )}
         </div>
+      )
     }
-    return (
-      <FullPage>
-        {content}
-      </FullPage>
-    )
+    return <FullPage>{content}</FullPage>
   }
 }
 
