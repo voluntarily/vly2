93aa1d9666c864764f0283b567a1211781143049
Minh-Phu <Lminhphu0304@gmail.com>
2019-09-25 17:07:21 +1200

    Lint fix

--- components/Person/PersonBadge.js
+++ components/Person/PersonBadge.js
@@ -1,44 +1,44 @@
-  import React, { useEffect, useState } from 'react'
-  import apiCaller from '../../lib/apiCaller'
-  import styled from 'styled-components'
+import React, { useEffect, useState } from 'react'
+import apiCaller from '../../lib/apiCaller'
+import styled from 'styled-components'
 
-  const BadgeWrapper = styled.div`
+const BadgeWrapper = styled.div`
     margin-top: 10px;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
   `
 
-  const Badge = styled.div`
+const Badge = styled.div`
     margin: 10px;
   `
 
-  const getBadgeData = async ({ _id }, setUserBadge) => {
-    const data = await apiCaller(`badge/${_id}`)
-    setUserBadge(data)
-  }
+const getBadgeData = async ({ _id }, setUserBadge) => {
+  const data = await apiCaller(`badge/${_id}`)
+  setUserBadge(data)
+}
 
-  function PersonBadge ({ person }) {
-    const [userBadge, setUserBadge] = useState(null)
-    useEffect(() => {
-      getBadgeData(person, setUserBadge)
-    }, [person])
+function PersonBadge ({ person }) {
+  const [userBadge, setUserBadge] = useState(null)
+  useEffect(() => {
+    getBadgeData(person, setUserBadge)
+  }, [person])
 
-    // TODO: Replace badge._id with badge.name in <Badge title> and <img alt>
-    return userBadge ? (
-      <BadgeWrapper>
-          {
-          userBadge.map((badge) => {
-            return (
-              <Badge key={badge._id} title={badge._id}>
-                <a href={`https://api.badgr.io/public/assertions/${badge.entityId}`} target='_blank'>
-                  <img width='60px' height='60px' alt={`${badge._id} badge`} src={`${badge.badgeclassOpenBadgeId}/image?type=png`} />
-                </a>
-              </Badge>)
-          })
-        }
-      </BadgeWrapper>
-    ) : null
-  }
+  // TODO: Replace badge._id with badge.name in <Badge title> and <img alt>
+  return userBadge ? (
+    <BadgeWrapper>
+      {
+        userBadge.map((badge) => {
+          return (
+            <Badge key={badge._id} title={badge._id}>
+              <a href={`https://api.badgr.io/public/assertions/${badge.entityId}`} target='_blank'>
+                <img width='60px' height='60px' alt={`${badge._id} badge`} src={`${badge.badgeclassOpenBadgeId}/image?type=png`} />
+              </a>
+            </Badge>)
+        })
+      }
+    </BadgeWrapper>
+  ) : null
+}
 
-  export const PersonBadgeSection = PersonBadge
+export const PersonBadgeSection = PersonBadge
