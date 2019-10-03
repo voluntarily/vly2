825837727d0e5ecbaf1b1e75596877ea67c4a45f
Eteroa Lafaele <eteroalafaele080@gmail.com>
2019-10-03 16:07:05 +1300

    Removed the the termsinfo page and just added a terms.js in the term folder

--- pages/terms/term-en.js
+++ pages/terms/term-en.js
@@ -1,108 +1,144 @@
-import { Button } from 'antd'
-import Markdown from 'markdown-to-jsx'
-import React, { Component } from 'react'
-import { Helmet } from 'react-helmet'
-import styled from 'styled-components'
-import AboutCTA from '../../components/About/AboutCTA.js'
-import { FullPage, Spacer } from '../../components/VTheme/VTheme'
-import publicPage from '../../hocs/publicPage'
-import withIntl from '../../lib/withIntl'
-import aboutEn from './about-en-md.js'
-import aboutMi from './terminfo-en-md.js/index.js'
+/* eslint-disable max-len */
+export default () => `
+<h2>Welcome to Voluntarily</h2>
+	<p>These terms and conditions outline the rules and regulations for the use of Voluntarily's Website.</p> <br /> 
+	<span style="text-transform: capitalize;"> Voluntarily</span> is located at:<br /> 
+	<address>48 Emily Place Auckland CDB, Auckland <br />0101 - Auckland , New Zealand<br />
+	</address>
+	<p>By accessing this website we assume you accept these terms and conditions in full. Do not continue to use Voluntarily's website 
+	if you do not accept all of the terms and conditions stated on this page.</p>
+	<p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice
+	and any or all Agreements: "Client", "You" and "Your" refers to you, the person accessing this website
+	and accepting the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers
+	to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves, or either the Client
+	or ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake
+	the process of our assistance to the Client in the most appropriate manner, whether by formal meetings
+	of a fixed duration, or any other means, for the express purpose of meeting the Client's needs in respect
+	of provision of the Company's stated services/products, in accordance with and subject to, prevailing law
+	of New Zealand. Any use of the above terminology or other words in the singular, plural,
+	capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p><h2>Cookies</h2>
+	<p>We employ the use of cookies. By using Voluntarily's website you consent to the use of cookies 
+	in accordance with Voluntarily's privacy policy.</p><p>Most of the modern day interactive web sites
+	use cookies to enable us to retrieve user details for each visit. Cookies are used in some areas of our site
+	to enable the functionality of this area and ease of use for those people visiting. Some of our 
+	affiliate / advertising partners may also use cookies.</p><h2>License</h2>
+	<p>Unless otherwise stated, Voluntarily and/or it's licensors own the intellectual property rights for
+	all material on Voluntarily. All intellectual property rights are reserved. You may view and/or print
+	pages from https://voluntarily.nz/ for your own personal use subject to restrictions set in these terms and conditions.</p>
+	<p>You must not:</p>
+	<ol>
+		<li>Republish material from https://voluntarily.nz/</li>
+		<li>Sell, rent or sub-license material from https://voluntarily.nz/</li>
+		<li>Reproduce, duplicate or copy material from https://voluntarily.nz/</li>
+	</ol>
+	<p>Redistribute content from Voluntarily (unless content is specifically made for redistribution).</p>
+<h2>Hyperlinking to our Content</h2>
+	<ol>
+		<li>The following organizations may link to our Web site without prior written approval:
+			<ol>
+			<li>Government agencies;</li>
+			<li>Search engines;</li>
+			<li>News organizations;</li>
+			<li>Online directory distributors when they list us in the directory may link to our Web site in the same
+				manner as they hyperlink to the Web sites of other listed businesses; and</li>
+			<li>Systemwide Accredited Businesses except soliciting non-profit organizations, charity shopping malls,
+				and charity fundraising groups which may not hyperlink to our Web site.</li>
+			</ol>
+		</li>
+	</ol>
+	<ol start="2">
+		<li>These organizations may link to our home page, to publications or to other Web site information so long
+			as the link: (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or
+			approval of the linking party and its products or services; and (c) fits within the context of the linking
+			party's site.
+		</li>
+		<li>We may consider and approve in our sole discretion other link requests from the following types of organizations:
+			<ol>
+				<li>commonly-known consumer and/or business information sources such as Chambers of Commerce, American
+					Automobile Association, AARP and Consumers Union;</li>
+				<li>dot.com community sites;</li>
+				<li>associations or other groups representing charities, including charity giving sites,</li>
+				<li>online directory distributors;</li>
+				<li>internet portals;</li>
+				<li>accounting, law and consulting firms whose primary clients are businesses; and</li>
+				<li>educational institutions and trade associations.</li>
+			</ol>
+		</li>
+	</ol>
+	<p>We will approve link requests from these organizations if we determine that: (a) the link would not reflect
+	unfavorably on us or our accredited businesses (for example, trade associations or other organizations
+	representing inherently suspect types of business, such as work-at-home opportunities, shall not be allowed
+	to link); (b)the organization does not have an unsatisfactory record with us; (c) the benefit to us from
+	the visibility associated with the hyperlink outweighs the absence of <?=$companyName?>; and (d) where the
+	link is in the context of general resource information or is otherwise consistent with editorial content
+	in a newsletter or similar product furthering the mission of the organization.</p>
 
-const getText = locale => {
-  switch (locale) {
-    case 'mi': {
-      return aboutMi()
-    }
-    case 'fr': {
-      return aboutMi()
-    }
-  }
-  return aboutEn()
-}
+	<p>These organizations may link to our home page, to publications or to other Web site information so long as
+	the link: (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval
+	of the linking party and it products or services; and (c) fits within the context of the linking party's
+	site.</p>
 
-const AboutSection = styled.div`
-  width: 72rem;
-  margin: 0 0;
+	<p>If you are among the organizations listed in paragraph 2 above and are interested in linking to our website,
+	you must notify us by sending an e-mail to <a href="mailto:kiaora@voluntarily.co.nz" title="send an email to kiaora@voluntarily.co.nz">kiaora@voluntarily.co.nz</a>.
+	Please include your name, your organization name, contact information (such as a phone number and/or e-mail
+	address) as well as the URL of your site, a list of any URLs from which you intend to link to our Web site,
+	and a list of the URL(s) on our site to which you would like to link. Allow 2-3 weeks for a response.</p>
 
-  h1 {
-    font-size: 4rem;
-    letter-spacing: -3px;
-    font-weight: bold;
-    color: gray;
-  }
+	<p>Approved organizations may hyperlink to our Web site as follows:</p>
 
-  p {
-    font-size: 2rem;
-    letter-spacing: -1.3px;
-    font-weight: 400;
-    color: #333;
-  }
-
-  @media screen and (min-width: 768px) and (max-width: 1025px) {
-    width: calc(100vw - 4rem);
-    margin: 2rem 0 2rem 0;
-    letter-spacing: -01px;
-    h1 {
-      font-size: 2.5rem;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    width: calc(100vw - 2rem);
-    margin: 1rem 0 1rem 0;
-
-    h1 {
-      font-size: 2rem;
-      letter-spacing: -1.5px;
-    }
-
-    p {
-      font-size: 1.5rem;
-    }
-  }
+	<ol>
+		<li>By use of our corporate name; or</li>
+		<li>By use of the uniform resource locator (Web address) being linked to; or</li>
+		<li>By use of any other description of our Web site or material being linked to that makes sense within the
+			context and format of content on the linking party's site.</li>
+	</ol>
+	<p>No use of Voluntarily's logo or other artwork will be allowed for linking absent a trademark license
+	agreement.</p>
+<h2>Iframes</h2>
+	<p>Without prior approval and express written permission, you may not create frames around our Web pages or
+	use other techniques that alter in any way the visual presentation or appearance of our Web site.</p>
+<h2>Reservation of Rights</h2>
+	<p>We reserve the right at any time and in its sole discretion to request that you remove all links or any particular
+	link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also
+	reserve the right to amend these terms and conditions and its linking policy at any time. By continuing
+	to link to our Web site, you agree to be bound to and abide by these linking terms and conditions.</p>
+<h2>Removal of links from our website</h2>
+	<p>If you find any link on our Web site or any linked web site objectionable for any reason, you may contact
+	us about this. We will consider requests to remove links but will have no obligation to do so or to respond
+	directly to you.</p>
+	<p>Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness
+	or accuracy; nor do we commit to ensuring that the website remains available or that the material on the
+	website is kept up to date.</p>
+<h2>Content Liability</h2>
+	<p>We shall have no responsibility or liability for any content appearing on your Web site. You agree to indemnify
+	and defend us against all claims arising out of or based upon your Website. No link(s) may appear on any
+	page on your Web site or within any context containing content or materials that may be interpreted as
+	libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or
+	other violation of, any third party rights.</p>
+<h2>Disclaimer</h2>
+	<p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website (including, without limitation, any warranties implied by law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill). Nothing in this disclaimer will:</p>
+	<ol>
+	<li>limit or exclude our or your liability for death or personal injury resulting from negligence;</li>
+	<li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+	<li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+	<li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+	</ol>
+	<p>The limitations and exclusions of liability set out in this Section and elsewhere in this disclaimer: (a)
+	are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer or
+	in relation to the subject matter of this disclaimer, including liabilities arising in contract, in tort
+	(including negligence) and for breach of statutory duty.</p>
+	<p>To the extent that the website and the information and services on the website are provided free of charge,
+	we will not be liable for any loss or damage of any nature.</p>
+<h2>You must attend your interested Opportunity </h2>
+	<p>You will have to attend the Opportunity that you have said yes too.  Give 42 hours notice to Teacher or Activity provider if you cant make it.</p>
+<h2>Be honest </h2>
+	<p>Dont lie its not cool mate.</p>
+<h2>Buy Ete KFC</h2>
+	<p>Buy Ete KFC yasssss</p>
+<h2>Give Ete . KMart Voucher!</h2>
+	<p>KMART for life!!!!!!!</p>
+<h2>Credit & Contact Information</h2>
+	<p>This Terms and conditions page was created at <a style="color:inherit;text-decoration:none;cursor:text;"
+		href="https://termsandconditionstemplate.com">termsandconditionstemplate.com</a> generator. If you have
+	any queries regarding any of our terms, please contact us.</p>		
 `
-
-class About extends Component {
-  render () {
-    const about = getText(this.props.intl.locale)
-
-    return (
-      <FullPage>
-        <Helmet>
-          <title>Terms and Conditions</title>
-        </Helmet>
-        <Spacer />
-        <Spacer />
-        <AboutSection>
-          <Markdown
-            children={about}
-            options={{
-              overrides: {
-                Button: { component: Button }
-              }
-            }}
-          />
-        </AboutSection>
-        <AboutCTA />
-
-        <style jsx>{`
-          div {
-            margin: 3em;
-            max-width: 50em;
-          }
-          @media (max-width: 600px) {
-            div {
-              margin: 0 3em;
-            }
-          }
-        `}</style>
-      </FullPage>
-    )
-  }
-}
-
-export const AboutTest = About // for test
-
-export default publicPage(withIntl(About))
