07aa6e88a2a6a667fdf30c11f08f4fb35255ceb7
Andrew Watkins <andrew@groat.nz>
2019-10-10 07:41:37 +1300

    remove react-html-email

--- pages/test/test-email.js
+++
@@ -1,9 +0,0 @@
-import publicPage from '../../hocs/publicPage'
-import exampleHTML from '../../components/emails/exampleEmail'
-
-const TestEmail = () => {
-  return (exampleHTML)
-}
-
-export default publicPage(TestEmail)
-// export default TestEmail
