fc03f42a624083ab195f54e27834b1c440d6b00f
Andrew Watkins <andrew@groat.nz>
2019-10-09 13:46:50 +1300

    tidy up test files

---
+++ pages/test/test-email.js
@@ -0,0 +1,9 @@
+import publicPage from '../../hocs/publicPage'
+import exampleHTML from '../../components/emails/exampleEmail'
+
+const TestEmail = () => {
+  return (exampleHTML)
+}
+
+export default publicPage(TestEmail)
+// export default TestEmail
