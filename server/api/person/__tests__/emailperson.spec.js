ffd750ef2eeda097dabc3bd86c70eb0010799bf4
Andrew Watkins <andrew@groat.nz>
2019-10-10 12:38:02 +1300

    simplify email templates

--- server/api/person/__tests__/emailperson.spec.js
+++ server/api/person/__tests__/emailperson.spec.js
@@ -1,25 +1,65 @@
 import test from 'ava'
 import { emailPerson } from '../email/emailperson'
+import ops from '../../opportunity/__tests__/opportunity.fixture'
+import orgs from '../../organisation/__tests__/organisation.fixture'
+import people from './person.fixture'
+import objectid from 'objectid'
+import { config } from '../../../../config/config'
+import { JSDOM } from 'jsdom'
+import { getByText } from '@testing-library/dom'
 
-test('Should send verification email to person', async t => {
-  const person = {
-    name: 'Testy McTestFace',
-    nickname: 'Testy',
-    phone: '123 456789',
-    email: 'andrew@omgtech.co.nz',
-    role: 'tester'
+test.before('Setup fixtures', (t) => {
+  // not using mongo or server here so faking ids
+  people.map(p => {
+    p._id = objectid().toString()
+    p.href = `${config.appUrl}/${p._id}`
+  })
+  const me = people[0]
+  const to = people[1] // the interested person
+
+  orgs.map(org => { org._id = objectid().toString() })
+  const offerOrg = orgs[0]
+
+  // Set myself as the requestor for all of the opportunities, and fake ids
+  ops.map((op, index) => {
+    op._id = objectid().toString()
+    op.requestor = me
+    op.offerOrg = offerOrg
+    op.href = `${config.appUrl}/${op._id}`
+  })
+  t.context = {
+    me,
+    to,
+    op: ops[0],
+    people,
+    ops,
+    orgs
   }
+})
 
+test.serial('Send acknowledgeInterest email to person', async t => {
   const props = {
-    token: '123456789ABCDEF',
-    send: true // when true email is actually sent
+    send: true, // when true email is actually sent
+    me: t.context.me,
+    op: t.context.op
   }
-
-  const info = await emailPerson(person, 'verify', props)
-  // TODO verify templating
+  const info = await emailPerson(t.context.to, 'acknowledgeInterest', props)
 
   // these pass if send is enabled
-  t.true(info.accepted[0] === person.email)
+  t.true(info.accepted[0] === t.context.to.email)
   t.true(info.rejected.length === 0)
   t.regex(info.response, /250.*/, info.response)
+})
+
+test('render acknowledgeInterest email to person', async t => {
+  const props = {
+    send: true, // when true email is actually sent
+    me: t.context.me,
+    op: t.context.op
+  }
+  const html = await emailPerson(t.context.to, 'acknowledgeInterest', props, true)
+  const document = (new JSDOM(html)).window.document
+  t.truthy(getByText(document, 'Help make a difference'))
+  t.truthy(getByText(document, t.context.op.name))
+  t.truthy(getByText(document, `Kia ora ${t.context.to.nickname},`))
 })
--- server/api/person/email/emailperson.js
+++ server/api/person/email/emailperson.js
@@ -2,11 +2,13 @@ const { getTransport, getDevelopmentTransport } = require('../../../services/ema
 const Email = require('email-templates')
 const path = require('path')
 const { config } = require('../../../../config/config')
-
 /*
   format and send a email to the given address
+  @person the target of the email
+  @template the template to format html
+  @props includes details of the opportunity, requestor etc
 */
-module.exports.emailPerson = async (person, template, props) => {
+module.exports.emailPerson = async (person, template, props, renderOnly = false) => {
   try {
     const transport = (process.env.NODE_ENV === 'development') ? await getDevelopmentTransport() : await getTransport()
     const email = new Email({
@@ -15,33 +17,34 @@ module.exports.emailPerson = async (person, template, props) => {
       },
       // uncomment below to send emails in development/test env:
       send: true,
+      subjectPrefix: config.env === 'production' ? false : `[${config.env.toUpperCase()}] `,
 
-      // Comment the line bellow to see preview email
-      preview: false,
-      // Uncomment this line to see the result of the email when it sent
-      // preview: {
-      //   app: 'chrome',
-      //   wait: false
-      // },
-
-      // Not sure if the transport is neccesary anymore
-      // transport: {
-      //   jsonTransport: true,
-      // }
+      // Comment the line below to see preview email
+      preview: true,
       textOnly: config.onlyEmailText, // The value onlyEmailText has a Boolean type not string
       transport
     })
-    return await email.send({
-      template: path.join(__dirname, template),
-      message: {
-        to: person.email,
-        attachments: props.attachment ? props.attachment : null
-      },
-      locals: {
-        person,
-        props
-      }
-    })
+    if (renderOnly) {
+      return await email.render(
+        path.join(__dirname, template + '/html'),
+        {
+          person,
+          ...props
+        }
+      )
+    } else {
+      return await email.send({
+        template: path.join(__dirname, template),
+        message: {
+          to: person.email,
+          attachments: props.attachment ? props.attachment : null
+        },
+        locals: {
+          person,
+          ...props
+        }
+      })
+    }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error)
