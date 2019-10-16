700b40dba22623f2d221ff51c7198b09c58226a3
Minh-Phu <Lminhphu0304@gmail.com>
2019-10-16 23:30:25 +1300

    Use for of in getAbility loop so we can break if admin role added in

--- server/services/authorize/removeUnauthorizedFields.js
+++ server/services/authorize/removeUnauthorizedFields.js
@@ -6,7 +6,7 @@ const removeUnauthorizedFields = (Schema) => (req, res, next) => {
   const authorizedFields = Schema.accessibleFieldsBy(req.ability, action)
   Array.isArray(req.crudify.result)
     ? req.crudify.result = req.crudify.result.map(eachResult => pick(eachResult, authorizedFields))
-    : req.crudify.result = pick(req.crudify.result, authorizedFields)
+    : req.crudify.result = req.crudify.result._id.toString() === req.sesion.me._id.toString() ? { ...req.crudify.result } : pick(req.crudify.result, authorizedFields)
   next()
 }
 
