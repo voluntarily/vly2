8944d923903ba393c4a80ca915758b406c0f6b97
harshanip <perera.harshani@gmail.com>
2019-10-29 10:48:49 +1300

    VP-683 Update ActCard to use VTheme. Update testcases to verify card is rendered as expected

--- components/Act/ActCard.js
+++ components/Act/ActCard.js
@@ -4,93 +4,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
+import { Card } from '../VTheme/VTheme'
 
 // todo if image is not present then use a fallback.
 const ActCard = ({ act, onPress, ...props }) => {
+  const draft = act.status === 'draft' ? 'DRAFT: ' : ''
   const cardImage = act.imgUrl ? act.imgUrl : '../../static/missingimage.svg'
+  const duration = act.duration ? `⏱ ${act.duration}` : ''
   return (
-    <div>
+    <Card>
       <Link href={`/acts/${act._id}`}>
         <a>
-          <div className='requestContainer'>
-            <img className='requestImg' src={cardImage} />
-            <p className='requestTitle'>{act.name}</p>
-            <p className='requestDateTime'>{act.duration}</p>
-            <p className='requestDescription'>{act.subtitle}</p>
-          </div>
+          <img src={cardImage} />
+          <figcaption>
+            <h1>
+              {draft}
+              {act.name}
+            </h1>
+            <p>{duration}</p>
+            <p>{act.subtitle}</p>
+          </figcaption>
         </a>
       </Link>
-      <style jsx>{`
-      .requestContainer {
-        width: 18.5rem;
-        letter-spacing: -0.3px;
-        line-height: 24px;
-        margin-bottom: 0px;
-      }
-
-      .requestImg {
-        width: 100%;
-        height: 10rem;
-        background-color: rgba(0,0,0,0.0);
-        object-fit: cover;
-        object-position: center;
-      }
-
-      .requestTitle {
-        margin-top: 0.3rem;
-        margin-bottom: 0px;
-        vertical-align: middle;
-        font-weight: bold;
-        font-size: 16px;
-        color: #000;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-      }
-
-      .requestDateTime {
-        vertical-align: middle;
-        margin-bottom: 0px;
-        font-weight: bold;
-        font-size: 16px;
-        color: #585858;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-      }
-
-      .requestDescription {
-        vertical-align: top;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 24px;
-        color: #000;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-      }
-
-      .requestTitle :hover {
-        color: #6549aa;
-      }
-
-      @media screen and (max-width: 768px) {
-        .requestContainer {
-          width: calc(100vw - 2rem);
-          margin-bottom: 1.5rem;
-        }
-        .requestImg {
-          height: 12rem;
-        }
-      }
-    `}</style>
-    </div>
+    </Card>
   )
 }
 
