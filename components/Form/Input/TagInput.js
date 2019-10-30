9e61e230cce3a1bf4e33985117a9ea57b8cc414c
Toby Collett <thjc@plan9.net.nz>
2019-10-30 22:01:23 +1300

    Protect against empty existing tags

--- components/Form/Input/TagInput.js
+++ components/Form/Input/TagInput.js
@@ -49,7 +49,7 @@ class TagInput extends React.Component {
 
     handleSearch = value => {
       const val = value.trim()
-      const matchingTags = val
+      const matchingTags = (val && this.props.existingTags)
         ? this.props.existingTags
           .filter(tag => tag.toLowerCase().indexOf(val.toLowerCase()) !== -1)
         : []
