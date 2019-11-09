066719e924018cb37a09df062649cbaa380ccf11
Eteroa Lafaele <eteroalafaele080@gmail.com>
2019-11-09 14:37:48 +1300

    Added the link to the terms and condition

--- components/Interest/RegisterInterestItem.js
+++ components/Interest/RegisterInterestItem.js
@@ -52,10 +52,10 @@ class RegisterInterestItem extends Component {
     } = this.props.form
 
     const termsCondtionsOptions = [
-      { label: 'I accept the Terms and Conditions', value: 'I accept the Terms and Conditions' }
+      { label: 'I accept the ', value: 'I accept the Terms and Conditions' }
     ]
 
-    const termsCondtions = <FormattedMessage id='termsCondtions' defaultMessage='Terms & Conditions' description='termsCondtions' />
+    const termsCondtions = <FormattedMessage id='termsCondtions' defaultMessage='Terms & Conditions' description='termsCondtions'/>
     // Options to configure the controls on this page based on the state of the interest.
     const options = getOptions(this.props.interest)
 
@@ -97,21 +97,27 @@ class RegisterInterestItem extends Component {
           <Row>
             {/* Button to handle positive state change */}
             {options.nextStateButtonEnabled && (options.formAlwaysVisible || this.state.isFormVisible)
-              ? <span>
+              ? <span>   
 
-                <Form.Item label={termsCondtions}>
+                <Form.Item>
                   {getFieldDecorator('termsCondtions', {
                     rules: [
-                      { required: true, message: 'I agree to the'}
+                      { required: true, message: 'I agree to the Terms and Conditions'}
                     ]
-                    // <Link href = {'x'}
                   })(
                     <Checkbox.Group
-                      options={termsCondtionsOptions}
+                     options={termsCondtionsOptions}
                     />
-                  )}
+                  )
+                  }
+                  <a
+                  href='/terms'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  >
+                  Terms and Conditions
+                  </a>
                 </Form.Item>
-
                 <Button type='primary' disabled={hasErrors(getFieldsError())} shape='round' onClick={this.handleChangeStateButtonClicked.bind(this)}>
                   {options.nextStateButtonText}
                 </Button>
