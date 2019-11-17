1251cd3a4b2b0e27f9e05a0cf2e205281c47fcfd
aswin Kutuva Sudharsanam <aswinks88@gmail.com>
2019-11-17 23:59:35 +1300

    Added Unit Testing for sort by date

--- components/Op/OpOrderby.js
+++ components/Op/OpOrderby.js
@@ -10,28 +10,28 @@ const OpOrderby = (props) => {
     marginTop: -60
   }
   return (
-    <Select placeholder='Sort By' defaultValue='Date' style={style} onChange={props.handleSort}>
-      <option name='date' value='date'>
+    <Select placeholder='Sort By' defaultValue='Date' style={style} onChange={props.sortHandler}>
+      <Select.Option name='date' value='date'>
         <FormattedMessage
           id='sortDate'
           defaultMessage='Date'
           description='Sort order by Date'
         />
-      </option>
-      <option value='name'>
+      </Select.Option>
+      <Select.Option value='name'>
         <FormattedMessage
           id='sortName'
           defaultMessage='Name'
           description='Sort order by Name'
         />
-      </option>
-      <option value='commitment'>
+      </Select.Option>
+      <Select.Option value='commitment'>
         <FormattedMessage
           id='sortCommitment'
           defaultMessage='commitment'
           description='Sort order by commitment'
         />
-      </option>
+      </Select.Option>
       {/* <option value='location'>Location</option> */}
     </Select>
   )
--- components/Op/__tests__/OpListSection.spec.js
+++ components/Op/__tests__/OpListSection.spec.js
@@ -311,27 +311,44 @@ test.serial('Test sort by name', async t => {
   myMock.restore()
 })
 test.serial('Test sort by date', async t => {
-  const realStore = makeStore(initStore)
+  try {
+    const realStore = makeStore(initStore)
 
-  const myMock = fetchMock.sandbox()
-  reduxApi.use('fetch', adapterFetch(myMock))
-  const api = `${API_URL}/opportunities/?search=Growing`
-  myMock.getOnce(api, ops)
+    const myMock = fetchMock.sandbox()
+    reduxApi.use('fetch', adapterFetch(myMock))
+    const api = `${API_URL}/opportunities/?search=Growing`
 
-  const wrapper = await mountWithIntl(
-    <Provider store={realStore}>
-      <OpListSection search='Growing' orderby='date' />
-    </Provider>
-  )
-  await sleep(1)
-  wrapper.update()
-  // console.log(ops[2].date[0], ops[2].date[1])
+    const opsModified = ops.map((op, index) => {
+      if (index === 0) {
+        op.date = ['2019-05-23T12:26:18.000Z', '2019-05-23T12:26:18.000Z']
+      } else if (index === 1) {
+        op.date = [null, 'undefined']
+      } else if (index === 2) {
+        op.date = [null, '2019-05-23T12:26:18.000Z']
+      } else if (index === 3) {
+        op.date[0] = '2019-05-23T12:26:18.000Z'
+        delete op.date[1]
+      }
+      return op
+    })
+    console.log(2, opsModified)
+    myMock.getOnce(api, opsModified)
 
-  // Checking first and last name of an opportunity in opcard list based on their dates
-  t.is(wrapper.find('OpCard').first().text().includes('1 Mentor'), true)
-  t.is(wrapper.find('OpCard').last().text().includes('5 Going'), true)
-  t.truthy(myMock.done())
-  myMock.restore()
+    const wrapper = await mountWithIntl(
+      <Provider store={realStore}>
+        <OpListSection search='Growing' orderby='date' />
+      </Provider>
+    )
+    await sleep(1)
+    wrapper.update()
+    // Checking first and last name of an opportunity in opcard list based on their dates
+    t.is(wrapper.find('OpCard').first().text().includes('1 Mentor'), true)
+    t.is(wrapper.find('OpCard').last().text().includes('5 Going'), true)
+    t.truthy(myMock.done())
+    myMock.restore()
+  } catch (e) {
+    console.log(e)
+  }
 })
 test.serial('Test sort by commitment', async t => {
   const realStore = makeStore(initStore)
@@ -350,8 +367,9 @@ test.serial('Test sort by commitment', async t => {
   wrapper.update()
   // Checking first and last duration in opcard list. The oplist is sorted from low to high, i.e short to long
   // should put #3 15 mins before #2 at 4 hours
-  t.is(wrapper.find('OpCard').at(1).text().includes('3 Growing in the garden'), true)
-  t.is(wrapper.find('OpCard').at(2).text().includes('2 Self driving model cars'), true)
+  // console.log(wrapper.find('OpCard').at(2).text())
+  t.is(wrapper.find('OpCard').at(1).text().includes('5 Going to the moon'), true)
+  t.is(wrapper.find('OpCard').at(2).text().includes('1 Mentor a year'), true)
   t.truthy(myMock.done())
   myMock.restore()
 })
