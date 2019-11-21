41309a02adaefe3b7c00e2e1370961f359778bc8
StellaRu <stella_ru@outlook.com>
2019-11-21 22:49:05 +1300

    test div

--- components/Op/__tests__/OpShareLink.spec.js
+++ components/Op/__tests__/OpShareLink.spec.js
@@ -6,7 +6,8 @@ import { ShareLinks } from '../OpShareLinks'
 const appUrl = 'https://www.testUrl.com'
 
 test('render share links', t => {
-  const wrapper = renderWithIntl(<ShareLinks url={appUrl}/>, { context: '' })
+  const wrapper = renderWithIntl(<ShareLinks url={appUrl} />, { context: '' })
+  t.truthy(wrapper.find('div'))
   t.truthy(wrapper.find('FacebookShareButton'))
   t.truthy(wrapper.find('TwitterShareButton'))
   t.truthy(wrapper.find('LinkedinShareButton'))
