7aee8c6c45c1749aa78eabad74beec33ee1789f2
StellaRu <stella_ru@outlook.com>
2019-11-21 21:56:00 +1300

    test share links when rendering

---
+++ components/Op/__tests__/OpShareLink.spec.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import test from 'ava'
+import { renderWithIntl } from '../../../lib/react-intl-test-helper'
+import { ShareLinks } from '../OpShareLinks'
+
+const appUrl = 'https://www.testUrl.com'
+
+test('render share links', t => {
+  const wrapper = renderWithIntl(<ShareLinks url={appUrl}/>, { context: '' })
+  t.truthy(wrapper.find('FacebookShareButton'))
+  t.truthy(wrapper.find('TwitterShareButton'))
+  t.truthy(wrapper.find('LinkedinShareButton'))
+  t.truthy(wrapper.find('WhatsappShareButton'))
+})
