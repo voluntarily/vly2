b40477a539fa2a868d880664cbc7d17d084459d4
StellaRu <stella_ru@outlook.com>
2019-11-22 16:27:49 +1300

    update test

--- components/Op/__tests__/OpShareLink.spec.js
+++ components/Op/__tests__/OpShareLink.spec.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import test from 'ava'
-import { renderWithIntl } from '../../../lib/react-intl-test-helper'
+import { shallowWithIntl } from '../../../lib/react-intl-test-helper'
 import { ShareLinks } from '../OpShareLinks'
 
-const appUrl = 'https://www.testUrl.com'
-
-test('render share links', t => {
-  const wrapper = renderWithIntl(<ShareLinks url={appUrl} />, { context: '' })
-  t.truthy(wrapper.find('div'))
+test('render social medias', t => {
+  const appUrl = 'https://www.testUrl.com'
+  const wrapper = shallowWithIntl(<ShareLinks url={appUrl} />)
   t.truthy(wrapper.find('FacebookShareButton'))
   t.truthy(wrapper.find('TwitterShareButton'))
   t.truthy(wrapper.find('LinkedinShareButton'))
