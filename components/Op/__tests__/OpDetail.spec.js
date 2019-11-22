d1675fc8f2f2d9858562f4b5e29229cef881cbc5
Walter Lim <waltissomewhere@gmail.com>
2019-11-22 23:35:41 +1300

    working on tests

--- components/Op/__tests__/OpDetail.spec.js
+++ components/Op/__tests__/OpDetail.spec.js
@@ -21,4 +21,20 @@ test('render the detail with op', t => {
   t.is(wrapper.find('h1').text(), t.context.op.name)
 })
 
+
+
+
+test.serial('render the tabs', t => {
+
+
+  const wrapper = renderWithIntl(<OpDetail op={t.context.op} onPress={() => {}} />)
+t.is(wrapper.find('.ant-tabs-tab').children().text(), 'About')
+t.is(wrapper.find('.ant-tabs-tab').children().text(), 'Questions')
+t.is(wrapper.find('.ant-tabs-tab').children().text(), 'Updates')
+t.is(wrapper.find('.ant-tabs-tab').children().text(), 'Manage Volunteers')
+t.is(wrapper.find('.ant-tabs-tab').children().text(), 'Edit Activity')
+
+
+
+}
 // test.todo('verify markdown in description is rendered')
