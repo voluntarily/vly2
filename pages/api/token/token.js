f1e6ddf9487ba05282fac69a7373a996a7e7c1e1
Andrew Watkins <andrew@groat.nz>
2019-11-26 15:34:14 +1300

    remove unwanted console.logs

--- pages/api/token/token.js
+++ pages/api/token/token.js
@@ -6,7 +6,6 @@ import { handleURLToken } from '../../../lib/sec/actiontoken'
 */
 
 export const handleToken = async (req, res, actionTable) => {
-  // console.log('handleToken', req.query)
   const { token } = req.query
   // request must have a ?token=
   if (!token) {
@@ -14,14 +13,13 @@ export const handleToken = async (req, res, actionTable) => {
   }
   // if user is not authenticated then get them in.
   if (!req.session.isAuthenticated) {
-    // console.log('signing thru to', req.originalUrl)
     return res.redirect(`/auth/sign-thru?redirect=${req.originalUrl}`)
   }
   try {
     const payload = await handleURLToken(token, actionTable)
     return res.redirect(payload.redirectUrl)
   } catch (e) {
-    console.log('handleToken:', e)
+    console.error('handleToken:', e)
     res.status(500).end()
   }
 }
