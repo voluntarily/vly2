904867517da02ce3eadf93a070d235b3d9473d22
Andrew Watkins <andrew@groat.nz>
2019-11-28 08:50:12 +1300

    add adminMsg and tidy layout

--- components/Member/InviteMembers.js
+++ components/Member/InviteMembers.js
@@ -2,17 +2,30 @@ import { FormattedMessage } from 'react-intl'
 import { message, Radio } from 'antd'
 import callApi from '../../lib/callApi'
 import { MemberStatus } from '../../server/api/member/member.constants'
+import TextArea from 'antd/lib/input/TextArea'
+import { useState } from 'react'
+import {
+  DescriptionContainer,
+  FormGrid,
+  InputContainer
+} from '../VTheme/FormStyles'
 
 export default ({ org }) => {
+  const [adminMsg, setAdminMsg] = useState('')
+
   const handleInviteMember = (e) => {
-    console.log('handleInviteMember', e.target.value, org)
     const memberStatus = e.target.value
     const memberValidation = `${org.name} Invitation`
-    const url = `notify/org/${org._id}?memberStatus=${memberStatus}&memberValidation=${memberValidation}`
-    callApi(url)
-    message
-      .loading('Sending email..', 2.5)
+    const url = `notify/org/${org._id}?memberStatus=${memberStatus}&memberValidation=${memberValidation}&adminMsg=${adminMsg}`
+    try {
+      callApi(url)
+      message
+        .loading('Sending email..', 2.5)
+    } catch {
+      console.error('handleInviteMember', e)
+    }
   }
+
   return (
     <>
       <h2>
@@ -22,34 +35,82 @@ export default ({ org }) => {
           description='title for invite organisation members section on orgAdmin page'
         />
       </h2>
-      <p>
-        <FormattedMessage
-          defaultMessage='Use this button to send yourself an email that you can forward to your staff.<br />The email will contain a link that brings people to Voluntarily and automatically<br />makes them members of your organisation, placing them on your home page.'
-          id='InviteMembers.description'
-          description='inform orgAdmin how to send emails to staff'
-        />
-      </p>
-      <div>
-        <Radio.Group buttonStyle='solid' onChange={handleInviteMember}>
-          <Radio.Button value={MemberStatus.FOLLOWER}>
+      <FormGrid>
+        <DescriptionContainer>
+          <FormattedMessage
+            defaultMessage={
+            `Use these buttons to send yourself an email that 
+              you can forward to people in your school or business.
+              Each button will send you an email containing a link 
+              that will automatically add the recipient to your 
+              organisation as a follower or member. `
+            }
+            id='InviteMembers.description'
+            description='inform orgAdmin how to send emails to staff'
+          />
+        </DescriptionContainer>
+        <InputContainer>
+          <label>
             <FormattedMessage
-              defaultMessage='Follower'
-              id='InviteMembers.follower'
+              defaultMessage='Add a personal message to the people you are inviting'
+              id='InviteMembers.adminMessage'
+              description='label for text box for admin personal message'
             />
-          </Radio.Button>
-          <Radio.Button value={MemberStatus.JOINER}>
+          </label>
+          <TextArea
+            value={adminMsg}
+            onChange={e => setAdminMsg(e.target.value)}
+          />
+        </InputContainer>
+        <DescriptionContainer>
+          <FormattedMessage
+            defaultMessage={
+            `Use Joiner if you want to review new members 
+              before accepting them and Administrator to give 
+              people permission to edit the organisation pages 
+              and add new members. Once people join they will appear in the tables below.`
+            }
+            id='InviteMembers.description2'
+            description='continued inform orgAdmin how to send emails to staff'
+          />
+        </DescriptionContainer>
+        <InputContainer>
+          <label>
             <FormattedMessage
-              defaultMessage='Joiner'
-              id='InviteMembers.joiner'
+              defaultMessage='Set membership status and send email'
+              id='InviteMembers.InviteBtnPrompt'
+              description='prompt for which invitation button to use'
             />
-          </Radio.Button>
-          <Radio.Button value={MemberStatus.MEMBER}>
-            <FormattedMessage
-              defaultMessage='Member'
-              id='InviteMembers.member'
-            />
-          </Radio.Button>
-        </Radio.Group>
-      </div>
+          </label><br />
+          <Radio.Group buttonStyle='solid' size='large' onChange={handleInviteMember}>
+            <Radio.Button value={MemberStatus.FOLLOWER}>
+              <FormattedMessage
+                defaultMessage='Follower'
+                id='InviteMembers.follower'
+              />
+            </Radio.Button>
+            <Radio.Button value={MemberStatus.JOINER}>
+              <FormattedMessage
+                defaultMessage='Joiner'
+                id='InviteMembers.joiner'
+              />
+            </Radio.Button>
+            <Radio.Button value={MemberStatus.MEMBER}>
+              <FormattedMessage
+                defaultMessage='Member'
+                id='InviteMembers.member'
+              />
+            </Radio.Button>
+            <Radio.Button value={MemberStatus.ORGADMIN}>
+              <FormattedMessage
+                defaultMessage='Administrator'
+                id='InviteMembers.orgAdmin'
+              />
+            </Radio.Button>
+          </Radio.Group>
+        </InputContainer>
+
+      </FormGrid>
+
     </>)
 }
