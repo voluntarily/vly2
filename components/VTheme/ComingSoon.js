e383135da06916d9e2c0df0c7b743c6065ed2448
Walter Lim <waltissomewhere@gmail.com>
2019-11-29 13:26:57 +1300

    added coming soon component and demo usage in storybook

---
+++ components/VTheme/ComingSoon.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import { Icon } from 'antd'
+import { PBold } from './VTheme'
+const ComingsoonContainer = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  background-color: #e1e1e1;
+
+  div {
+    place-self: center;
+  }
+`
+
+const ComingSoonGrid = styled.div`
+  display: grid;
+  grid-template-columns: 4rem 1fr;
+  align-self: center;
+`
+
+export function ComingSoon ({ children }) {
+  return (
+    <ComingsoonContainer>
+      <ComingSoonGrid>
+        <Icon spin style={{ fontSize: '40px', color: '#333' }} type='setting' />
+        <div>
+          <PBold>Needs Work</PBold>
+          <p>{children}</p>
+        </div>
+      </ComingSoonGrid>
+    </ComingsoonContainer>
+  )
+}
+
+export default ComingSoon
--- components/_intro/test.stories.js
+++ components/_intro/test.stories.js
@@ -1,6 +1,48 @@
 import { storiesOf } from '@storybook/react'
+import ComingSoon from '../VTheme/ComingSoon'
+import { FormGrid } from '../VTheme/FormStyles'
+import styled from 'styled-components'
+import { Divider } from 'antd'
+
+const TestContainer = styled.div`
+  height: 80vh;
+`
+const TestItem = styled.div`
+  height: 10rem;
+`
+
+const ExamplePurple = styled.div`
+  height: 10rem;
+  background-color: purple;
+`
 
 storiesOf('Test Components', module)
-  .add('Confirm details', () => (
-    <div />
+  .add('Confirm details', () => <div />)
+  .add('Test Coming soon', () => (
+    <div>
+      <TestContainer>
+        <ComingSoon>
+          VP-123 - Free Ice Cream for participating volunteers
+          <br /> anyone got any FE skills?
+        </ComingSoon>
+      </TestContainer>
+      <Divider />
+      <p>example of a filled container</p>
+      <Divider />
+      <FormGrid>
+        <ExamplePurple />
+        <div>
+          <TestItem>
+            {' '}
+            <ComingSoon>
+              VP-123 - Free Ice Cream for participating volunteers
+            </ComingSoon>
+          </TestItem>
+        </div>
+      </FormGrid>
+
+      <Divider />
+      <p>example of use in a form</p>
+      <Divider />
+    </div>
   ))
