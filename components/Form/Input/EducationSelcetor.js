b84b455e5e9d76f3722ea38f9ab5334b80b20322
Eteroa Lafaele <eteroalafaele080@gmail.com>
2019-12-02 17:58:50 +1300

    Created the education selcetor

---
+++ components/Form/Input/EducationSelcetor.js
@@ -0,0 +1,32 @@
+import { Select } from 'antd'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const { Option } = Select
+
+class EducationSelector extends React.Component {
+  render () {
+    const { existingLocations, onChange, value, width } = this.props
+    const children = existingLocations.map(location => <Option key={location}>{location}</Option>)
+    return (
+      <Select
+        showSearch
+        placeholder='Select your education'
+        onChange={onChange}
+        value={value}
+        style={width && { width: width }}
+      >
+        {children}
+      </Select>
+    )
+  }
+}
+
+EducationSelector.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  existingLocations: PropTypes.arrayOf(PropTypes.string).isRequired,
+  width: PropTypes.string
+}
+
+export default EducationSelector
