f8692da8b207d97b69f7524f9f628a3e4bc5d92f
carey <carey.kwan@tec.govt.nz>
2019-12-09 10:24:19 +1300

    VP-875/happening_soon_responsive: fixed linting issues

--- components/Op/OpList.js
+++ components/Op/OpList.js
@@ -17,11 +17,11 @@ const OpList = ({ ops, ...props }) => (
   <Row>
     {ops ? (
       ops.map((op, index) =>
-      <Col key={index} xs={24} md={12} lg={8} xl={6}>
-        <CardWrapper>
-          <OpCard size='Small' op={op}/>
-        </CardWrapper>
-      </Col>)
+        <Col key={index} xs={24} md={12} lg={8} xl={6}>
+          <CardWrapper>
+            <OpCard size='Small' op={op} />
+          </CardWrapper>
+        </Col>)
     ) : (
       <FormattedMessage
         id='op.list.empty'
