4e1e3085b01a34aa39331cac1d052e01e42b7241
Andrew Watkins <andrew@groat.nz>
2019-12-11 11:28:12 +1300

    complete health check coverage

--- __tests__/api/health/health.spec.js
+++ __tests__/api/health/health.spec.js
@@ -5,37 +5,57 @@ import health from '../../../pages/api/health/health'
 import param from '../../../pages/api/health/[param]'
 import log from '../../../pages/api/health/log'
 import pub from '../../../pages/api/health/pub'
+import config from '../../../pages/api/health/config'
 import sinon from 'sinon'
 import { TOPIC_API__HEALTH } from '../../../server/services/pubsub/topic.constants'
 import PubSub from 'pubsub-js'
 
 test('Should respond to health check', async t => {
-  const mockReq = new MockExpressRequest()
-  const mockRes = new MockExpressResponse()
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
 
-  mockReq.query = { msg: 'Test' }
-  await health(mockReq, mockRes)
-  t.deepEqual(mockRes._getJSON().query, { msg: 'Test' })
-  t.is(200, mockRes.statusCode, 'OK Response')
+  req.query = { msg: 'Test' }
+  await health(req, res)
+  t.deepEqual(res._getJSON().query, { msg: 'Test' })
+  t.is(200, res.statusCode, 'OK Response')
 })
 
 test('Should respond to parameter check', async t => {
-  const mockReq = new MockExpressRequest()
-  const mockRes = new MockExpressResponse()
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
 
-  mockReq.query = { param: 'Test' }
-  await param(mockReq, mockRes)
-  t.deepEqual(mockRes._getJSON().query, { param: 'Test' })
-  t.is(200, mockRes.statusCode, 'OK Response')
+  req.query = { param: 'Test' }
+  await param(req, res)
+  t.deepEqual(res._getJSON().query, { param: 'Test' })
+  t.is(200, res.statusCode, 'OK Response')
+})
+
+test('Should prevent config check for non admins', async t => {
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.ability = { can: sinon.fake.returns(false) }
+  req.query = { param: 'Test' }
+  await config(req, res)
+  t.is(401, res.statusCode, 'Not allowed by CASL')
+})
+
+test('Should respond to config check', async t => {
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.ability = { can: sinon.fake.returns(true) }
+  req.query = { param: 'Test' }
+  await config(req, res)
+  t.is(200, res.statusCode, 'OK Response')
+  t.deepEqual(res._getJSON().env, 'test')
 })
 
 test('Should respond to log check', async t => {
-  const mockReq = new MockExpressRequest()
-  const mockRes = new MockExpressResponse()
-  mockReq.query = { msg: 'Log Test' }
-  await log(mockReq, mockRes)
-  t.deepEqual(mockRes._getJSON(), 'Log Test')
-  t.is(200, mockRes.statusCode, 'OK Response')
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.query = { msg: 'Log Test' }
+  await log(req, res)
+  t.deepEqual(res._getJSON(), 'Log Test')
+  t.is(200, res.statusCode, 'OK Response')
 })
 
 /* Example of testing Pub/Sub calls
@@ -51,21 +71,21 @@ test('Should respond to pub check', async t => {
 
   PubSub.subscribe(TOPIC_API__HEALTH, spy)
 
-  const mockReq = new MockExpressRequest()
-  const mockRes = new MockExpressResponse()
-  mockReq.query = { msg: 'Pub Test' }
-  await pub(mockReq, mockRes)
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.query = { msg: 'Pub Test' }
+  await pub(req, res)
 
   t.is(spy.callCount, 0)
   clock.tick(1)
   t.is(spy.callCount, 1)
   clock.restore()
   t.deepEqual(spy.args[0][1], { msg: 'Pub Test' })
-  t.deepEqual(mockRes._getJSON(), 'Pub Test')
-  t.is(200, mockRes.statusCode, 'OK Response')
+  t.deepEqual(res._getJSON(), 'Pub Test')
+  t.is(200, res.statusCode, 'OK Response')
 })
 
-// mockReq.session = {
+// req.session = {
 //   isAuthenticated: false,
 //   me: {},
 //   user: {}
