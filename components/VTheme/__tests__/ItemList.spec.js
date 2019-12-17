e7ea132572795922dc866cd1a5dbfcb5d689a107
James Goodman <james.goodman@tec.govt.nz>
2019-12-18 11:10:03 +1300

    VP-939 Switch render to renderWithIntl in ItemList.spec.js

--- components/VTheme/__tests__/ItemList.spec.js
+++ components/VTheme/__tests__/ItemList.spec.js
@@ -1,19 +1,19 @@
 import test from 'ava'
 import { ItemVolunteers } from '../ItemList'
-import { render } from 'enzyme'
+import { renderWithIntl} from '../../../lib/react-intl-test-helper'
 
 test('render Volunteers per student properly if the value is < 1', t => {
 //   t.context.act.volunteers = 0.2
-  const wrapper = render(<ItemVolunteers volunteers={0.2} type='act' />)
+  const wrapper = renderWithIntl(<ItemVolunteers volunteers={0.2} type='act' />)
   t.is(wrapper.find('span').first().text(), 'Volunteers per student:')
 })
 test('render volunteer properly if the value is >= 1', t => {
 //   t.context.act.volunteers = 5
-  const wrapper = render(<ItemVolunteers volunteers={5} type='act' />)
+  const wrapper = renderWithIntl(<ItemVolunteers volunteers={5} type='act' />)
   t.is(wrapper.find('span').first().text(), 'Volunteers:')
 })
 test('render volunteer values === 0 properly', t => {
 //   t.context.volunteers = 0
-  const wrapper = render(<ItemVolunteers volunteers={0} type='act' />)
+  const wrapper = renderWithIntl(<ItemVolunteers volunteers={0} type='act' />)
   t.is(wrapper.find('span').first().text(), '')
 })
