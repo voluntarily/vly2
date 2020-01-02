5da561512401a4b3c43f6c90ec0e9baf1911927c
Andrew Watkins <andrew@groat.nz>
2020-01-03 12:16:53 +1300

    VP-884 my/org puts admins first

--- server/api/member/member.lib.js
+++ server/api/member/member.lib.js
@@ -1,6 +1,7 @@
 const Member = require('./member')
 const PubSub = require('pubsub-js')
 const { TOPIC_MEMBER__UPDATE } = require('../../services/pubsub/topic.constants')
+const { MemberStatus } = require('./member.constants')
 
 /* get a single member record with org and person populated out */
 const getMemberbyId = id => {
@@ -28,6 +29,19 @@ const addMember = async (member) => {
   return got
 }
 
+function compareMemberStatus (a, b) {
+  if (a.status === b.status) return 0
+  if (a.status === MemberStatus.ORGADMIN) return -1
+  if (b.status === MemberStatus.ORGADMIN) return 1
+  if (a.status === MemberStatus.MEMBER) return -1
+  if (b.status === MemberStatus.MEMBER) return 1
+  if (a.status === MemberStatus.FOLLOWER) return -1
+  if (b.status === MemberStatus.FOLLOWER) return 1
+
+  // all others return arbitary a
+  return -1
+}
+
 const findOrgByPersonIdAndCategory = async (personId, category) => {
   // search membership table for org matching category and person id
   const query = { person: personId }
@@ -41,6 +55,8 @@ const findOrgByPersonIdAndCategory = async (personId, category) => {
   if (!myorgs.length) { // failed to find matching org
     return null
   }
+  // sort to give most important level of membership first.
+  myorgs.sort(compareMemberStatus)
   // get id of Organisation
   return myorgs[0].organisation._id
 }
