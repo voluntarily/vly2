2251ff7013edcd98b6ac70d74ae0256ba3c88985
Andrew Watkins <andrew@groat.nz>
2020-01-06 17:12:24 +1300

    move readyqs data to ready page

--- pages/goal/school/ready.js
+++ pages/goal/school/ready.js
@@ -1,7 +1,41 @@
 import securePage from '../../../hocs/securePage'
 import { FullPage } from '../../../components/VTheme/VTheme'
-import readyqs from './ready.qs.js'
 import { VideoQuestions, hashObj } from '../../../components/quiz/quiz'
+
+const readyqs = [
+  {
+    name: 'Are you School Ready?',
+    description: 'Watch this video to learn what you need to know when working in a school',
+    step: 1,
+    src: 'https://www.youtube.com/embed/kv8GrLpfVNo',
+    next: 'ready2.qs.js',
+    questions: [
+      {
+        name: 'tShirt',
+        q: 'What does Vaughan have on his t-shirt?',
+        options: [
+          'Unicorn',
+          'Dinosaur',
+          'Robot bee',
+          'Jandals'
+        ]
+      },
+      {
+        name: 'launchDate',
+        q: 'When does Voluntarily Launch?',
+        options: [
+          '2020',
+          '2021',
+          '2022'
+        ]
+      }
+    ],
+    answers: {
+      tShirt: 1,
+      launchDate: 0
+    }
+  }
+]
 
 const Ready = ({ vqa, me }) => {
   const handleSubmit = success => {
