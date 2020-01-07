493f40d97293588eb784ceecb426d9233e2b7569
Andrew Watkins <andrew@groat.nz>
2020-01-07 15:04:32 +1300

    test route onCompleted test

--- __tests__/goal/school/ready.spec.js
+++ __tests__/goal/school/ready.spec.js
@@ -3,7 +3,8 @@ import { Ready as SchoolReady } from '../../../pages/goal/school/ready'
 import { shallowWithIntl } from '../../../lib/react-intl-test-helper'
 import { hashObj } from '../../../components/quiz/quiz'
 import quiz from '../../../components/quiz/__tests__/quiz.fixture'
-
+import withMockRoute from '../../../server/util/mockRouter'
+import sinon from 'sinon'
 test('render OpList', async t => {
   // first test GetInitialProps
   const email = 'testy@voluntarily.nz'
@@ -14,13 +15,19 @@ test('render OpList', async t => {
     }
   }
   const hash = hashObj(quiz[0].answers, email)
+  const RoutedSchoolReady = withMockRoute(SchoolReady)
 
   const props = await SchoolReady.getInitialProps({ store })
   t.is(props.vqa.name, 'Are you School Ready?')
   t.is(props.vqa.hash, hash)
 
-  const wrapper = shallowWithIntl(<SchoolReady {...props} me={me} />)
+  const outer = shallowWithIntl(<RoutedSchoolReady {...props} me={me} />)
+
+  const router = outer.props().router
+  router.push = sinon.spy()
+  const wrapper = outer.dive()
+
   t.true(wrapper.exists('VideoQuiz'))
-  wrapper.find('VideoQuiz').props().onSubmit(true)
-  wrapper.find('VideoQuiz').props().onSubmit(false)
+  wrapper.find('VideoQuiz').props().onCompleted()
+  t.true(router.push.calledOnce)
 })
