1dbe13748a360e350d7d55cc58e4a5e267be44fb
Andrew Watkins <andrew@groat.nz>
2020-01-10 18:18:44 +1300

    simplify loading line

--- components/Interest/RegisterInterestSection.js
+++ components/Interest/RegisterInterestSection.js
@@ -4,7 +4,6 @@
 */
 import { message } from 'antd'
 import React, { Component } from 'react'
-import { FormattedMessage } from 'react-intl'
 import reduxApi, { withInterests } from '../../lib/redux/reduxApi'
 import Loading from '../Loading'
 import InterestConfirmationCard from './InterestConfirmationCard'
@@ -57,43 +56,33 @@ class RegisterInterestSection extends Component {
   render () {
     // If we haven't finished making the API request to the server yet...
     if (this.props.interests.loading) {
-      return (
-        <section>
-          <Loading>
-            <p>
-              <FormattedMessage
-                id='registerInterest.loading'
-                description='loading message'
-                defaultMessage='Loading ...'
-              />
-            </p>
-          </Loading>
-        </section>)
-    } else { // If we have access to the interests section of the Redux store...
-      // Get the interest out of the store, if any.
-      let interest = null
+      return (<Loading />)
+    }
 
-      if (this.props.interests.sync && this.props.interests.data.length > 0) {
-        interest = this.props.interests.data[0]
-      } else { // If not, use a blank interest.
-        interest = getNewInterest(this.props.meID, this.props.op)
-      }
-      return (
-        <section>
-          <RegisterInterestItem
-            interest={interest}
-            onChangeStatus={this.handleChangeStatus.bind(this)}
-            onWithdraw={this.handleWithdraw.bind(this)}
-          />
-          {
-            (interest.status === 'committed') &&
+    // If we have access to the interests section of the Redux store...
+    // Get the interest out of the store, if any.
+    let interest = null
+
+    if (this.props.interests.sync && this.props.interests.data.length > 0) {
+      interest = this.props.interests.data[0]
+    } else { // If not, use a blank interest.
+      interest = getNewInterest(this.props.meID, this.props.op)
+    }
+    return (
+      <section>
+        <RegisterInterestItem
+          interest={interest}
+          onChangeStatus={this.handleChangeStatus.bind(this)}
+          onWithdraw={this.handleWithdraw.bind(this)}
+        />
+        {
+          (interest.status === 'committed') &&
               (
                 <InterestConfirmationCard organizer={this.props.op.requestor} />
               )
-          }
-        </section>
-      )
-    }
+        }
+      </section>
+    )
   }
 }
 
