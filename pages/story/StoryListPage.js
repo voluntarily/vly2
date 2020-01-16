53389cc72afe1e1671412e1b31fa7f0f1fd92fdc
FARJANA_CHAITY <FARJANA@pamfergusson.org.nz>
2020-01-16 14:48:12 +1300

    default export for story list page

--- pages/story/StoryListPage.js
+++ pages/story/StoryListPage.js
@@ -1,9 +1,9 @@
 import publicPage from '../../hocs/publicPage'
 import { FullPage } from '../../components/VTheme/VTheme'
 
-const storyListPage = () =>
+export const storyListPage = () =>
   <FullPage>
     <h1>Story List</h1>
   </FullPage>
-module.exports = storyListPage
+
 export default publicPage(storyListPage)
