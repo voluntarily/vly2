aec4fb170d8bb148829b84b0653c080a7ace746a
FARJANA_CHAITY <FARJANA@pamfergusson.org.nz>
2020-01-23 13:52:20 +1300

    new story/ blog creation component

---
+++ components/Story/AddStory.js
@@ -0,0 +1,40 @@
+/*
+  Display the new blog creation form
+*/
+import { Button } from 'antd'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import { connect } from 'react-redux'
+import { Role } from '../../server/services/authorize/role.js'
+// todo if image is not present then use a fallback.
+const AddStory = ({ roles, ...props }) => {
+  if (roles && roles.includes(Role.ADMIN || Role.ORG_ADMIN)) {
+    return (
+      <Link href='/act/new'>
+        <Button type='primary' shape='round' size='large'>
+          <FormattedMessage
+            id='story.new'
+            defaultMessage='New Blog'
+            description='Button to create a new blog'
+          />
+        </Button>
+      </Link>
+    )
+  } else {
+    return null
+  }
+}
+
+AddStory.propTypes = {
+  roles: PropTypes.array
+}
+
+const mapStateToProps = store => ({
+  roles: store.session.me.role
+})
+
+export default connect(
+  mapStateToProps
+)(AddStory)
