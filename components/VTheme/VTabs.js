1389d27246e59e4bdae7204c57c80cb35b81cec2
Walter Lim <waltissomewhere@gmail.com>
2020-01-31 13:33:26 +1300

    lint

--- components/VTheme/VTabs.js
+++ components/VTheme/VTabs.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 import { Tabs } from 'antd'
 
-
 const VTabs = styled(Tabs)`
 overflow: visible;
 text-align: center;
 font-weight: bold;
 color: #6549AA;
 `
-export default VTabs;
+export default VTabs
