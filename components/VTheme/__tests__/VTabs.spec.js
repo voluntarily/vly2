49611c260379628a614676441d7d7bdacef77d31
Walter Lim <waltissomewhere@gmail.com>
2020-02-03 10:41:11 +1300

    added test for vtabs component

---
+++ components/VTheme/__tests__/VTabs.spec.js
@@ -0,0 +1,9 @@
+import test from 'ava'
+import { render } from 'enzyme'
+import { VTabs } from '../VTabs'
+
+test('Check Tabs Render', t => {
+//   t.context.act.volunteers = 0.2
+  const wrapper = render(<VTabs/>)
+  t.is(wrapper.find('VTabs').length, 1)
+})
