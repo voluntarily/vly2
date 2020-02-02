6760ace169ebf540f66942da18e0848a9e980656
Walter Lim <waltissomewhere@gmail.com>
2020-02-03 10:46:02 +1300

    lint

--- components/VTheme/__tests__/VTabs.spec.js
+++ components/VTheme/__tests__/VTabs.spec.js
@@ -4,6 +4,6 @@ import { VTabs } from '../VTabs'
 
 test('Check Tabs Render', t => {
 //   t.context.act.volunteers = 0.2
-  const wrapper = render(<VTabs/>)
+  const wrapper = render(<VTabs />)
   t.is(wrapper.find('VTabs').length, 1)
 })
