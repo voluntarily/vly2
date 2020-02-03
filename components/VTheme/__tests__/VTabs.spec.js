b7f8850b133c771e841d75ca46b2dd8f2e22d64c
Walter Lim <waltissomewhere@gmail.com>
2020-02-03 13:11:27 +1300

    fix lint

--- components/VTheme/__tests__/VTabs.spec.js
+++ components/VTheme/__tests__/VTabs.spec.js
@@ -4,7 +4,6 @@ import VTabs from '../VTabs'
 import { Tabs } from 'antd'
 const { TabPane } = Tabs
 test('Check Tabs Render', t => {
-
   const TabCheck = mount(<VTabs><TabPane>potato</TabPane></VTabs>)
   t.true(TabCheck.exists())
 })
