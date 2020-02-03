8f5c1a2f1e4eae2adf637f237ff7e675bb7297eb
James Goodman <james.goodman@tec.govt.nz>
2020-02-03 15:02:22 +1300

    VP-1083 Remove unused ActivityFields constant

--- server/api/activity/activity.constants.js
+++ server/api/activity/activity.constants.js
@@ -4,25 +4,7 @@ const ActivityStatus = {
   RETIRED: 'retired'
 }
 
-const ActivityFields = {
-  ID: '_id',
-  NAME: 'name',
-  SUBTITLE: 'subtitle',
-  IMG_URL: 'imgUrl',
-  DESCRIPTION: 'description',
-  DURATION: 'duration',
-  LOCATION: 'location',
-  STATUS: 'status',
-  DATE: 'date',
-  OFFER_ORG: 'offerOrg',
-  REQUESTOR: 'requestor',
-  FROM_ACTIVITY: 'fromActivity',
-  DATE_ADDED: 'dateAdded',
-  TAGS: 'tags'
-}
-
 module.exports = {
   SchemaName: 'Activity',
-  ActivityStatus,
-  ActivityFields
+  ActivityStatus
 }
