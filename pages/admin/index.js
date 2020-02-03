99b06bb3098fa4bd526534b03d74c078935a3188
Andrew Watkins <andrew@groat.nz>
2020-02-03 22:33:50 +1300

    remove db api and ui to generate fake data.

--- pages/admin/index.js
+++
@@ -1,2 +0,0 @@
-import admin from './admin.js'
-export default admin
--- server/api/db/db.routes.js
+++
@@ -1,6 +0,0 @@
-const { dbAction } = require('./db.controller')
-
-module.exports = (server) => {
-  server.get('/api/db/:action/', dbAction)
-  server.post('/api/db/:action/', dbAction)
-}
