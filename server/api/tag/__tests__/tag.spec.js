4fdd3f159b4647666dfd747aa2a138cc8f61f539
Andrew Watkins <andrew@groat.nz>
2020-02-13 08:38:16 +1300

    add CASL abilties to tags

--- server/api/tag/__tests__/tag.spec.js
+++ server/api/tag/__tests__/tag.spec.js
@@ -4,37 +4,142 @@ import { server, appReady } from '../../../server'
 import Tag from '../tag'
 import MemoryMongo from '../../../util/test-memory-mongo'
 import tags from './tag.fixture.js'
+import { jwtData, jwtDataDali } from '../../../middleware/session/__tests__/setSession.fixture'
+import people from '../../person/__tests__/person.fixture'
+import Person from '../../person/person'
 
 test.before('before connect to database', async (t) => {
   t.context.memMongo = new MemoryMongo()
   await t.context.memMongo.start()
   await appReady
+
+  const tagCollection = [
+    { tags: tags },
+    { tags: ['one', 'two', 'three'] }
+  ]
+  t.context.people = await Person.create(people).catch((err) => console.error('Unable to create people', err))
+  t.context.tags = await Tag.create(tagCollection).catch((err) => console.error('Unable to create tags', err))
+  console.log(t.context.tags)
 })
 
 test.after.always(async (t) => {
+  await Tag.deleteMany()
   await t.context.memMongo.stop()
 })
 
-test.beforeEach('connect and add tag entries', async (t) => {
-  t.context.tags = await Tag.create({ tags: tags }).catch((err) => console.error('Unable to create tags', err))
-})
-
-test.afterEach.always(async () => {
-  await Tag.deleteMany()
-})
-
-test.serial('verify fixture database has tags', async t => {
+test('verify fixture database has tags', async t => {
   const p = await Tag.find()
-  t.is(1, p.length)
+  t.is(2, p.length)
   t.truthy(p[0].tags.includes('robots'))
 })
 
-test.serial('Should correctly give count of all tags', async t => {
+/* -- Anon person */
+test('Tag API - anon - create', async t => {
+  const response = await request(server)
+    .post('/api/tags')
+    .send({ tags: ['a', 'b', 'c'] })
+
+  t.is(response.statusCode, 403)
+})
+
+test('Tag API - anon - read', async t => {
   const res = await request(server)
     .get('/api/tags')
     .set('Accept', 'application/json')
+    .expect(403)
+  t.is(res.status, 403)
+})
+test('Tag API - anon - update', async t => {
+  const tag = t.context.tags[0]
+
+  const response = await request(server)
+    .put(`/api/tags/${tag._id}`)
+    .send({ tags: ['a', 'b', 'c'] })
+
+  t.is(response.statusCode, 403)
+})
+
+test('Tag API - anon - delete', async t => {
+  const tag = t.context.tags[0]
+
+  const response = await request(server)
+    .delete(`/api/tags/${tag._id}`)
+
+  t.is(response.statusCode, 403)
+})
+
+/* -- Signed in person */
+
+test('Tag API - signed - create', async t => {
+  const response = await request(server)
+    .post('/api/tags')
+    .set('Cookie', [`idToken=${jwtDataDali.idToken}`])
+    .send({ tags: ['a', 'b', 'c'] })
+
+  t.is(response.statusCode, 403)
+})
+
+test('Tag API - signed - read', async t => {
+  const res = await request(server)
+    .get('/api/tags')
+    .set('Accept', 'application/json')
+    .set('Cookie', [`idToken=${jwtDataDali.idToken}`])
     .expect(200)
     .expect('Content-Type', /json/)
+
   const got = res.body
   t.is(tags.length, got.length)
+})
+
+test('Tag API - signed - update', async t => {
+  const tag = t.context.tags[0]
+
+  const response = await request(server)
+    .put(`/api/tags/${tag._id}`)
+    .set('Cookie', [`idToken=${jwtDataDali.idToken}`])
+    .send({ tags: ['a', 'b', 'c'] })
+
+  t.is(response.statusCode, 403)
+})
+
+test('Tag API - signed - delete', async t => {
+  const tag = t.context.tags[0]
+
+  const response = await request(server)
+    .delete(`/api/tags/${tag._id}`)
+    .set('Cookie', [`idToken=${jwtDataDali.idToken}`])
+
+  t.is(response.statusCode, 403)
+})
+
+/* -- Admin person */
+
+test('Tag API - admin - CRUD', async t => {
+  let response = await request(server)
+    .post('/api/tags')
+    .set('Cookie', [`idToken=${jwtData.idToken}`])
+    .send({ tags: ['a', 'b', 'c'] })
+
+  t.is(response.statusCode, 200)
+  const newtags = response.body
+  const id = newtags._id
+
+  // get by id - non admin check
+  response = await request(server)
+    .put(`/api/tags/${id}`)
+    .set('Cookie', [`idToken=${jwtData.idToken}`])
+  t.is(response.statusCode, 200)
+
+  // update
+  response = await request(server)
+    .put(`/api/tags/${id}`)
+    .set('Cookie', [`idToken=${jwtData.idToken}`])
+    .send({ tags: ['a', 'b', 'c', 'd'] })
+  t.is(response.statusCode, 200)
+
+  response = await request(server)
+    .delete(`/api/tags/${id}`)
+    .set('Cookie', [`idToken=${jwtData.idToken}`])
+
+  t.is(response.statusCode, 200)
 })
--- server/api/tag/tag.controller.js
+++ server/api/tag/tag.controller.js
@@ -4,7 +4,10 @@ const Tag = require('./tag')
  * Creates one or more tags and sends a JSON response of the created objects
  */
 async function listTags (req, res) {
+  console.log('listTags', req.query)
   try {
+    // note: currently we just return the first tag collection.
+    // there's no way to identify other dictionaries.
     const fetched = await Tag.findOne({}, 'tags', { lean: true })
 
     let responseData = []
