dfb2ac7c7a6eafcba4962739d3ff09f8b7aa4509
James Goodman <james.goodman@tec.govt.nz>
2020-02-14 14:25:20 +1300

    VP-1089 Interest API org admin create permissions

--- server/api/interest/interest.ability.js
+++ server/api/interest/interest.ability.js
@@ -97,6 +97,10 @@ const ruleBuilder = async (session) => {
       subject: SchemaName,
       action: Action.READ,
       conditions: { opportunity: { $in: myOpportunityIds } }
+    }, {
+      subject: SchemaName,
+      action: Action.CREATE,
+      inverted: true
     })
   }
 
