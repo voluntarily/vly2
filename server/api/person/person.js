cb3db163662faee7d3e43df57e6d86b8a7e59700
Eteroa Lafaele <eteroalafaele080@gmail.com>
2020-02-15 16:26:13 +1300

    Removed the default in the type in person

--- server/api/person/person.js
+++ server/api/person/person.js
@@ -24,7 +24,7 @@ const personSchema = new Schema({
   facebook: { type: 'String' },
   twitter: { type: 'String' },
   education: { type: 'String' },
-  job: { type: 'String', default: '' },
+  job: { type: 'String' },
   sendEmailNotifications: { type: 'Boolean', default: true, required: true },
   role: {
     type: [String],
