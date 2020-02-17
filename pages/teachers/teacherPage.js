8b588ade063a0fef510f9b7ba2572e484a9f33f3
Walter Lim <waltissomewhere@gmail.com>
2020-02-17 13:35:51 +1300

    split out partners and fix lint

--- pages/teachers/teacherPage.js
+++ pages/teachers/teacherPage.js
@@ -1,7 +1,8 @@
-import { Button, Divider } from 'antd'
 import React, { Component } from 'react'
-import { Helmet } from 'react-helmet'
+
 import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+import { Button, Divider } from 'antd'
 import { FormattedMessage } from 'react-intl'
 import SectionTitle from '../../components/LandingPageComponents/SectionTitle'
 import PersonaSection from '../../components/LandingPageComponents/PersonaSection'
@@ -16,7 +17,12 @@ import {
 } from '../../components/VTheme/VTheme'
 import publicPage from '../../hocs/publicPage'
 import Hero2 from '../../components/LandingPageComponents/Hero2'
+import {
+  SponsorContainer,
+  SponsorIcon
+} from '../../components/LandingPageComponents/Partners'
 
+//annoying hack to make sure buttons dont inherit block width
 const ButtonContainer = styled.div`
   margin-top: 1rem;
   @media screen and (max-width: 768px) {
@@ -87,19 +93,6 @@ const ListItemImage = styled.img`
     order: -2;
   }
 `
-const SponsorIcon = styled.img`
-  object-fit: cover;
-  object-position: center;
-
-  height: 10rem;
-  width: 10rem;
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    object-fit: contain;
-    margin-right: 1rem;
-    margin-bottom: 1rem;
-  }
-`
 
 // Page-Specific Containers
 
@@ -135,18 +128,6 @@ const SectionGridItemLeft = styled.div`
   align-self: center;
   @media screen and (max-width: 768px) {
     order: -1;
-    margin-bottom: 4rem;
-  }
-`
-
-const SponsorContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 10rem);
-  grid-gap: 4rem;
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 2rem;
-    margin-top: 2rem;
     margin-bottom: 4rem;
   }
 `
