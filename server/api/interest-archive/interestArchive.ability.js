47a33a4dd1266431ce08947adb5166db6af78127
James Goodman <james.goodman@tec.govt.nz>
2020-02-17 15:25:53 +1300

    VP-1090 Interest archive API org admin permissions

--- server/api/interest-archive/interestArchive.ability.js
+++ server/api/interest-archive/interestArchive.ability.js
@@ -41,12 +41,41 @@ const ruleBuilder = async (session) => {
     })
   }
 
+  const orgAdminRules = []
+
+  if (session.me && session.me._id && session.me.role.includes(Role.ORG_ADMIN)) {
+    const myArchivedOpportunities = await ArchivedOpportunity.find({ offerOrg: { $in: session.me.orgAdminFor } })
+    const myArchivedOpportunityIds = myArchivedOpportunities.map(opportunity => opportunity._id.toString())
+
+    orgAdminRules.push({
+      subject: SchemaName,
+      action: Action.LIST,
+      conditions: { opportunity: { $in: myArchivedOpportunityIds } }
+    }, {
+      subject: SchemaName,
+      action: Action.READ,
+      conditions: { opportunity: { $in: myArchivedOpportunityIds } }
+    }, {
+      subject: SchemaName,
+      action: Action.CREATE,
+      inverted: true
+    }, {
+      subject: SchemaName,
+      action: Action.UPDATE,
+      conditions: { opportunity: { $in: myArchivedOpportunityIds } }
+    }, {
+      subject: SchemaName,
+      action: Action.DELETE,
+      inverted: true
+    })
+  }
+
   return {
     [Role.ANON]: anonRules,
     [Role.VOLUNTEER_PROVIDER]: allRules,
     [Role.OPPORTUNITY_PROVIDER]: opportunityProviderRules,
     [Role.ACTIVITY_PROVIDER]: allRules,
-    [Role.ORG_ADMIN]: allRules,
+    [Role.ORG_ADMIN]: orgAdminRules,
     [Role.ADMIN]: allRules
   }
 }
