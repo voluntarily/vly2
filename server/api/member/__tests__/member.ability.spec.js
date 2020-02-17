ed66e48774a231d1d0ff88dd697994efb6a66afd
James Goodman <james.goodman@tec.govt.nz>
2020-02-18 12:23:12 +1300

    VP-1092 Add member API permissions tests

---
+++ server/api/member/__tests__/member.ability.spec.js
@@ -0,0 +1,362 @@
+import test from 'ava'
+import request from 'supertest'
+import { server, appReady } from '../../../server'
+import MemoryMongo from '../../../util/test-memory-mongo'
+import { loadInterestFixtures, clearInterestFixtures, sessions } from './member.ability.fixture'
+import { MemberStatus } from '../member.constants'
+
+test.before('setup database and app', async (t) => {
+  t.context.memMongo = new MemoryMongo()
+  await t.context.memMongo.start()
+  await appReady
+})
+
+test.after.always(async (t) => {
+  await t.context.memMongo.stop()
+})
+
+test.beforeEach('populate database fixtures', async (t) => {
+  t.context.fixtures = await loadInterestFixtures()
+})
+
+test.afterEach.always(async () => {
+  await clearInterestFixtures()
+})
+
+const testScenarios = [
+  {
+    role: 'anon',
+    action: 'list',
+    makeRequest: async () => {
+      return request(server)
+        .get('/api/members')
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'anon',
+    action: 'read',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[0]._id}`)
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'anon',
+    action: 'create',
+    makeRequest: async (context) => {
+      return request(server)
+        .post('/api/members')
+        .send({
+          person: context.fixtures.people[0]._id,
+          organisation: context.fixtures.organisations[0]._id,
+          validation: 'Test validation',
+          status: MemberStatus.JOINER
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'anon',
+    action: 'update',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[0]._id}`)
+        .send({
+          validation: 'Updated test validation'
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'anon',
+    action: 'delete',
+    makeRequest: async (context) => {
+      return request(server).delete(`/api/members/${context.fixtures.members[0]._id}`)
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'list',
+    makeRequest: async () => {
+      return request(server)
+        .get('/api/members')
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+      t.is(response.body.length, 2)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'read (own membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'read (other\'s membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[2]._id}`)
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 404)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'create',
+    makeRequest: async (context) => {
+      return request(server)
+        .post('/api/members')
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+        .send({
+          organisation: context.fixtures.organisations[0]._id,
+          validation: 'Test validation',
+          status: MemberStatus.JOINER
+        })
+    },
+    assertions: (t, response) => {
+      t.fail('Need some additional tests around allowed status/person ids')
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'update (own membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+        .send({
+          status: MemberStatus.NONE
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'update (other\'s membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[2]._id}`)
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+        .send({
+          status: MemberStatus.NONE
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 404)
+    }
+  },
+  {
+    role: 'authenticated',
+    action: 'delete',
+    makeRequest: async (context) => {
+      return request(server)
+        .delete(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[1].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'list',
+    makeRequest: async () => {
+      return request(server)
+        .get('/api/members')
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+      t.is(response.body.length, 2)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'read (own membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'read (other\'s membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[2]._id}`)
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 404)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'create',
+    makeRequest: async (context) => {
+      return request(server)
+        .post('/api/members')
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+        .send({
+          person: context.fixtures.people[1]._id,
+          organisation: context.fixtures.organisations[0]._id,
+          validation: 'Test validation',
+          status: MemberStatus.MEMBER
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'update (own membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+        .send({
+          status: MemberStatus.ORGADMIN
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'update (other\'s membership)',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[2]._id}`)
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+        .send({
+          status: MemberStatus.ORGADMIN
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 404)
+    }
+  },
+  {
+    role: 'org admin',
+    action: 'delete',
+    makeRequest: async (context) => {
+      return request(server)
+        .delete(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[3].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 403)
+    }
+  },
+  {
+    role: 'admin',
+    action: 'list',
+    makeRequest: async () => {
+      return request(server)
+        .get('/api/members')
+        .set('Cookie', [`idToken=${sessions[0].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+      t.is(response.body.length, 3)
+    }
+  },
+  {
+    role: 'admin',
+    action: 'read',
+    makeRequest: async (context) => {
+      return request(server)
+        .get(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[0].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'admin',
+    action: 'create',
+    makeRequest: async (context) => {
+      return request(server)
+        .post('/api/members')
+        .set('Cookie', [`idToken=${sessions[0].idToken}`])
+        .send({
+          person: context.fixtures.people[2]._id,
+          organisation: context.fixtures.organisations[0]._id,
+          validation: 'Test validation',
+          status: MemberStatus.MEMBER
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'admin',
+    action: 'update',
+    makeRequest: async (context) => {
+      return request(server)
+        .put(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[0].idToken}`])
+        .send({
+          status: MemberStatus.NONE
+        })
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  },
+  {
+    role: 'admin',
+    action: 'delete',
+    makeRequest: async (context) => {
+      return request(server)
+        .delete(`/api/members/${context.fixtures.members[0]._id}`)
+        .set('Cookie', [`idToken=${sessions[0].idToken}`])
+    },
+    assertions: (t, response) => {
+      t.is(response.statusCode, 200)
+    }
+  }
+]
+
+for (const { role, action, makeRequest, assertions } of testScenarios) {
+  test.serial(`Interest Archive API - ${role} - ${action}`, async t => {
+    const response = await makeRequest(t.context)
+
+    assertions(t, response)
+  })
+}
