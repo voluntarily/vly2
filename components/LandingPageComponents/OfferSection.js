b206b02f98e6e74aff87301c5cf4703e10e44f29
Walter Lim <waltissomewhere@gmail.com>
2020-02-20 21:52:07 +1300

    initial offers

--- components/LandingPageComponents/OfferSection.js
+++ components/LandingPageComponents/OfferSection.js
@@ -1,42 +1,91 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
 import { FormattedMessage } from 'react-intl'
 import SectionTitle from '../../components/LandingPageComponents/SectionTitle'
 
-
 const OfferContainer = styled.div`
-display: grid;
-grid-template-columns: 1fr 1fr 1fr;
-gap: 2rem;
-margin-bottom: 8rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+  margin-bottom: 8rem;
 `
 
 const OfferCard = styled.div`
-box-shadow: 2px 2px 12px 0 rgba(190, 190, 190, 0.5);
-height: 10rem;
-background-color: pink;
+  background: #ffffff;
+  box-shadow: 2px 2px 16px 0 rgba(118, 118, 118, 0.5);
+  border-radius: 8px;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+  figcaption {
+    padding: 1rem;
+  }
+
+  h3 {
+    bottom: 0;
+    font-size: 1.5rem;
+    font-weight: bold;
+    letter-spacing: -0.38px;
+    line-height: 32px;
+
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    font-size: 1.1rem;
+  }
+
+  img {
+    border-radius: 8px;
+  }
+
+  :hover {
+
+    transform: scale(1.05);
+   
+  }
 `
 
 const OfferSection = () => (
-    <div>
+  <div>
     <SectionTitle>
-        <FormattedMessage
-          id='landing.sectiontitle.offers'
-          defaultMessage='What we offer'
-        />
-
-      </SectionTitle>
+      <FormattedMessage
+        id='landing.sectiontitle.offers'
+        defaultMessage='What we offer'
+      />
+    </SectionTitle>
     <OfferContainer>
-        
-        <OfferCard></OfferCard>
-        <OfferCard></OfferCard>
-        <OfferCard></OfferCard>
-        
-
+      <OfferCard>
+        <img src='/static/img/offers/volunteering.png' />
+        <figcaption>
+          <h3>Volunteering</h3>
+          <p>
+            Use your skills by volunteering to work with teachers and students
+            in schools
+          </p>
+        </figcaption>
+      </OfferCard>
+      <OfferCard>
+        {' '}
+        <img src='/static/img/offers/resources.png' />
+        <figcaption>
+          <h3>Resources</h3>
+          <p>
+            Organize activities for students in a few clicks with educational
+            activity templates and resources
+          </p>
+        </figcaption>
+      </OfferCard>
+      <OfferCard>
+        {' '}
+        <img src='/static/img/offers/recognition.png' />
+        <figcaption>
+          <h3>Recognition</h3>
+          <p>Get recognised for your volunteering when you use Voluntarily.</p>
+        </figcaption>
+      </OfferCard>
     </OfferContainer>
-    </div>
+  </div>
 )
 
 export default OfferSection
