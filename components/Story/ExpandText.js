11fc1cb78bef433dc957f92e1bc801508459e127
farjanacan <farjana@pamfergusson.org.nz>
2020-02-24 12:30:52 +1300

    read more becomes visible when text is long

--- components/Story/ExpandText.js
+++ components/Story/ExpandText.js
@@ -5,24 +5,25 @@ import { FormattedMessage } from 'react-intl'
 export const ExpandText = ({ storyBody, maxLength }) => {
   const [expand, setExpand] = useState(true)
 
-  if (storyBody <= maxLength) {
+  if (storyBody.length <= maxLength) {
     return (
       <P> {storyBody} </P>
     )
-  }
-  return (
-    <P>
-      {expand ? `${storyBody.substr(0, maxLength).trim()} ...` : storyBody}
-      {expand ? (
-        <a onClick={() => setExpand(false)}>
-          <FormattedMessage id='readMore' defaultMessage=' read more' description='hyperlink to expand a story' />
-        </a>
-      ) : (
-        <a onClick={() => setExpand(true)}>
-          <FormattedMessage id='readLess' defaultMessage=' read less' description='hyperlink to shrink a story' />
+  } else {
+    return (
+      <P>
+        {expand ? `${storyBody.substr(0, maxLength).trim()} ...` : storyBody}
+        {expand ? (
+          <a onClick={() => setExpand(false)}>
+            <FormattedMessage id='readMore' defaultMessage=' read more' description='hyperlink to expand a story' />
+          </a>
+        ) : (
+          <a onClick={() => setExpand(true)}>
+            <FormattedMessage id='readLess' defaultMessage=' read less' description='hyperlink to shrink a story' />
 
-        </a>
-      )}
-    </P>
-  )
+          </a>
+        )}
+      </P>
+    )
+  }
 }
