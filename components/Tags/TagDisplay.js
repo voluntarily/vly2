8db318d4ee2eb1dc18a46cdb941dbab0d7a957c3
Blair McClelland <blair.mcclelland@alphacertlabs.com>
2020-02-25 19:48:18 +1300

    VP-754, VP-859 - Allow multiple tags to be entered seperated by commas, matching styling of tags between TagInput and TagDisplay

--- components/Tags/TagDisplay.js
+++ components/Tags/TagDisplay.js
@@ -1,22 +1,5 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-
-const TagStyle = styled.div`
-width: auto;
-
-padding: 0 0.5rem;
-margin: 0.1rem;
-display: inline-block;
-position: relative;
-
-vertical-align: middle;
-font-size: 1.2rem;
-font-weight: 500;
-
-border-radius: 0.2rem;
-background-color: #e8e8e8;
-color: #666;
-`
+import { TagStyle } from '../VTheme/VTheme'
 
 export function TagDisplay ({ tags }) {
   if (!tags) return ''
