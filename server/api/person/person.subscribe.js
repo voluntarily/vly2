2f6a79b0ac15a260692a4db8b352c25f2e1e5e12
Andrew Watkins <andrew@groat.nz>
2020-02-26 19:30:16 +1300

    pub/sub TOPIC_MEMBER__UPDATE when membership changes and send emails

--- server/api/person/person.subscribe.js
+++ server/api/person/person.subscribe.js
@@ -1,7 +1,8 @@
 const { emailPerson } = require('./person.email')
 const { config } = require('../../../config/clientConfig')
 const PubSub = require('pubsub-js')
-const { TOPIC_PERSON__CREATE, TOPIC_PERSON__EMAIL_SENT } = require('../../services/pubsub/topic.constants')
+const { TOPIC_PERSON__CREATE, TOPIC_MEMBER__UPDATE, TOPIC_PERSON__EMAIL_SENT } = require('../../services/pubsub/topic.constants')
+const { MemberStatus } = require('../member/member.constants')
 
 /**
  * This will be easier to add more status without having too much if. All we need is add another folder in email template folder and the status will reference to that folder
@@ -21,6 +22,27 @@ module.exports = (server) => {
     const info = await emailPerson('welcome', person, {
       send: true,
       from: welcomeFrom
+    })
+    PubSub.publish(TOPIC_PERSON__EMAIL_SENT, info)
+  })
+
+  PubSub.subscribe(TOPIC_MEMBER__UPDATE, async (msg, member) => {
+    // a new member has been created or a member status has changed
+    // send email to let people know
+
+    // skip states without emails
+    if ([MemberStatus.NONE, MemberStatus.JOINER, MemberStatus.EXMEMBER]
+      .includes(member.status)) {
+      return
+    }
+    const org = member.organisation
+    org.href = `${config.appUrl}/orgs/${org._id}`
+    org.imgUrl = new URL(org.imgUrl, config.appUrl).href
+    const template = `member_${member.status}`
+    const info = await emailPerson(template, member.person, {
+      send: true,
+      from: welcomeFrom,
+      org
     })
     PubSub.publish(TOPIC_PERSON__EMAIL_SENT, info)
   })
