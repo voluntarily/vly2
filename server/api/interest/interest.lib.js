86fa7b49352fa325cc3842a554fb2bc136668192
Andrew Watkins <andrew@groat.nz>
2020-02-28 12:39:00 +1300

    populate sub sub doc correctly

--- server/api/interest/interest.lib.js
+++ server/api/interest/interest.lib.js
@@ -21,13 +21,14 @@ const getInterestDetail = async (interestID) => {
   // Get the interest and populate out key information needed for emailing
   const interestDetail = await Interest.findById(interestID)
     .populate({ path: 'person', select: personFields })
-    .populate({ path: 'opportunity', select: 'name requestor imgUrl date duration' })
-    .populate({ path: 'messages.author', select: 'nickname name email' })
-    .populate({ path: 'opportunity.requestor', select: personFields })
+    .populate({
+      path: 'opportunity',
+      select: 'name imgUrl date duration',
+      populate: { path: 'requestor', select: personFields }
+    })
+    .populate({ path: 'messages.author', select: 'nickname imgUrl' }) // just enough to label the message
     .exec()
 
-  // const requestorDetail = await Person.findById(interestDetail.opportunity.requestor, )
-  // interestDetail.opportunity.requestor = requestorDetail
   interestDetail.opportunity.imgUrl = new URL(interestDetail.opportunity.imgUrl, config.appUrl).href
   interestDetail.opportunity.href = `${config.appUrl + '/ops/' + interestDetail.opportunity._id}`
   interestDetail.person.href = `${config.appUrl + '/people/' + interestDetail.person._id}`
