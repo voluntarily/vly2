28f6905efd24a6dc4550f1efabcabdddf7a5ef7b
Walter Lim <waltissomewhere@gmail.com>
2020-03-02 18:04:20 +1300

    add test

--- components/Person/__tests__/PersonDetail.spec.js
+++ components/Person/__tests__/PersonDetail.spec.js
@@ -4,6 +4,7 @@ import { renderWithIntl } from '../../../lib/react-intl-test-helper'
 import objectid from 'objectid'
 import people from '../../../server/api/person/__tests__/person.fixture'
 import PersonDetail from '../PersonDetail'
+import { ActivityContainer } from '../../VTheme/VTheme'
 
 test.before('Setup People fixtures', (t) => {
   // not using mongo or server here so faking ids
@@ -20,4 +21,5 @@ test('render person details', t => {
   const wrapper = renderWithIntl(<PersonDetail person={t.context.me} />)
   t.truthy(wrapper.find('Head'))
   t.is(wrapper.find('h1').text(), t.context.me.name)
+  t.truthy(wrapper.find(ActivityContainer))
 })
