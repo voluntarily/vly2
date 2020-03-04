0af8e93fc4efe4e69b43dd9bb67442338e70dc18
Walter Lim <waltissomewhere@gmail.com>
2020-03-05 12:36:21 +1300

    try section

--- components/Close/GenericClose.js
+++ components/Close/GenericClose.js
@@ -1,11 +1,14 @@
 import { HalfGrid } from '../VTheme/VTheme'
 import CloseCard from './CloseActionCard'
+import { Divider } from 'antd'
 
 const GenericClose = () => (
   <HalfGrid>
     <img src='https://picsum.photos/640/480' />
     <div>
       <h2>Welcome to Voluntarily!</h2>
+      <Divider />
+      <h3>Try the following things next:</h3>
       <CloseCard />
     </div>
   </HalfGrid>
