3eef892c5219a4930d38622b96a24d8685f97a20
Walter Lim <waltissomewhere@gmail.com>
2020-03-05 12:14:38 +1300

    starting work on contewnt

--- components/Close/GenericClose.js
+++ components/Close/GenericClose.js
@@ -3,7 +3,7 @@ import { HalfGrid } from '../VTheme/VTheme'
 const GenericClose = () => (
   <HalfGrid>
     <img src='https://picsum.photos/640/480' />
-    <h1>Welcome to Voluntarily!</h1>
+    <h2>Welcome to Voluntarily!</h2>
   </HalfGrid>
 )
 
