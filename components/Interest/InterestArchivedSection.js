b09745434440eaff5cd59bdc816a9daa7f9105c2
Andrew Watkins <andrew@groat.nz>
2020-03-06 08:41:14 +1300

    map over InterestSection and InterestTable changes

--- components/Interest/InterestArchivedSection.js
+++ components/Interest/InterestArchivedSection.js
@@ -2,61 +2,66 @@
   Smart component. for the given opportunity gets a list of Interests
   and displays them in a table. actions change the state of the interested volunteers
 */
-// import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import { useEffect } from 'react'
 import InterestArchivedTable from './InterestArchivedTable'
 import { FormattedMessage } from 'react-intl'
 import PropTypes from 'prop-types'
-
-import reduxApi, { withInterestsArchived } from '../../lib/redux/reduxApi'
+import reduxApi from '../../lib/redux/reduxApi'
 import Loading from '../Loading'
+import { useSelector, useDispatch } from 'react-redux'
+import { InterestStatus, InterestAction } from '../../server/api/interest/interest.constants'
 
-class InterestArchivedSection extends Component {
-  async componentDidMount () {
-    // Get all interests
-    const opid = this.props.opid
-    try {
-      await this.props.dispatch(reduxApi.actions.interestsArchived.get({ op: opid }))
-    } catch (err) {
-      console.error('error in getting interests', err)
+export const InterestArchivedSection = ({ opid }) => {
+  const interests = useSelector(state => state.interestsArchived)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const getOpInterests = async () => {
+      await dispatch(reduxApi.actions.interestsArchived.get({ op: opid }))
     }
+    getOpInterests()
+  }, [opid])
+  // If we haven't finished making the API request to the server yet...
+  if (!interests.sync) {
+    return (<Loading label='interestsArchived' entity={interests} />)
   }
 
-  async markAsPresent (interest) {
-    interest.status = 'attended'
-    await this.props.dispatch(reduxApi.actions.interestsArchived.put({ id: interest._id }, { body: JSON.stringify(interest) }))
-  }
-
-  async markAsAbsent (interest) {
-    interest.status = 'not attended'
-    await this.props.dispatch(reduxApi.actions.interestsArchived.put({ id: interest._id }, { body: JSON.stringify(interest) }))
-  }
-
-  render () {
-    if (!this.props.interestsArchived.sync) {
-      return (<Loading label='interestsArchived' entity={this.props.interestsArchived} />)
+  const handleAction = async (updatedInterests, action, message) => {
+    const putInterest = {
+      ...(message && { messages: [{ body: message }] }),
+      type: action
     }
-    return (
-      <section>
-        <h2>
-          <FormattedMessage
-            id='interestArchiveSection.name'
-            defaultMessage='Volunteers'
-            description='label for interest table on op detail page'
-          />
-        </h2>
-        <InterestArchivedTable
-          interests={this.props.interestsArchived.data}
-          onPresent={this.markAsPresent.bind(this)}
-          onAbsent={this.markAsAbsent.bind(this)}
+    switch (action) {
+      case InterestAction.ACCEPT: // Attended
+        putInterest.status = InterestStatus.ATTENDED
+        break
+      case InterestAction.REJECT: // Not Attended
+        putInterest.status = InterestStatus.NOTATTENDED
+        break
+      case InterestAction.MESSAGE:
+        break
+    }
+    await dispatch(reduxApi.actions.interestsArchived.put({ id: updatedInterests._id }, { body: JSON.stringify(putInterest) }))
+  }
+
+  return (
+    <section>
+      <h2>
+        <FormattedMessage
+          id='interestArchiveSection.name'
+          defaultMessage='Volunteers'
+          description='label for interest table on op detail page'
         />
-      </section>
-    )
-  }
+      </h2>
+      <InterestArchivedTable
+        interests={interests.data}
+        onAction={handleAction}
+      />
+    </section>
+  )
 }
 InterestArchivedSection.propTypes = {
   opid: PropTypes.string.isRequired
 }
 
-export const InterestArchivedSectionTest = InterestArchivedSection
-export default withInterestsArchived(InterestArchivedSection)
+export default InterestArchivedSection
