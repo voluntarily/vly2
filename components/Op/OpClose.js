15064313f4b66f047e84cfc836029db51dd8b50c
Walter Lim <waltissomewhere@gmail.com>
2020-03-07 14:00:52 +1300

    intl strings for opclose

--- components/Op/OpClose.js
+++ components/Op/OpClose.js
@@ -3,6 +3,7 @@ import CloseCard from '../Close/CloseActionCard'
 import { Divider } from 'antd'
 import styled from 'styled-components'
 import OpPreview from './OpPreview'
+import { FormattedMessage } from 'react-intl'
 const CloseGrid = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr;
@@ -12,7 +13,6 @@ max-width: 1280px;
 img {
   width: 100%;
 }
-
 
 @media screen and (min-width: 768px) and (max-width: 1281px) {
     grid-template-columns: calc(50vw - 4rem) calc(50vw - 4rem);
@@ -33,11 +33,29 @@ const ActClose = () => (
   <CloseGrid>
     <img src='/static/img/goal/goal-first-volunteer.png' />
     <div>
-      <SmallHeader>Activity created</SmallHeader>
-      <h2>Volunteers can now see your activity and offer to help you out!</h2>
+      <SmallHeader>
+        <FormattedMessage
+          defaultMessage='Activity Created'
+          id='actCreated'
+          description='subheading for activity created'
+        />
+      </SmallHeader>
+      <h2>
+        <FormattedMessage
+          defaultMessage='Volunteers can now see your activity and offer to help you out!'
+          id='actCreatedTitle'
+          description='heading for activity created'
+        />
+      </h2>
       <OpPreview />
       <Divider />
-      <p>Try the following things next:</p>
+      <p>
+        <FormattedMessage
+          defaultMessage='Try the following things next:'
+          id='actCreatedSub'
+          description='heading for activity creation actions'
+        />
+      </p>
       <CloseCard cardTitle='Share Activity' cardDescription='Tell your community about your activity so they can get involved' imgLink='/static/img/icons/invite.svg' link='#' />
 
       <CloseCard cardTitle='See your Dashboard' cardDescription='See what you have signed up for' imgLink='/static/img/icons/home.svg' link='#' />
