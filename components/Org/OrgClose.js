c212d328c464ce6d10f28e6d0e7095ad55fbbea1
Walter Lim <waltissomewhere@gmail.com>
2020-03-07 14:13:36 +1300

    intl strings for orgclose

--- components/Org/OrgClose.js
+++ components/Org/OrgClose.js
@@ -2,6 +2,7 @@
 import CloseCard from '../Close/CloseActionCard'
 import { Divider } from 'antd'
 import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
 const CloseGrid = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr;
@@ -32,10 +33,29 @@ const OrgClose = () => (
   <CloseGrid>
     <img src='/static/img/goal/goal-first-volunteer.png' />
     <div>
-      <SmallHeader>School created</SmallHeader>
-      <h2>You can now start recruiting volunteers and bringing staff onto the platform!</h2>
+      <SmallHeader>
+
+        <FormattedMessage
+          defaultMessage='School Created'
+          id='orgSchoolSubheading'
+          description='subheading for school created'
+        />
+      </SmallHeader>
+      <h2>
+        <FormattedMessage
+          defaultMessage='You can now start recruiting volunteers and bringing staff onto the platform!'
+          id='orgSchoolHeading'
+          description='Heading for school created'
+        />
+      </h2>
       <Divider />
-      <p>Try the following things next:</p>
+      <p>
+        <FormattedMessage
+          defaultMessage='Try the following things next:'
+          id='orgSchoolActionHeading'
+          description='Heading for school next actions'
+        />
+      </p>
       <CloseCard cardTitle='Try an inspiring the future activity' cardDescription='Invite volunteers into your school to talk about their careers. Inspire. Excite. Ignite.' imgLink='/static/img/actions/itf.png' link='#' />
       <CloseCard cardTitle='Invite Staff' cardDescription='Invite the rest of your school staff to use the platform and call in skilled volunteers as well' imgLink='/static/img/icons/invite.svg' link='#' />
       <CloseCard
