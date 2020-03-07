126e1521bf5b0abd47d3ebea63aec465d6b3c82e
Walter Lim <waltissomewhere@gmail.com>
2020-03-07 15:14:48 +1300

    fix typo

--- components/Close/__tests__/CloseActionCard.spec.js
+++ components/Close/__tests__/CloseActionCard.spec.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import { shallow } from 'enzyme'
 import CloseCard from './CloseActionCard'
 
-test('OpClose renders cards ', t => {
+test('CloseCard renders Data ', t => {
   const wrapper = shallow(<CloseCard imgLink='/static/img/194px-Testcard_F.png' cardTitle='Bake Potatoes' cardDescription='We will Bake Potatoes' />)
   t.is(wrapper.find('img'), '/static/img/194px-Testcard_F.png')
   t.is(wrapper.find('strong').text(), 'Bake Potatoes')
