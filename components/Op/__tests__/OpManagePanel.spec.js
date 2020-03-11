0478dd4ec800d3f78e08b689911c6ed00559c723
Andrew Watkins <andrew@groat.nz>
2020-03-11 14:46:36 +1300

    manage panel changes for archived ops. + test

---
+++ components/Op/__tests__/OpManagePanel.spec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import test from 'ava'
+import { shallowWithIntl } from '../../../lib/react-intl-test-helper'
+import { OpManagePanel } from '../OpManagePanel'
+import { OpportunityStatus } from '../../../server/api/opportunity/opportunity.constants'
+
+test('render OpManagePanel for active op', t => {
+  const op = {
+    _id: 'fakeid',
+    name: 'Test Opportunity',
+    status: OpportunityStatus.ACTIVE
+  }
+  const wrapper = shallowWithIntl(<OpManagePanel op={op} />)
+  t.is(wrapper.find('h2').first().find('FormattedMessage').props().defaultMessage, 'Volunteers')
+  t.true(wrapper.exists('InterestSection'))
+  t.true(wrapper.exists('Connect(OpCloseOpportunity)'))
+})
+
+test('render OpManagePanel for completed op', t => {
+  const op = {
+    _id: 'fakeid',
+    name: 'Test Opportunity',
+    status: OpportunityStatus.COMPLETED
+  }
+  const wrapper = shallowWithIntl(<OpManagePanel op={op} />)
+  t.is(wrapper.find('h2').first().find('FormattedMessage').props().defaultMessage, 'Volunteers')
+  t.true(wrapper.exists('InterestArchivedSection'))
+})
