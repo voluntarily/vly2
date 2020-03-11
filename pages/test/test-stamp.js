79fb3a44b3bb64236cdf40066a8e6e4f2537cbc7
Andrew Watkins <andrew@groat.nz>
2020-03-11 14:47:17 +1300

    simple overlay stamp component

---
+++ pages/test/test-stamp.js
@@ -0,0 +1,28 @@
+import publicPage from '../../hocs/publicPage'
+import { FullPage } from '../../components/VTheme/VTheme'
+import styled from 'styled-components'
+import { Stamp } from '../../components/VTheme/Stamp'
+
+export const Rect = styled.div`
+  display: block; /* Hidden by default */
+  width: 20rem; 
+  height: 20rem;
+  margin: 1rem;
+  padding: 1rem;
+  border: solid;
+  background-color: ivory; 
+  position: relative;
+  
+`
+
+const TestPublicPage = ({ locale, session, isAuthenticated }) =>
+  <FullPage>
+    <h1>Simple Public Page</h1>
+    <Rect>
+      <p>This is the content of the rectangle </p>
+      <Stamp>Completed</Stamp>
+    </Rect>
+
+  </FullPage>
+
+export default publicPage(TestPublicPage)
