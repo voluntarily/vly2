f8a39c38f4c17935aea57aa9c79580d2b63c65f0
Andrew Watkins <andrew@groat.nz>
2020-03-17 15:15:30 +1300

    lint fix

--- x/db/perf/sentences.js
+++ x/db/perf/sentences.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 const path = `${__dirname}/sentences.txt`
 const sentences = []
 
-fs.readFileSync(path, 'utf-8').split(/\r?\n/).forEach(line => {sentences.push(line)})
+fs.readFileSync(path, 'utf-8').split(/\r?\n/).forEach(line => { sentences.push(line) })
 
 console.log(sentences.length)
