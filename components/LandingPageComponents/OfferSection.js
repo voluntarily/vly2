56e9cd39e53b61eafca7a8d68abfe3724ae4d521
Walter Lim <waltissomewhere@gmail.com>
2020-03-22 22:19:03 +1300

    add back it calls to action to join the build

--- components/LandingPageComponents/OfferSection.js
+++ components/LandingPageComponents/OfferSection.js
@@ -50,7 +50,9 @@ width: 100%;
   p {
     font-size: 1.1rem;
   }
-
+  small {
+  font-size: 1rem
+}
  
 
   :hover {
@@ -89,12 +91,37 @@ const OfferSection = () => (
 
     <OfferContainer>
 
+      <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz'>
+        <OfferCard>
+
+          <img src='./static/img/icons/code-party.svg' />
+          <figcaption>
+            <h3>Join the movement</h3>
+            <p>The entire project is open-source, and everyone is welcome to join. Click for more info.
+
+            </p>
+          </figcaption>
+        </OfferCard>
+      </a>
+      <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz/get-involved'>
+        <OfferCard>
+
+          <img src='./static/img/icons/chat.svg' />
+          <figcaption>
+            <h3>Join the conversation</h3>
+            <p>Join the community chat on Slack and help shape the future of the platform
+
+            </p>
+          </figcaption>
+        </OfferCard>
+      </a>
       {/* <Link href='/search'> */}
 
       <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz/pre-register'>
 
         <OfferCard>
           <figcaption>
+            <small>Coming soon</small>
             <h3>Can you help out?</h3>
             <p>
             Want to volunteer your time, or resources to help out someone in need? See what is needed and offer your skills.
@@ -108,6 +135,8 @@ const OfferSection = () => (
         <OfferCard>
 
           <figcaption>
+
+            <small>Coming soon</small>
             <h3>Do you need a hand?</h3>
             <p>It’s okay to ask. We make it easy with templates that will help you communicate exactly what you need.
             </p>
