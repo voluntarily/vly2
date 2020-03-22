d3c7d39a5732c0d675712b199456fd11f11ab929
Walter Lim <waltissomewhere@gmail.com>
2020-03-23 10:12:57 +1300

    submit

--- components/LandingPageComponents/OfferSection.js
+++ components/LandingPageComponents/OfferSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-// import Link from 'next/link'
+import Link from 'next/link'
 import { Button } from 'antd'
 
 const OfferContainer = styled.div`
@@ -35,7 +35,7 @@ width: 100%;
   border-radius: 8px;
   -webkit-transition: all 0.2s;
   transition: all 0.2s;
-
+min-height: 292px;
 
   h3 {
     bottom: 0;
@@ -72,6 +72,7 @@ width: 100%;
   @media screen and (max-width: 768px) {
     min-height: initial;
 
+
   padding: 2rem;
     width: calc(100vw - 2rem);
 
@@ -96,13 +97,28 @@ const OfferSection = () => (
 
           <img src='./static/img/icons/code-party.svg' />
           <figcaption>
-            <h3>Join the movement</h3>
+            <h3>Build the movement</h3>
             <p>The entire project is open-source, and everyone is welcome to join. Click for more info.
 
             </p>
           </figcaption>
         </OfferCard>
       </a>
+
+      <Link href='/home'>
+
+        <a>
+
+          <OfferCard>
+            <figcaption>
+              <h3>Offer to help</h3>
+              <p>
+            Want to volunteer your time, or resources to help out someone in need? See what is needed and offer your skills.
+              </p><Button size='large' shape='round' type='primary' style={{ marginTop: '1rem' }}>See how you can help</Button>
+            </figcaption>
+          </OfferCard>
+        </a>
+      </Link>
       <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz/get-involved'>
         <OfferCard>
 
@@ -115,21 +131,7 @@ const OfferSection = () => (
           </figcaption>
         </OfferCard>
       </a>
-      {/* <Link href='/search'> */}
 
-      <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz/pre-register'>
-
-        <OfferCard>
-          <figcaption>
-            <small>Coming soon</small>
-            <h3>Can you help out?</h3>
-            <p>
-            Want to volunteer your time, or resources to help out someone in need? See what is needed and offer your skills.
-            </p><Button size='large' shape='round' type='primary' style={{ marginTop: '1rem' }}>See how you can help</Button>
-          </figcaption>
-        </OfferCard>
-      </a>
-      {/* </Link> */}
       {/* <Link href='/act'> */}
       <a rel='noopener noreferrer' target='_blank' href='https://blog.voluntarily.nz/pre-register'>
         <OfferCard>
@@ -137,8 +139,8 @@ const OfferSection = () => (
           <figcaption>
 
             <small>Coming soon</small>
-            <h3>Do you need a hand?</h3>
-            <p>It’s okay to ask. We make it easy with templates that will help you communicate exactly what you need.
+            <h3>Ask for help</h3>
+            <p>Coming this week. We make it easy to ask for exactly what you need.
             </p>
             <Button size='large' shape='round' type='primary' style={{ marginTop: '1rem' }}>See what help is available</Button>
           </figcaption>
