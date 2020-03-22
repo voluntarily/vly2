80b71b2fad69161c7719f0f98d01ed7798c96297
Andrew Watkins <andrew@groat.nz>
2020-03-23 05:55:45 +1300

    re-instate testcafe, move codecept

--- systemtest/landing.test.js
+++ systemtest/landing.test.js
@@ -1,11 +1,11 @@
 import { ReactSelector, waitForReact } from 'testcafe-react-selectors'
 const { config } = require('../config/clientConfig')
 
-// fixture`Getting Started` // eslint-disable-line no-undef
-//   .page`http://localhost:${config.serverPort}/`
-//   .beforeEach(async () => {
-//     await waitForReact(30000)
-//   })
+fixture`Getting Started` // eslint-disable-line no-undef
+  .page`http://localhost:${config.serverPort}/`
+  .beforeEach(async () => {
+    await waitForReact(30000)
+  })
 
 test('Test we can search from landing page', async t => { // eslint-disable-line no-undef
   await t
