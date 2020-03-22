88c4c392443be7cbd1b31c7ba75600e5f0674097
Mitch Weber <michael.weber@nefkom.net>
2020-03-22 14:12:24 +1300

    VP-1484 Refactor old templates

--- server/api/person/person.subscribe.js
+++ server/api/person/person.subscribe.js
@@ -11,6 +11,7 @@ const {
 const { MemberStatus } = require('../member/member.constants')
 const { InterestStatus } = require('../interest/interest.constants')
 const { getICalendar } = require('../opportunity/opportunity.calendar')
+const { OpportunityType } = require('../opportunity/opportunity.constants')
 const Organisation = require('../organisation/organisation')
 const { addMember } = require('../member/member.lib')
 
@@ -88,7 +89,7 @@ module.exports = (server) => {
       op.href = `${config.appUrl}/ops/${op._id}`
 
       interest.person.href = `${config.appUrl}/home`
-      const template = `interest_vp_${interest.type}_${interest.status}`
+      const template = `interest_vp_${op.type || OpportunityType.ASK}_${interest.type}_${interest.status}`
       const message = interest.messages.slice(-1)[0] // last element should be most recent
       const props = { from: op.requestor, op, interest, message }
       if (interest.status === InterestStatus.INVITED) {
@@ -111,7 +112,7 @@ module.exports = (server) => {
       op.requestor.href = `${config.appUrl}/home`
       op.href = `${config.appUrl}/ops/${op._id}`
 
-      const template = `interest_op_${interest.type}_${interest.status}`
+      const template = `interest_op_${op.type || OpportunityType.ASK}_${interest.type}_${interest.status}`
       const message = interest.messages.slice(-1)[0] // last element should be most recent
       const props = { from: interest.person, op, interest, message }
       const info = await emailPerson(template, op.requestor, props)
