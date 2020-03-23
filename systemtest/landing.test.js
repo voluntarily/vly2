75b33b951407f91f1a8d05e5b547d9cf000c9826
Andrew Somervell <andrew.somervell@practiv.com>
2020-03-23 18:44:04 +1300

    It's a real test case, honest

--- systemtest/landing.test.js
+++ systemtest/landing.test.js
@@ -1,4 +1,4 @@
-import { waitForReact } from 'testcafe-react-selectors'
+import { ReactSelector, waitForReact } from 'testcafe-react-selectors'
 const { config } = require('../config/clientConfig')
 
 fixture`Getting Started` // eslint-disable-line no-undef
@@ -7,9 +7,9 @@ fixture`Getting Started` // eslint-disable-line no-undef
     await waitForReact(30000)
   })
 
-// test('Test we can search from landing page', async t => { // eslint-disable-line no-undef
+  test('Test we can search from landing page', async t => { // eslint-disable-line no-undef
 //   await t
 //     .typeText(ReactSelector('Search'), 'robots')
 //     .pressKey('enter')
 //     .expect(ReactSelector('SearchPage SectionSubtitle').innerText, { timeout: 10000 }).eql('Search results for "robots"')
-// })
+  })
