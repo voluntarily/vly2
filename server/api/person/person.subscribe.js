ddc0c2f09eba1ed4ecbc9acab2b231f691da7827
Andrew Watkins <andrew@groat.nz>
2020-03-23 16:54:47 +1300

    Person create adds member to core org

--- server/api/person/person.subscribe.js
+++ server/api/person/person.subscribe.js
@@ -43,7 +43,8 @@ module.exports = (server) => {
     await addMember({
       person: person._id.toString(),
       organisation: coreOrganisation._id.toString(),
-      validation: SYSTEM_MEMBER_VALIDATION_MESSAGE
+      validation: SYSTEM_MEMBER_VALIDATION_MESSAGE,
+      status: MemberStatus.MEMBER
     })
   })
 
