8575db25aefbc909af36818f85e94d0d2e360ef4
Andrew Watkins <andrew@groat.nz>
2020-03-26 06:21:40 +1300

    useWindowSize hook and vertical menu

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -3,21 +3,21 @@ import Link from 'next/link'
 import { withRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-
+import { useWindowSize } from '../../lib/useWindowSize'
+const { SubMenu, Item } = Menu
 const VMenu = styled(Menu)`
 border-bottom: 2px solid transparent;
-
+border-right: none;
 .ant-menu-item {
   border: none;
   
 }
 
 @media screen and (max-width: 767px) {
-    display: none;
+    // display: none;
   }
 `
-
-const Navigation = ({ items, defaultItem, router, me, ...props }) => {
+export const NavigationH = ({ items, router }) => {
   const activeItem = router.pathname.slice(1)
   return (
     <VMenu
@@ -36,6 +36,39 @@ const Navigation = ({ items, defaultItem, router, me, ...props }) => {
 
     </VMenu>
   )
+}
+export const NavigationV = ({ items, router }) => {
+  const activeItem = router.pathname.slice(1)
+  return (
+    <VMenu
+      theme='light'
+      mode='inline'
+      // style={{ float: 'right' }}
+      selectedKeys={[activeItem]}
+    >
+      <SubMenu
+        key='sub1'
+        title='☰'
+      >
+        {items.map(item => (
+          <Item key={item.key}>
+            <Link key={item.href} href={item.href}>
+              <a>{item.text}</a>
+            </Link>
+          </Item>
+        ))}
+      </SubMenu>
+
+    </VMenu>
+  )
+}
+
+const COLLAPSE_MENU_WIDTH = 767
+const Navigation = (props) => {
+  const [width] = useWindowSize()
+  return (width < COLLAPSE_MENU_WIDTH)
+    ? <NavigationV {...props} />
+    : <NavigationH {...props} />
 }
 
 Navigation.propTypes = {
