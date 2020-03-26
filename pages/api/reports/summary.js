5e42eb0732369de750fdd135c3c551ad00f3a6da
Josh McArthur <joshua.mcarthur@gmail.com>
2020-03-26 19:04:36 +1300

    Refactor reports/summary to use Promise.all; add interest and activity counts

--- pages/api/reports/summary.js
+++ pages/api/reports/summary.js
@@ -1,16 +1,28 @@
-const Opportunity = require('../../../server/api/opportunity/opportunity')
-const { OpportunityType, OpportunityStatus } = require('../../../server/api/opportunity/opportunity.constants')
 const Person = require('../../../server/api/person/person')
+const Activity = require('../../../server/api/activity/activity')
+const { Interest } = require('../../../server/api/interest/interest')
+const Opportunity = require('../../../server/api/opportunity/opportunity')
+
+const { OpportunityType, OpportunityStatus } = require('../../../server/api/opportunity/opportunity.constants')
+const { ActivityStatus } = require('../../../server/api/activity/activity.constants')
 
 export default async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
-  const opAskCount = await Opportunity.countDocuments({ status: OpportunityStatus.ACTIVE, type: OpportunityType.ASK })
-  const opOfferCount = await Opportunity.countDocuments({ status: OpportunityStatus.ACTIVE, type: OpportunityType.OFFER })
-  const personCount = await Person.countDocuments({ })
+  const operations = [
+    Opportunity.countDocuments({ status: OpportunityStatus.ACTIVE, type: OpportunityType.ASK }).exec(),
+    Opportunity.countDocuments({ status: OpportunityStatus.ACTIVE, type: OpportunityType.OFFER }).exec(),
+    Person.countDocuments({ }).exec(),
+    Interest.countDocuments({ }).exec(),
+    Activity.countDocuments({status: ActivityStatus.ACTIVE}).exec()
+  ];
 
-  const result = {
-    Opportunity: { [OpportunityType.ASK]: opAskCount, [OpportunityType.OFFER]: opOfferCount },
-    Person: personCount
-  }
-  res.send(result)
+  return Promise.all(operations).then(
+    ([askCount, offerCount, personCount, interestCount, activityCount]) =>
+      res.send({
+        Person: personCount,
+        Opportunity: { [OpportunityType.ASK]: askCount, [OpportunityType.OFFER]: offerCount },
+        Interest: interestCount,
+        Activity: activityCount
+      })
+  );
 }
