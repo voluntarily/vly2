d109c53328f813fe5b8c8c68284c86a8d6ba275e
Josh McArthur <joshua.mcarthur@gmail.com>
2020-03-26 22:12:59 +1300

    Add unit tests for reports/summary API handler

---
+++ __tests__/api/reports/summary.spec.js
@@ -0,0 +1,39 @@
+import test from 'ava'
+import MockExpressRequest from 'mock-express-request'
+import MockExpressResponse from 'mock-express-response'
+import MemoryMongo from '../../../server/util/test-memory-mongo'
+import { OpportunityType } from '../../../server/api/opportunity/opportunity.constants'
+
+import summary from '../../../pages/api/reports/summary'
+import sinon from 'sinon'
+
+test.before('start in memory mongo and create fake server', async (t) => {
+  t.context.memMongo = new MemoryMongo()
+  await t.context.memMongo.start()
+})
+
+test.after.always(async (t) => {
+  t.context.memMongo && (await t.context.memMongo.stop())
+})
+
+test('Should respond with summary data for an admin', async t => {
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.ability = { can: sinon.fake.returns(true) }
+  await summary(req, res)
+  t.deepEqual(res._getJSON(), {
+    Person: 0,
+    Activity: 0,
+    Interest: 0,
+    Opportunity: { [OpportunityType.ASK]: 0, [OpportunityType.OFFER]: 0 }
+  })
+  t.is(200, res.statusCode, 'OK Response')
+})
+
+test('Should require the logged in user is an admin', async t => {
+  const req = new MockExpressRequest()
+  const res = new MockExpressResponse()
+  req.ability = { can: sinon.fake.returns(false) }
+  await summary(req, res)
+  t.is(401, res.statusCode, 'Authorisation required')
+})
