569a242b08b1cf17ab30fa23a835e296cbea7624
Walter Lim <waltissomewhere@gmail.com>
2020-03-27 22:51:55 +1300

    cleanup

--- components/Op/InterestedOpsSection.js
+++ components/Op/InterestedOpsSection.js
@@ -42,16 +42,16 @@ export const InterestedOpsSection = () => {
   return (
     <>
       <ProfileSection>
-      <ProfileSectionTitle>
-        <FormattedMessage
-          id='interestedOpsSection.title'
-          defaultMessage='You offered to help with'
-          description='Subtitle for home page for the volunteers current ops'
-        />
-        <small>Activities you have offered to help out with are listed below:</small>
-      </ProfileSectionTitle>
-      <OpList ops={ops} />
-    </ProfileSection>
+        <ProfileSectionTitle>
+          <FormattedMessage
+            id='interestedOpsSection.title'
+            defaultMessage='You offered to help with'
+            description='Subtitle for home page for the volunteers current ops'
+          />
+          <small>Activities you have offered to help out with are listed below:</small>
+        </ProfileSectionTitle>
+        <OpList ops={ops} />
+      </ProfileSection>
       <Divider />
     </>
   )
