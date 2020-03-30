4cde52caa8a0e798cae20d0a620cc2346bdb168d
Walter Lim <waltissomewhere@gmail.com>
2020-03-30 20:47:31 +1300

    push fixes to copy

--- pages/terms/privacypage.js
+++ pages/terms/privacypage.js
@@ -9,7 +9,7 @@ const PrivacyPage = () => (
       <h1>We care about privacy</h1>
       <Divider />
       <p>
-      Your privacy is really important to us, as is the privacy of all the people who use the Website. Our Privacy Policy follows some basic principles:
+      Your privacy is really important to us, as is the privacy of all the people who use the Website. Our <a href='/terms/privacy' target='_blank' rel='noreferrer noopener'> Privacy Policy</a> follows some basic principles:
       </p>
 
       <p />
@@ -20,14 +20,14 @@ const PrivacyPage = () => (
         <img src='../static/img/icons/chat.svg' />
         <p>Other Website users might share personal details with you.<br />Respect their privacy and don’t share their details unless they allow you to.</p>
         <img src='../static/img/icons/kind.svg' />
-        <p>The platform is intended for use only by people who are at least 16 years old. We will do everything we can to keep you personal information safe.</p>
+        <p>The platform is intended for use only by people who are at least 16 years old.<br />We will do everything we can to keep you personal information safe.</p>
       </InfoItem>
-      <aside>View the full<a href='/terms/privacy' target='_blank' rel='noreferrer noopener'> Privacy Policy</a></aside><br />
+
       {/* <Link>
         <a> */}
       <TermsButtonContainer>
         <div>
-          <Button type='primary' shape='round' size='large' block>Accept Privacy Policy</Button>
+          <Button type='primary' shape='round' size='large' block>Continue</Button>
         </div>
       </TermsButtonContainer>
       {/*   </a>
