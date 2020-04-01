3777319d22e94c120dea1734e28dce94ffc4162b
Andrew Watkins <andrew@groat.nz>
2020-04-02 11:01:14 +1300

    add useState example

--- pages/test/test-state.js
+++ pages/test/test-state.js
@@ -1,7 +1,9 @@
 import { A4 } from '../../components/VTheme/VTheme'
 import LocalStateTest from '../../components/testredux/LocalStateTest.js'
+import LocalUseStateTest from '../../components/testredux/LocalUseStateTest.js'
 import ReduxStoreTest from '../../components/testredux/ReduxStoreTest'
 import ReduxAsyncTest from '../../components/testredux/ReduxAsyncTest'
+
 const Section = (props) =>
   <section>
     <h1>{props.title}</h1>
@@ -12,6 +14,7 @@ const Section = (props) =>
 const TestReduxPage = () =>
   <A4>
     <Section title='Local State'><LocalStateTest /></Section>
+    <Section title='Local Use State'><LocalUseStateTest /></Section>
     <Section title='Redux Store'><ReduxStoreTest /></Section>
     <Section title='Redux Async'><ReduxAsyncTest /></Section>
   </A4>
