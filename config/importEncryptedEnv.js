b2a1b825af35d561635a96b7e9c31bcc0200c128
Zach Sim <ZachS@datacom.co.nz>
2020-04-07 15:09:45 +1200

    Fix formatting and remove unused import.

--- config/importEncryptedEnv.js
+++ config/importEncryptedEnv.js
@@ -2,23 +2,22 @@ const nodecipher = require('node-cipher')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
 const rimraf = require('rimraf')
-const { envHasKey } = require('./env')
 
 // importEnv(path?, secret?)
 // This will decrypt using node-cipher and import environment variables using dotenv.
 //
 // path is an optional variable and will default to process.env.ENV_PATH
 // secret is an optional variable and will default to process.env.ENV_SECRET
-const importEnv = (env_name = process.env.ENV_ENVIRONMENT, secret = process.env.ENV_SECRET) => {
-  if(env_name && secret) {
-    const input = `${env_name}.env.enc`;
-    const output = `${env_name}.env`;
+const importEnv = (envName = process.env.ENV_ENVIRONMENT, secret = process.env.ENV_SECRET) => {
+  if (envName && secret) {
+    const input = `${envName}.env.enc`
+    const output = `${envName}.env`
 
     nodecipher.decryptSync({
       input,
       output,
       password: secret
-    });
+    })
 
     const myEnv = dotenv.config({ path: output })
     dotenvExpand(myEnv)
