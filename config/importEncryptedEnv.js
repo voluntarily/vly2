a3783c2fd1dfd6ad27828e31a18faf3f111a8460
Zach Sim <ZachS@datacom.co.nz>
2020-04-07 14:31:45 +1200

    Switch to utilise node-cipher instead of secure-env

--- config/importEncryptedEnv.js
+++ config/importEncryptedEnv.js
@@ -1,21 +1,22 @@
-const secureEnv = require('secure-env')
+const nodecipher = require('node-cipher')
+const dotenv = require('dotenv')
+const dotenvExpand = require('dotenv-expand')
 const { envHasKey } = require('./env')
 
 // importEnv(path?, secret?)
-// This will decrypt and import environment variables using secure-env.
+// This will decrypt using node-cipher and import environment variables using dotenv.
 //
 // path is an optional variable and will default to process.env.ENV_PATH
 // secret is an optional variable and will default to process.env.ENV_SECRET
-const importEnv = (path = process.env.ENV_PATH, secret = process.env.ENV_SECRET) => {
-  const decryptedEnv = secureEnv({ path, secret })
-  // Merge decrypted environment variables into process.env
-  Object.entries(decryptedEnv).forEach(([key, value]) => {
-    if (!envHasKey(key)) {
-      process.env[key] = value
-    } else {
-      console.warn(`"${key}" has already been defined won't be overwritten`)
-    }
-  })
+const importEnv = (env_name = process.env.ENV_ENVIRONMENT, secret = process.env.ENV_SECRET) => {
+  nodecipher.decryptSync({
+    input: `${env_name}.env.enc`,
+    output: `${env_name}.env`,
+    password: secret
+  });
+
+  var myEnv = dotenv.config({ path: `${env_name}.env` })
+  dotenvExpand(myEnv)
 }
 
 // Public API
