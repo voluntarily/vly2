e834e5d07eef1daa67cdd4ff27e2352f92ad0ed6
Mitch Weber <michael.weber@nefkom.net>
2020-04-10 18:23:29 +1200

    Show different titles for ask/offers

--- __tests__/act/actlistpage.spec.js
+++ __tests__/act/actlistpage.spec.js
@@ -5,6 +5,7 @@ import { shallowWithIntl } from '../../lib/react-intl-test-helper'
 import acts from '../../server/api/activity/__tests__/activity.fixture'
 import objectid from 'objectid'
 import withMockRoute from '../../server/util/mockRouter'
+import * as nextRouter from 'next/router'
 import sinon from 'sinon'
 
 test.before('Setup fixtures', (t) => {
@@ -33,14 +34,49 @@ test.before('Setup fixtures', (t) => {
   }
 })
 
-test('render ActListPage', async t => {
-  // const props = await ActListPage.getInitialProps({ store: t.context.store })
-  const RoutedActListPage = withMockRoute(ActListPage)
+test.afterEach(() => {
+  sinon.restore()
+})
+
+test.serial('render ActListPage for asks', async t => {
+  mockUseRouter('ask')
+  const RoutedActListPage = withMockRoute(ActListPage, 'a/ask', { type: 'ask' })
 
   const outer = shallowWithIntl(<RoutedActListPage />)
   const router = outer.props().router
   router.push = sinon.spy()
   const wrapper = outer.dive()
-  t.is(wrapper.find('h1 FormattedMessage').first().props().id, 'ActListPage.Title')
+  t.is(wrapper.find('h1 FormattedMessage').first().props().id, 'ActListPage.Ask.Title')
   t.true(wrapper.exists('ActListSection'))
 })
+
+test.serial('render ActListPage for offers', async t => {
+  mockUseRouter('offer')
+  const RoutedActListPage = withMockRoute(ActListPage, 'a/offer', { type: 'offer' })
+
+  const outer = shallowWithIntl(<RoutedActListPage />)
+  const router = outer.props().router
+  router.push = sinon.spy()
+  const wrapper = outer.dive()
+  t.is(wrapper.find('h1 FormattedMessage').first().props().id, 'ActListPage.Offer.Title')
+  t.true(wrapper.exists('ActListSection'))
+})
+
+const mockUseRouter = (type) => {
+  const router = () => {
+    return ({
+      pathname: `/a/${type}`,
+      route: `/${type}`,
+      query: { type },
+      asPath: `/a/${type}`,
+      initialProps: {},
+      pageLoader: sinon.fake(),
+      App: sinon.fake(),
+      Component: sinon.fake(),
+      replace: sinon.fake(),
+      push: sinon.fake(),
+      back: sinon.fake()
+    })
+  }
+  sinon.replace(nextRouter, 'useRouter', router)
+}
