79ab24d2e37e1fd824af61b8cf3ef5a66ab86537
Andrew Somervell <andrew.somervell@practiv.com>
2020-04-11 13:25:54 +1200

    update to latest master, make font scaling work nicely in hero, other fixes

--- components/LandingPageComponents/Hero.js
+++ components/LandingPageComponents/Hero.js
@@ -14,14 +14,16 @@ const HeroImage = styled.div`
 const HeroText = styled.div`
   padding: 5vw;
   h1 {
-    font-size: 7vh;
     font-weight: 700;
-    line-height: 8vh;
-  }
+    font-size: calc(48px + (28 - 16) * ((100vw - 300px) / (1600 - 300)));
+    line-height: calc(58px + (16) * ((100vw - 300px)/(1600 - 300)));
+    }
   h2 {
-    font-size: 4vh;
     font-weight: 200;
-  }
+    font-size: calc(21px + (28 - 16) * ((100vw - 300px) / (1600 - 300)));
+    line-height: calc(30px + (16) * ((100vw - 300px)/(1600 - 300)));
+    }
+
 `
 
 const Hero = ({ isAuthenticated }) => (
@@ -40,8 +42,8 @@ const Hero = ({ isAuthenticated }) => (
       <Col xs={24} sm={24} md={14} lg={14} xl={14}></Col>
       <Col xs={24} sm={24} md={14} lg={14} xl={14}>
         <HeroText>
-          <h1 className='title'>People helping people.</h1>
-          <h2 className='subtitle'>We connect people who can volunteer time, advice, or stuff to those who need it.</h2>
+          <h1>People helping people.</h1>
+          <h2>We connect people who can volunteer time, advice, or stuff to those who need it.</h2>
           {!isAuthenticated &&
             <> 
             <Link href='/home'><Button type='primary' shape='round' size='large'> Sign up </Button></Link>&nbsp;&nbsp;
