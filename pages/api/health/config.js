fd014dd936fcae393477e9795ed304acffa00f10
Stella <stellarupro@gmail.com>
2020-04-15 15:40:33 +1200

    Updated

--- pages/api/health/config.js
+++ pages/api/health/config.js
@@ -6,8 +6,8 @@ const config = require('../../../config/serverConfig').config
 export default (req, res) => {
   res.setHeader('Content-Type', 'application/json')
 
-  // if (!req.ability.can('manage', 'Organisation')) {
-  //   return res.status(401).json('Authorisation required')
-  // }
+  if (!req.ability.can('manage', 'Organisation')) {
+    return res.status(401).json('Authorisation required')
+  }
   res.status(200).json(config)
 }
