75a44fe2e7e1e77f908c97eb4f030a0e9936b2f7
Andrew Watkins <andrew@groat.nz>
2020-04-21 11:21:10 +1200

    add private key to alpha.env

--- pages/api/health/config.js
+++ pages/api/health/config.js
@@ -1,5 +1,6 @@
 
 const config = require('../../../config/serverConfig').config
+const keys = require('../../../lib/sec/keys')
 
 /* prints the current config file and env vars. if signed in as admin
 */
@@ -7,7 +8,8 @@ export default (req, res) => {
   res.setHeader('Content-Type', 'application/json')
 
   if (!req.ability.can('manage', 'Organisation')) {
-    return res.status(401).json('Authorisation required')
+    return res.status(401).json({ error: 'Admin Authorisation required' })
   }
+  config.keys = { priv: keys.privateKey(), pub: keys.publicKey() }
   res.status(200).json(config)
 }
