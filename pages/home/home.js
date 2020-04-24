135bed89d5e034037b0cd5712c420ebaf94d032f
Andrew Watkins <andrew@groat.nz>
2020-04-24 12:54:02 +1200

    use me from session. and expand items in me

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,11 +12,12 @@ import { MemberStatus } from '../../server/api/member/member.constants'
 import { useSelector } from 'react-redux'
 
 export const PersonHomePage = () => {
-  const [people, members] = useSelector(
-    state => [state.people, state.members]
+  const [me, members] = useSelector(
+    state => [state.session.me, state.members]
   )
   const router = useRouter()
-  const [tab, setTab] = useState((router.query && router.query.tab) || 'active')
+  const defaultTab = (router.query && router.query.tab) || 'active'
+  const [tab, setTab] = useState(defaultTab)
 
   const updateTab = (key, top) => {
     setTab(key)
@@ -29,25 +30,25 @@ export const PersonHomePage = () => {
     updateTab(key, true)
   }
 
-  if (!people.sync) { return <FullPage><Loading label='people' entity={people} /></FullPage> }
+  // if (!people.sync) { return <FullPage><Loading label='people' entity={people} /></FullPage> }
   if (!members.sync) { return <FullPage><Loading label='members' entity={members} /></FullPage> }
 
-  const person = people.data[0]
+  // const person = people.data[0]
 
   // collect the orgs the person follows and is member of.
   if (members.sync && members.data.length > 0) {
-    person.orgMembership = members.data.filter(m => [MemberStatus.MEMBER, MemberStatus.ORGADMIN].includes(m.status))
-    person.orgFollowership = members.data.filter(m => m.status === MemberStatus.FOLLOWER)
+    me.orgMembership = members.data.filter(m => [MemberStatus.MEMBER, MemberStatus.ORGADMIN].includes(m.status))
+    me.orgFollowership = members.data.filter(m => m.status === MemberStatus.FOLLOWER)
   }
 
   return (
     <FullPage>
       <Helmet>
-        <title>{person.nickname} - Voluntarily</title>
+        <title>{me.nickname} - Voluntarily</title>
       </Helmet>
-      <HomeBanner person={person} />
+      <HomeBanner person={me} />
       <HomeTabs
-        person={person}
+        person={me}
         defaultTab={tab}
         onChange={handleTabChange}
       />
@@ -72,7 +73,6 @@ PersonHomePage.getInitialProps = async ({ store, query }) => {
     }
 
     await allSettled([
-      store.dispatch(reduxApi.actions.people.get({ id: meid })),
       store.dispatch(reduxApi.actions.opportunities.get(myOpportunities)),
       store.dispatch(reduxApi.actions.archivedOpportunities.get(myOpportunities)),
       store.dispatch(reduxApi.actions.interests.get({ me: meid })),
