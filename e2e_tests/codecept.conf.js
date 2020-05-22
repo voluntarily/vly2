7ddd9cb5cf42f4c75ac386fd3fedfdc4885f3c09
Andrew Watkins <andrew@groat.nz>
2020-05-22 17:12:37 +1200

    basic codecept configuration

--- e2e_tests/codecept.conf.js
+++ e2e_tests/codecept.conf.js
@@ -1,40 +1,34 @@
-// exports.config = {
-//   tests: './*_test.js',
-//   output: './output',
-//   helpers: {
-//     WebDriver: {
-//       url: 'http://localhost:3122',
-//       browser: 'chrome',
-//       host: '127.0.0.1',
-//       port: 4444,
-//       restart: false,
-//       windowSize: '1920x1680',
-//       desiredCapabilities: {
-//         chromeOptions: {
-//           args: [ /* "--headless", */ '--disable-gpu', '--window-size=1200,1000', '--no-sandbox' ]
-//         }
-//       }
-//     },
-//     REST: {
-//       endpoint: 'http://site.com/api',
-//       onRequest: (request) => {
-//         request.headers.auth = '123'
-//       }
-//     }
-//   },
-//   include: {
-//     I: './steps.js'
-//   },
-//   bootstrap: null,
-//   mocha: {},
-//   name: 'e2e_tests',
-//   plugins: {
-//     wdio: {
-//       enabled: true,
-//       services: ['selenium-standalone']
-//     },
-//     allure: {
-//       enabled: true
-//     }
-//   }
-// }
+const { setHeadlessWhen, setWindowSize } = require('@codeceptjs/configure')
+
+// run headless when CI environment variable set
+setHeadlessWhen(process.env.CI)
+// set window size for any helper: Puppeteer, WebDriver, TestCafe
+setWindowSize(1600, 1200)
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:3122',
+      show: true
+      // windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js',
+    loginPage: './pages/LoginPage.js',
+    registrationPage: './pages/RegistrationPage.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'e2e_tests',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ e2e_tests/pages/LandingPage.js
@@ -0,0 +1,35 @@
+const { I } = inject()
+
+class LandingPage {}
+
+module.exports = {
+
+  // insert your locators and methods here
+  // setting locators
+  fields: {
+    email: '#user_basic_email',
+    password: '#user_basic_password'
+  },
+  submitButton: { css: '#new_user_basic input[type=submit]' },
+
+  openPortal () {
+    console.log('Going to open voluntarily portal')
+    I.amOnPage('https://voluntarily.nz/')
+    I.see('About')
+    I.see('Organisations')
+    I.see('Lost 🧐')
+  },
+
+  checkAboutPage () {
+    I.amOnPage('https://blog.voluntarily.nz/')
+    I.see('With Aotearoa NZ facing a future filled with uncertainty, helping each other is needed now more than ever.')
+  },
+
+  checkGetInvolvedPage () {
+    I.amOnPage('https://blog.voluntarily.nz/get-involved')
+    I.see('Nau mai haere mai | Welcome!')
+  }
+
+}
+
+Object.setPrototypeOf(module.exports, LandingPage.prototype)
