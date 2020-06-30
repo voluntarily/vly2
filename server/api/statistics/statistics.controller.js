32b27091e812119e9a2ce314241a8691266b9a9b
Harrison Leach <HarrisonL7190@gmail.com>
2020-06-30 14:51:18 +1200

    VP-1842: Refactor statistics summary query into library function

--- server/api/statistics/statistics.controller.js
+++ server/api/statistics/statistics.controller.js
@@ -1,7 +1,5 @@
-const { InterestArchive } = require('../interest/interest')
-const Member = require('../member/member')
-const mongoose = require('mongoose')
 const moment = require('moment')
+const {getMembersWithAttendedInterests} = require('./statistics.lib')
 
 const getSummary = async (req, res) => {
   const { orgId, timeframe } = req.params
@@ -20,73 +18,7 @@ const getSummary = async (req, res) => {
 
   try {
     // the volunteers for an organisation are those that have attended an opportunity
-
-    const membersWithAttendedInterests = await Member.aggregate([
-      // find members in organisation with orgId
-      { $match: { organisation: mongoose.Types.ObjectId(orgId) } },
-      // populate the archived interests for each member
-      {
-        $lookup: {
-          from: InterestArchive.collection.name,
-          localField: 'person',
-          foreignField: 'person',
-          as: 'archivedInterests'
-        }
-      },
-      // filter to have only attended archived interests
-      {
-        $addFields: {
-          archivedInterests: {
-            $filter: {
-              input: '$archivedInterests',
-              cond: {
-                $eq: ['$$archivedInterests.status', 'attended']
-              },
-              as: 'archivedInterests'
-            }
-          }
-        }
-      },
-      // populate archived opportunities for each archived interest
-      {
-        $lookup: {
-          from: 'archivedopportunities',
-          localField: 'archivedInterests.opportunity',
-          foreignField: '_id',
-          as: 'opportunitiesAttended'
-        }
-      },
-      // filter opportunites within the given timeframe
-      {
-        $addFields: {
-          opportunitiesAttended: {
-            $filter: {
-              input: '$opportunitiesAttended',
-              cond: {
-                $gt: [
-                  {
-                    $arrayElemAt: ['$$opportunitiesAttended.date', 0]
-                  },
-                  afterDate
-                ]
-              },
-              as: 'opportunitiesAttended'
-            }
-          }
-        }
-      },
-      // remove members that havent attended opportunities within the timeframe
-      {
-        $match: {
-          opportunitiesAttended: {
-            $exists: true,
-            $not: {
-              $size: 0
-            }
-          }
-        }
-      }
-    ])
+    const membersWithAttendedInterests = await getMembersWithAttendedInterests(orgId, afterDate)
 
     const totalVolunteers = membersWithAttendedInterests.length
     const totalDuration = moment.duration();
@@ -107,7 +39,7 @@ const getSummary = async (req, res) => {
       avgHoursPerVolunteer
     })
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send({error: e.message})
   }
 }
 
---
+++ server/api/statistics/statistics.lib.js
@@ -0,0 +1,75 @@
+const { InterestArchive } = require('../interest/interest')
+const Member = require('../member/member')
+const mongoose = require('mongoose')
+
+const getMembersWithAttendedInterests = async (orgId, afterDate) =>
+  await Member.aggregate([
+    // find members in organisation with orgId
+    { $match: { organisation: mongoose.Types.ObjectId(orgId) } },
+    // populate the archived interests for each member
+    {
+      $lookup: {
+        from: InterestArchive.collection.name,
+        localField: "person",
+        foreignField: "person",
+        as: "archivedInterests",
+      },
+    },
+    // filter to have only attended archived interests
+    {
+      $addFields: {
+        archivedInterests: {
+          $filter: {
+            input: "$archivedInterests",
+            cond: {
+              $eq: ["$$archivedInterests.status", "attended"],
+            },
+            as: "archivedInterests",
+          },
+        },
+      },
+    },
+    // populate archived opportunities for each archived interest
+    {
+      $lookup: {
+        from: "archivedopportunities",
+        localField: "archivedInterests.opportunity",
+        foreignField: "_id",
+        as: "opportunitiesAttended",
+      },
+    },
+    // filter opportunites within the given timeframe
+    {
+      $addFields: {
+        opportunitiesAttended: {
+          $filter: {
+            input: "$opportunitiesAttended",
+            cond: {
+              $gt: [
+                {
+                  $arrayElemAt: ["$$opportunitiesAttended.date", 0],
+                },
+                afterDate,
+              ],
+            },
+            as: "opportunitiesAttended",
+          },
+        },
+      },
+    },
+    // remove members that havent attended opportunities within the timeframe
+    {
+      $match: {
+        opportunitiesAttended: {
+          $exists: true,
+          $not: {
+            $size: 0,
+          },
+        },
+      },
+    },
+  ]);
+
+  module.exports = {
+    getMembersWithAttendedInterests
+  }
