b3e3bef2a603e267bc73ed6f1751ffba8b578e6e
Harrison Leach <HarrisonL7190@gmail.com>
2020-07-09 15:04:19 +1200

    VP-1856: Refactor statistics auth into reusable middleware

--- server/api/statistics/statistics.routes.js
+++ server/api/statistics/statistics.routes.js
@@ -1,6 +1,12 @@
-const { getSummary, getLocations, getActivityTags } = require('./statistics.controller')
+const {
+  getSummary,
+  getLocations,
+  getActivityTags
+} = require('./statistics.controller')
+const { authoriseStatistics } = require('./statistics.middleware')
 
 module.exports = (server) => {
+  server.use('/api/statistics/[^/]+/:orgId/:timeframe', authoriseStatistics)
   server.get('/api/statistics/summary/:orgId/:timeframe', getSummary)
   server.get('/api/statistics/locations/:orgId/:timeframe', getLocations)
   server.get('/api/statistics/activityTags/:orgId/:timeframe', getActivityTags)
