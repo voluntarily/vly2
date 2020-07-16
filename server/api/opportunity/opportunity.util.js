2401ba5fab91abd4b221f06f8ac5c2de8b68552d
Daria <dder725@aucklanduni.ac.nz>
2020-07-16 17:18:12 +1200

    Fix special char detection

--- server/api/opportunity/opportunity.util.js
+++ server/api/opportunity/opportunity.util.js
@@ -1,13 +1,13 @@
-const Opportunity = require('./opportunity')
-const { regions } = require('../location/locationData')
-const { Role } = require('../../services/authorize/role')
-const { OpportunityType } = require('./opportunity.constants')
-const { ASK, OFFER } = OpportunityType
-const stringSimilarity = require('string-similarity')
-const natural = require('natural')
+const Opportunity = require("./opportunity");
+const { regions } = require("../location/locationData");
+const { Role } = require("../../services/authorize/role");
+const { OpportunityType } = require("./opportunity.constants");
+const { ASK, OFFER } = OpportunityType;
+const stringSimilarity = require("string-similarity");
+const natural = require("natural");
 const arrayIntersects = (arrA, arrB) =>
-  arrA.filter((x) => arrB.includes(x)).length
-const MAX_RECOMMENDATIONS = 10
+  arrA.filter((x) => arrB.includes(x)).length;
+const MAX_RECOMMENDATIONS = 10;
 
 /**
  *
@@ -21,95 +21,106 @@ const getLocationRecommendations = async (me) => {
       !!loc.containedTerritories.find((containedTerritory) =>
         me.locations.includes(containedTerritory)
       )
-  )
+  );
 
   if (regionsToMatch.length === 0) {
-    return []
+    return [];
   }
 
   // Extract all region names and the territories of those regions
   // e.g. ['Bay of Plenty', 'Western Bay of Plenty District', 'Tauranga City', ..., 'Waikato', 'Thames-Coromandel District', 'Taupo District', ...]
   const regionNames = regionsToMatch.flatMap((region) => [
     region.name,
-    ...region.containedTerritories
-  ])
+    ...region.containedTerritories,
+  ]);
 
   let locationOps = await Opportunity.find({
     locations: { $in: regionNames },
-    requestor: { $ne: me._id }
+    requestor: { $ne: me._id },
   })
-    .sort('name')
-    .collation({ locale: 'en_US', strength: 1 })
-    .populate('requestor', 'name nickname imgUrl')
-    .populate('offerOrg', 'name imgUrl role')
+    .sort("name")
+    .collation({ locale: "en_US", strength: 1 })
+    .populate("requestor", "name nickname imgUrl")
+    .populate("offerOrg", "name imgUrl role");
   // if user has specified a territory, we should show the exact matches first, because we know
   // they are closest to the user.
-  const userIsInTerritory = !me.locations.includes(regionNames)
+  const userIsInTerritory = !me.locations.includes(regionNames);
   if (userIsInTerritory) {
-    const closestOpportunities = []
-    const otherOpportunities = []
+    const closestOpportunities = [];
+    const otherOpportunities = [];
     locationOps.map((op) => {
       (arrayIntersects(me.locations, op.locations)
         ? closestOpportunities
         : otherOpportunities
-      ).push(op)
-    })
+      ).push(op);
+    });
 
-    locationOps = closestOpportunities.concat(otherOpportunities)
+    locationOps = closestOpportunities.concat(otherOpportunities);
   }
 
-  return locationOps.slice(0, MAX_RECOMMENDATIONS)
-}
+  return locationOps.slice(0, MAX_RECOMMENDATIONS);
+};
 
 const getSkillsRecommendations = async (me) => {
-  const tagsToMatch = [...me.tags, ...me.topicGroups]
+  const tagsToMatch = [...me.tags, ...me.topicGroups];
   if (!tagsToMatch.length) {
-    return []
+    return [];
   }
-  const types = []
-  if (me.role.includes(Role.VOLUNTEER)) { types.push(OFFER) }
-  if (me.role.includes(Role.BASIC) || types.length === 0) { types.push(ASK) }
+  const types = [];
+  if (me.role.includes(Role.VOLUNTEER)) {
+    types.push(OFFER);
+  }
+  if (me.role.includes(Role.BASIC) || types.length === 0) {
+    types.push(ASK);
+  }
 
   // Stem the tags to contain the root of a word
-  var format = new RegExp(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/) // Tags with special characters are not stemmed
-  var stemmedTags = tagsToMatch.map((tag) => {
-    if (!format.test(tag)) {
-      return natural.PorterStemmer.stem(tag)
-    }
-  })
-  // Turn stemmed words to a regular expression
-  var regexTags = new RegExp(stemmedTags.join('|'), 'i')
-  // Join arrays of tags and tag word roots for search
-  var tagsToSearch = tagsToMatch.concat(regexTags)
-  const opsWithMatchingTags = await Opportunity
-    .find({
-      tags: { $in: tagsToSearch },
-      type: { $in: types },
-      requestor: { $ne: me._id }
-    })
-    .populate('requestor', 'name nickname imgUrl')
-    .populate('offerOrg', 'name imgUrl role')
-  const opsWithCounts = []
-
-  opsWithMatchingTags.forEach(op => {
-    let count = 0
-    op.tags.forEach(tag => {
-      if (tagsToMatch.includes(tag)) {
-        count++
-      } else if (stemmedTags.length !== 0) {
-        // Calculate similarity score that will be added to the score
-        const simScore = stringSimilarity.findBestMatch(tag, stemmedTags).bestMatch.rating
-        count = count + simScore
+  // Tags with special characters are not stemmed
+  var format = new RegExp(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/); 
+  var stemmedTags = tagsToMatch
+    .filter((tag) => {
+      if (format.test(tag)) {
+        return false;
+      } else {
+        return true;
       }
     })
-
-    opsWithCounts.push({ count, op })
+    .map((tag) => {
+      return natural.PorterStemmer.stem(tag);
+    });
+  // Turn stemmed words into a regular expression
+  var regexTags = new RegExp(stemmedTags.join("|"), "i");
+  // Join arrays of tags and tag word roots for search
+  var tagsToSearch = tagsToMatch.concat(regexTags);
+  const opsWithMatchingTags = await Opportunity.find({
+    tags: { $in: tagsToSearch },
+    type: { $in: types },
+    requestor: { $ne: me._id },
   })
+    .populate("requestor", "name nickname imgUrl")
+    .populate("offerOrg", "name imgUrl role");
+  const opsWithCounts = [];
+
+  opsWithMatchingTags.forEach((op) => {
+    let count = 0;
+    op.tags.forEach((tag) => {
+      if (tagsToMatch.includes(tag)) {
+        count++;
+      } else if (stemmedTags.length !== 0) {
+        // Calculate similarity score that will be added to the score
+        const simScore = stringSimilarity.findBestMatch(tag, stemmedTags)
+          .bestMatch.rating;
+        count = count + simScore;
+      }
+    });
+
+    opsWithCounts.push({ count, op });
+  });
 
   opsWithCounts.sort((a, b) => {
-    return b.count - a.count
-  })
-  return opsWithCounts.map(op => op.op).slice(0, MAX_RECOMMENDATIONS)
-}
+    return b.count - a.count;
+  });
+  return opsWithCounts.map((op) => op.op).slice(0, MAX_RECOMMENDATIONS);
+};
 
-module.exports = { getLocationRecommendations, getSkillsRecommendations }
+module.exports = { getLocationRecommendations, getSkillsRecommendations };
