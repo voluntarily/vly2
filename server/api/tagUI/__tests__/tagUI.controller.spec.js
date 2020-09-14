e51ea6b072171cb62ed1d7ceff0dd9194f90bd6e
dder725 <dder725@aucklanduni.ac.nz>
2020-09-14 22:22:15 +1200

    Set tagUI tests to failing to check build's integrity

--- server/api/tagUI/__tests__/tagUI.controller.spec.js
+++ server/api/tagUI/__tests__/tagUI.controller.spec.js
@@ -5,6 +5,7 @@ import request from 'supertest'
 import sinon from 'sinon'
 import AliasSet from '../aliasSet'
 import { aliases } from './tagUI.fixture'
+import { jwtData as jwtAdmin, jwtData } from '../../../../server/middleware/session/__tests__/setSession.fixture'
 
 test.before('before connect to database', async (t) => {
   t.context.memMongo = new MemoryMongo()
@@ -23,13 +24,21 @@ test.after.always(async (t) => {
   t.context.sandbox.restore()
 })
 
-test.serial('Return an empty array when there are no aliases in the DB', async (t) => {
+test.failing('Return an empty array when there are no aliases in the DB', async (t) => {
   const res = await request(server).get('/api/tagUI/allAliases').expect(200).expect('Content-Type', /json/)
 
   t.deepEqual(res.body.length, 0, 'json should receive empty array')
 })
 
-test.serial('Return the alias sets that exist in the DB', async (t) => {
+test.failing('Test volunteer listing feedback should return only his feedback', async t => {
+  const res = await request(server).get('/api/feedback').set('Cookie', [`idToken=${jwtVolunteer.idToken}`])
+
+  t.is(res.status, 200)
+  t.true(Array.isArray(res.body))
+  res.body.forEach(f => t.is(f.respondent, people[1]._id.toString())) // all returned entries are for himself
+})
+
+test.failing('Return the alias sets that exist in the DB', async (t) => {
   await AliasSet.create(aliases)
   const res = await request(server).get('/api/tagUI/allAliases').expect(200).expect('Content-Type', /json/)
 
@@ -39,7 +48,7 @@ test.serial('Return the alias sets that exist in the DB', async (t) => {
   t.deepEqual(res.body[2].tag, 'programming')
 })
 
-test.serial('Get all aliases for an existing tag', async (t) => {
+test.failing('Get all aliases for an existing tag', async (t) => {
   await AliasSet.create(aliases)
   const res = await request(server).get('/api/tagUI/getAliases/coding').expect(200).expect('Content-Type', /json/)
 
@@ -47,18 +56,17 @@ test.serial('Get all aliases for an existing tag', async (t) => {
   t.false(res.body.aliases.includes('coding'))
 })
 
-test.serial('Getting aliases for a non-existing tag returns 404', async (t) => {
+test.failing('Getting aliases for a non-existing tag returns 404', async (t) => {
   await AliasSet.create(aliases)
   const res = await request(server).get('/api/tagUI/getAliases/nonexistingTag')
 
   t.is(res.status, 404)
 })
 
-test.serial('Deleting a tag', async (t) => {
+test.failing('Deleting a tag', async (t) => {
   await AliasSet.create(aliases)
   await request(server).get('/api/tagUI/getAliases/programming').expect(200).expect('Content-Type', /json/)
-
-  await request(server).delete('/api/tagUI/deleteTag/programming').expect(200)
+  await request(server).delete('/api/tagUI/deleteTag/programming').set('Cookie', [`idToken=${jwtData.idToken}`]).expect(200)
 
   // The deleted tag does not exist in the alias collection
   await request(server).get('/api/tagUI/getAliases/programming').expect(404)
