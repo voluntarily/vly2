39b54d5e719a1b0412b5f5292769e53fc8ca3778
dder725 <dder725@aucklanduni.ac.nz>
2020-09-23 22:07:14 +1200

    Display alias draft

--- components/TagManagement/TagTable.js
+++ components/TagManagement/TagTable.js
@@ -1,23 +1,27 @@
 import React, { useEffect } from "react";
 import { Table, Tag, Space } from "antd";
+import {TagDisplay} from "../Tags/TagDisplay";
+import TagSelect from "../Form/Input/TagSelect";
+import { TagStyle } from '../VTheme/VTheme'
+
 
 const dataSource = [
   {
     key: "1",
     tag: "programming",
-    aliases: "development, coding",
+    aliases: ["development", "coding"],
     action: "x",
   },
   {
     key: "2",
     tag: "development",
-    aliases: "programming, coding",
+    aliases: ["programming", "coding"],
     action: "x",
   },
   {
     key: "3",
     tag: "coding",
-    aliases: "programming, development",
+    aliases: ["programming", "development"],
     action: "x",
   },
 ];
@@ -32,6 +36,9 @@ const columns = [
     title: "Aliases",
     dataIndex: "aliases",
     key: "aliases",
+    render: aliases => (
+    aliases.map(alias => <TagStyle key={alias} closable onClose={()=>onChange()}>{alias}</TagStyle>)
+    )
   },
   {
     title: "Action",
@@ -40,6 +47,9 @@ const columns = [
   },
 ];
 
+function onChange() {
+  console.log("On change triggered")
+}
 export const TagTable = (props) => {
   return <Table dataSource={dataSource} columns={columns} />;
 };
