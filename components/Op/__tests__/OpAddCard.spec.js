63c99f0d31fb8d6fec91541ac2d9e69ac3278150
avowkind <andrew@groat.nz>
2021-10-13 17:18:17 +1300

    all components passing

--- components/Op/__tests__/OpAddCard.spec.js
+++ components/Op/__tests__/OpAddCard.spec.js
@@ -7,7 +7,7 @@ test('OpAddCard renders properly', t => {
     <OpAddCard />
   )
 
-  t.is(wrapper.find('a').first().props().href, '/op/new')
+  t.is(wrapper.find('a').first().props().href, '/ops/new/ask')
   t.is(wrapper.find('h1').first().text(), 'Create Activity')
   t.is(wrapper.find('p').first().text(), 'This will create an opportunity that volunteers can sign up for 🥳')
 })
